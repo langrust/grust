b171bb4063d1d10ffff07844c8762b40e8cb1406
Emilie-Thome <emilie.thome@hotmail.fr>
2023-05-03 14:28:24 +0200

    feat: locate element in source

--- src/ast/file.rs
+++ src/ast/file.rs
@@ -1,3 +1,5 @@
+use crate::util::location::Location;
+
 #[derive(Debug, PartialEq)]
 
 /// Enumerates the different kinds of files in LanGRust.
@@ -5,9 +7,11 @@ pub enum File {
     /// A LanGRust [File::Module] is composed of todo!()
     Module(
         // todo!()
+        Location
     ),
     /// A LanGRust [File::Program] is composed of todo!()
     Program(
         // todo!()
+        Location
     ),
 }
--- tests/parser.rs
+++ tests/parser.rs
@@ -1,20 +1,34 @@
 #[cfg(test)]
 mod langrust_ast_constructs {
+    use codespan_reporting::files::Files;
     use grustine::langrust;
     use grustine::ast::{
         file::File
     };
+    use grustine::util::files;
+    use grustine::util::location::Location;
 
     #[test]
     fn file_parser() {
+        let mut files = files::Files::new();
+
+        let module_test_id = files.add(
+            "module_test.gr",
+            "module"
+        ).unwrap();
+        let program_test_id = files.add(
+            "program_test.gr",
+            "program"
+        ).unwrap();
+
         let file = langrust::fileParser::new()
-            .parse("module")
+            .parse(module_test_id, &files.source(module_test_id).unwrap())
             .unwrap();
-        assert_eq!(file, File::Module());
+        assert_eq!(file, File::Module(Location::default()));
         
         let file = langrust::fileParser::new()
-            .parse("program")
+            .parse(program_test_id, &files.source(program_test_id).unwrap())
             .unwrap();
-        assert_eq!(file, File::Program());
+        assert_eq!(file, File::Program(Location::default()));
     }
 }
