c3c1538cb19391e5d43cf413bd1c47facfa75210
Emilie-Thome <emilie.thome@hotmail.fr>
2023-05-05 14:46:50 +0200

    feat: parse component

--- src/ast/component.rs
+++ src/ast/component.rs
@@ -1,8 +1,16 @@
-use crate::util::location::Location;
+use crate::util::{location::Location, type_system::Type};
+
+use crate::ast::equation::Equation;
 
 #[derive(Debug, PartialEq)]
 /// LanGRust component AST.
 pub struct Component {
+    /// Component identifier.
+    pub id: String,
+    /// Component's inputs identifiers and their types.
+    pub inputs: Vec<(String, Type)>,
+    /// Component's equations.
+    pub equations: Vec<(String, Equation)>,
     /// Component location.
     pub location: Location,
 }
