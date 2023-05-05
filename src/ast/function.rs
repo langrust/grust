70b30bbe2642de2cbcba35223e60f6b4833663b2
Emilie-Thome <emilie.thome@hotmail.fr>
2023-05-05 14:12:08 +0200

    feat: parse function

--- src/ast/function.rs
+++ src/ast/function.rs
@@ -1,8 +1,18 @@
-use crate::util::location::Location;
+use crate::util::{location::Location, type_system::Type};
+
+use crate::ast::{calculus::Calculus, expression::Expression};
 
 #[derive(Debug, PartialEq)]
 /// LanGRust function AST.
 pub struct Function {
+    /// Function identifier.
+    pub id: String,
+    /// Function's inputs identifiers and their types.
+    pub inputs: Vec<(String, Type)>,
+    /// Function's calculi.
+    pub calculi: Vec<(String, Calculus)>,
+    /// Function's returned expression and its type.
+    pub returned: (Type, Expression),
     /// Function location.
     pub location: Location,
 }
