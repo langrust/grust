37fdbba746f7d7134d0c1a7d04343d5b8e146ae7
Emilie-Thome <emilie.thome@hotmail.fr>
2023-05-05 13:37:01 +0200

    feat: parse node

--- src/ast/node.rs
+++ src/ast/node.rs
@@ -1,8 +1,16 @@
-use crate::util::location::Location;
+use crate::util::{location::Location, type_system::Type};
+
+use crate::ast::equation::Equation;
 
 #[derive(Debug, PartialEq)]
 /// LanGRust node AST.
 pub struct Node {
+    /// Node identifier.
+    pub id: String,
+    /// Node's inputs identifiers and their types.
+    pub inputs: Vec<(String, Type)>,
+    /// Node's equations.
+    pub equations: Vec<(String, Equation)>,
     /// Node location.
     pub location: Location,
 }
