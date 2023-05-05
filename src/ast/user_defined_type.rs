c010b3d5d3db2bd906398cad3fe4378f7ebdf607
Emilie-Thome <emilie.thome@hotmail.fr>
2023-05-05 14:28:48 +0200

    feat: parse user defined types

--- src/ast/user_defined_type.rs
+++ src/ast/user_defined_type.rs
@@ -1,24 +1,35 @@
-use crate::util::location::Location;
+use crate::util::{location::Location, type_system::Type};
 
 #[derive(Debug, PartialEq)]
 /// LanGRust user defined type AST.
 pub enum UserDefinedType {
     /// Represents a structure definition.
     Structure {
-        // todo!()
+        /// The structure's identifier.
+        id: String,
+        /// The structure's fields: a field has an identifier and a type.
+        fields: Vec<(String, Type)>,
         /// Structure location.
         location: Location,
     },
     /// Represents an enumeration definition.
     Enumeration {
-        // todo!()
-        /// Structure location.
+        /// The enumeration's identifier.
+        id: String,
+        /// The enumeration's elements.
+        elements: Vec<String>,
+        /// Enumeration location.
         location: Location,
     },
     /// Represents an array definition.
     Array {
-        // todo!()
-        /// Structure location.
+        /// The array's identifier.
+        id: String,
+        /// The array's type.
+        array_type: Type,
+        /// The array's size.
+        size: usize,
+        /// Array location.
         location: Location,
     },
 }
