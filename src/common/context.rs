98a3c26ea332a8c22559872633a6348c86d1ecc1
emilie.e.thome <emilie.e.thome@renault.com>
2023-05-24 17:14:15 +0200

    feat: add get_field_or_error to context trait

--- src/common/context.rs
+++ src/common/context.rs
@@ -73,6 +73,39 @@ pub trait Context {
         errors: &mut Vec<Error>,
     ) -> Result<&Self::Item, Error>;
 
+    /// Returns a reference to the item corresponding to the name or raises an error.
+    ///
+    /// Raises an [Error::UnknownType] when the context does not contains an item
+    /// corresponding to the name. Otherwise, returns a reference to the item.
+    ///
+    /// # Example
+    ///
+    /// Basic usage:
+    ///
+    /// ```rust
+    /// use std::collections::HashMap;
+    /// use grustine::ast::{location::Location, type_system::Type};
+    /// use grustine::common::context::Context;
+    ///
+    /// let mut structure_fields = HashMap::new();
+    /// let mut errors = vec![];
+    /// let location = Location::default();
+    ///
+    /// let structure_name = String::from("Time");
+    /// let field_name = String::from("minute");
+    /// structure_fields.insert(field_name.clone(), Type::Integer);
+    ///
+    /// structure_fields.get_field_or_error(structure_name.clone(), field_name, location.clone(), &mut errors).unwrap();
+    /// structure_fields.get_field_or_error(structure_name.clone(), String::from("hour"), location, &mut errors).unwrap_err();
+    /// ```
+    fn get_field_or_error(
+        &self,
+        structure_name: String,
+        field_name: String,
+        location: Location,
+        errors: &mut Vec<Error>,
+    ) -> Result<&Self::Item, Error>;
+
     /// Insert the item corresponding to the name or raises an error.
     ///
     /// Raises an [Error::AlreadyDefinedElement] when the context already contains an item
@@ -139,6 +172,27 @@ impl<V> Context for HashMap<String, V> {
             None => {
                 let error = Error::UnknownType {
                     name: name.clone(),
+                    location: location.clone(),
+                };
+                errors.push(error.clone());
+                Err(error)
+            }
+        }
+    }
+
+    fn get_field_or_error(
+        &self,
+        structure_name: String,
+        field_name: String,
+        location: Location,
+        errors: &mut Vec<Error>,
+    ) -> Result<&Self::Item, Error> {
+        match self.get(&field_name) {
+            Some(item) => Ok(item),
+            None => {
+                let error = Error::UnknownField {
+                    structure_name,
+                    field_name,
                     location: location.clone(),
                 };
                 errors.push(error.clone());
@@ -221,6 +275,67 @@ mod get_element_or_error {
 
         let error = elements_context
             .get_element_or_error(String::from("y"), Location::default(), &mut errors)
+            .unwrap_err();
+
+        let control = vec![error];
+
+        assert_eq!(errors, control);
+    }
+}
+
+#[cfg(test)]
+mod get_field_or_error {
+    use crate::ast::{location::Location, type_system::Type};
+    use crate::common::context::Context;
+    use std::collections::HashMap;
+
+    #[test]
+    fn should_get_reference_when_field_in_structure() {
+        let mut errors = vec![];
+        let mut structure_fields = HashMap::new();
+
+        let structure_name = String::from("Point");
+        structure_fields.insert(String::from("x"), Type::Integer);
+        structure_fields.insert(String::from("y"), Type::Integer);
+
+        let field_type = structure_fields
+            .get_field_or_error(structure_name, String::from("x"), Location::default(), &mut errors)
+            .unwrap();
+
+        let control = Type::Integer;
+
+        assert_eq!(*field_type, control);
+    }
+
+    #[test]
+    fn should_not_add_error_when_field_in_structure() {
+        let mut errors = vec![];
+        let mut structure_fields = HashMap::new();
+
+        let structure_name = String::from("Point");
+        structure_fields.insert(String::from("x"), Type::Integer);
+        structure_fields.insert(String::from("y"), Type::Integer);
+
+        let _ = structure_fields
+            .get_field_or_error(structure_name, String::from("x"), Location::default(), &mut errors)
+            .unwrap();
+
+        let control = vec![];
+
+        assert_eq!(errors, control);
+    }
+
+    #[test]
+    fn should_raise_and_add_error_when_field_not_in_structure() {
+        let mut errors = vec![];
+        let mut structure_fields = HashMap::new();
+
+        let structure_name = String::from("Point");
+        structure_fields.insert(String::from("x"), Type::Integer);
+        structure_fields.insert(String::from("y"), Type::Integer);
+
+        let error = structure_fields
+            .get_field_or_error(structure_name, String::from("z"), Location::default(), &mut errors)
             .unwrap_err();
 
         let control = vec![error];
