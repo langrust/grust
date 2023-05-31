bd6e2e9641df603e6a4e37664dfd20a1ae1c3d86
emilie.e.thome <emilie.e.thome@renault.com>
2023-05-31 12:21:36 +0200

    feat: typing node application

--- src/ast/stream_expression/mod.rs
+++ src/ast/stream_expression/mod.rs
@@ -3158,6 +3158,166 @@ mod typing {
             )
             .unwrap_err();
     }
+
+    #[test]
+    fn should_type_node_application_stream_expression() {
+        let mut errors = vec![];
+        let mut nodes_context = HashMap::new();
+        nodes_context.insert(
+            String::from("my_node"),
+            NodeDescription {
+                inputs: vec![
+                    (String::from("x"), Type::Integer),
+                    (String::from("y"), Type::Integer),
+                ],
+                outputs: HashMap::from([(String::from("o"), Type::Integer)]),
+                locals: HashMap::new(),
+            },
+        );
+        let mut signals_context = HashMap::new();
+        signals_context.insert(String::from("x"), Type::Integer);
+        let mut elements_context = HashMap::new();
+        elements_context.insert(
+            String::from("f"),
+            Type::Abstract(Box::new(Type::Integer), Box::new(Type::Integer)),
+        );
+        let user_types_context = HashMap::new();
+
+        let mut stream_expression = StreamExpression::NodeApplication {
+            node: String::from("my_node"),
+            inputs: vec![
+                StreamExpression::MapApplication {
+                    function_expression: Expression::Call {
+                        id: String::from("f"),
+                        typing: None,
+                        location: Location::default(),
+                    },
+                    inputs: vec![StreamExpression::SignalCall {
+                        id: String::from("x"),
+                        typing: None,
+                        location: Location::default(),
+                    }],
+                    typing: None,
+                    location: Location::default(),
+                },
+                StreamExpression::Constant {
+                    constant: Constant::Integer(1),
+                    typing: None,
+                    location: Location::default(),
+                },
+            ],
+            signal: String::from("o"),
+            typing: None,
+            location: Location::default(),
+        };
+        let control = StreamExpression::NodeApplication {
+            node: String::from("my_node"),
+            inputs: vec![
+                StreamExpression::MapApplication {
+                    function_expression: Expression::Call {
+                        id: String::from("f"),
+                        typing: Some(Type::Abstract(
+                            Box::new(Type::Integer),
+                            Box::new(Type::Integer),
+                        )),
+                        location: Location::default(),
+                    },
+                    inputs: vec![StreamExpression::SignalCall {
+                        id: String::from("x"),
+                        typing: Some(Type::Integer),
+                        location: Location::default(),
+                    }],
+                    typing: Some(Type::Integer),
+                    location: Location::default(),
+                },
+                StreamExpression::Constant {
+                    constant: Constant::Integer(1),
+                    typing: Some(Type::Integer),
+                    location: Location::default(),
+                },
+            ],
+            signal: String::from("o"),
+            typing: Some(Type::Integer),
+            location: Location::default(),
+        };
+
+        stream_expression
+            .typing(
+                &nodes_context,
+                &signals_context,
+                &elements_context,
+                &user_types_context,
+                &mut errors,
+            )
+            .unwrap();
+
+        assert_eq!(stream_expression, control);
+    }
+
+    #[test]
+    fn should_raise_error_for_incompatible_node_application() {
+        let mut errors = vec![];
+        let mut nodes_context = HashMap::new();
+        nodes_context.insert(
+            String::from("my_node"),
+            NodeDescription {
+                inputs: vec![
+                    (String::from("x"), Type::Integer),
+                    (String::from("y"), Type::Integer),
+                ],
+                outputs: HashMap::from([(String::from("o"), Type::Integer)]),
+                locals: HashMap::new(),
+            },
+        );
+        let mut signals_context = HashMap::new();
+        signals_context.insert(String::from("x"), Type::Integer);
+        let mut elements_context = HashMap::new();
+        elements_context.insert(
+            String::from("f"),
+            Type::Abstract(Box::new(Type::Integer), Box::new(Type::Float)),
+        );
+        let user_types_context = HashMap::new();
+
+        let mut stream_expression = StreamExpression::NodeApplication {
+            node: String::from("my_node"),
+            inputs: vec![
+                StreamExpression::MapApplication {
+                    function_expression: Expression::Call {
+                        id: String::from("f"),
+                        typing: None,
+                        location: Location::default(),
+                    },
+                    inputs: vec![StreamExpression::SignalCall {
+                        id: String::from("x"),
+                        typing: None,
+                        location: Location::default(),
+                    }],
+                    typing: None,
+                    location: Location::default(),
+                },
+                StreamExpression::Constant {
+                    constant: Constant::Integer(1),
+                    typing: None,
+                    location: Location::default(),
+                },
+            ],
+            signal: String::from("o"),
+            typing: None,
+            location: Location::default(),
+        };
+
+        let error = stream_expression
+            .typing(
+                &nodes_context,
+                &signals_context,
+                &elements_context,
+                &user_types_context,
+                &mut errors,
+            )
+            .unwrap_err();
+
+        assert_eq!(errors, vec![error]);
+    }
 }
 
 #[cfg(test)]
