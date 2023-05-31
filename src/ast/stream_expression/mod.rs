2d536bc620ef9a8bff594680045abc4cad7e51d8
emilie.e.thome <emilie.e.thome@renault.com>
2023-05-31 09:05:54 +0200

    feat: add typing field in stream expressions structures

--- src/ast/stream_expression/mod.rs
+++ src/ast/stream_expression/mod.rs
@@ -1,3 +1,111 @@
+use crate::ast::{
+    constant::Constant, expression::Expression, location::Location, pattern::Pattern, type_system::Type,
+};
+
+#[derive(Debug, PartialEq, Clone)]
+/// LanGRust stream expression AST.
+pub enum StreamExpression {
+    /// Constant stream expression.
+    Constant {
+        /// The constant.
+        constant: Constant,
+        /// Stream Expression type.
+        typing: Option<Type>,
+        /// Stream expression location.
+        location: Location,
+    },
+    /// Signal call stream expression.
+    SignalCall {
+        /// Signal identifier.
+        id: String,
+        /// Stream Expression type.
+        typing: Option<Type>,
+        /// Stream expression location.
+        location: Location,
+    },
+    /// Initialized buffer stream expression.
+    FollowedBy {
+        /// The initialization constant.
+        constant: Constant,
+        /// The buffered expression.
+        expression: Box<StreamExpression>,
+        /// Stream Expression type.
+        typing: Option<Type>,
+        /// Stream expression location.
+        location: Location,
+    },
+    /// Map application stream expression.
+    MapApplication {
+        /// The expression applied.
+        function_expression: Expression,
+        /// The inputs to the expression.
+        inputs: Vec<StreamExpression>,
+        /// Stream Expression type.
+        typing: Option<Type>,
+        /// Stream expression location.
+        location: Location,
+    },
+    /// Node application stream expression.
+    NodeApplication {
+        /// The node applied.
+        node: String,
+        /// The inputs to the expression.
+        inputs: Vec<StreamExpression>,
+        /// The signal retrieved.
+        signal: String,
+        /// Stream Expression type.
+        typing: Option<Type>,
+        /// Stream expression location.
+        location: Location,
+    },
+    /// Structure stream expression.
+    Structure {
+        /// The structure name.
+        name: String,
+        /// The fields associated with their expressions.
+        fields: Vec<(String, StreamExpression)>,
+        /// Stream Expression type.
+        typing: Option<Type>,
+        /// Stream expression location.
+        location: Location,
+    },
+    /// Array stream expression.
+    Array {
+        /// The elements inside the array.
+        elements: Vec<StreamExpression>,
+        /// Stream Expression type.
+        typing: Option<Type>,
+        /// Stream expression location.
+        location: Location,
+    },
+    /// Pattern matching stream expression.
+    Match {
+        /// The stream expression to match.
+        expression: Box<StreamExpression>,
+        /// The different matching cases.
+        arms: Vec<(Pattern, Option<StreamExpression>, StreamExpression)>,
+        /// Stream Expression type.
+        typing: Option<Type>,
+        /// Stream expression location.
+        location: Location,
+    },
+    /// When present stream expression.
+    When {
+        /// The identifier of the value when present
+        id: String,
+        /// The optional stream expression.
+        option: Box<StreamExpression>,
+        /// The stream expression when present.
+        present: Box<StreamExpression>,
+        /// The default stream expression.
+        default: Box<StreamExpression>,
+        /// Stream Expression type.
+        typing: Option<Type>,
+        /// Stream expression location.
+        location: Location,
+    },
+}
+
 use std::collections::HashMap;
 
 use crate::ast::{
