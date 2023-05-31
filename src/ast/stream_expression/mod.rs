2c822c1919b4a12c14a66c615e231463aab14f45
emilie.e.thome <emilie.e.thome@renault.com>
2023-05-31 11:14:00 +0200

    feat: typing branch for followed by stream expression

--- src/ast/stream_expression/mod.rs
+++ src/ast/stream_expression/mod.rs
@@ -8,6 +8,7 @@ use crate::error::Error;
 
 mod array;
 mod constant;
+mod followed_by;
 mod map_application;
 mod r#match;
 mod signal_call;
@@ -146,7 +147,12 @@ impl StreamExpression {
         match self {
             StreamExpression::Constant { .. } => self.typing_constant(),
             StreamExpression::SignalCall { .. } => self.typing_signal_call(signals_context, errors),
-            StreamExpression::FollowedBy { .. } => todo!(),
+            StreamExpression::FollowedBy { .. } => self.typing_followed_by(
+                signals_context,
+                elements_context,
+                user_types_context,
+                errors,
+            ),
             StreamExpression::MapApplication { .. } => self.typing_map_application(
                 signals_context,
                 elements_context,
@@ -1021,6 +1027,114 @@ mod typing {
             .unwrap();
 
         assert_eq!(stream_expression, control);
+    }
+
+    #[test]
+    fn should_type_followed_by_stream_expression() {
+        let mut errors = vec![];
+        let mut signals_context = HashMap::new();
+        signals_context.insert(String::from("x"), Type::Integer);
+        let mut elements_context = HashMap::new();
+        elements_context.insert(
+            String::from("add_one"),
+            Type::Abstract(Box::new(Type::Integer), Box::new(Type::Integer)),
+        );
+        let user_types_context = HashMap::new();
+
+        let mut stream_expression = StreamExpression::FollowedBy {
+            constant: Constant::Integer(0),
+            expression: Box::new(StreamExpression::MapApplication {
+                function_expression: Expression::Call {
+                    id: String::from("add_one"),
+                    typing: None,
+                    location: Location::default(),
+                },
+                inputs: vec![StreamExpression::SignalCall {
+                    id: String::from("x"),
+                    typing: None,
+                    location: Location::default(),
+                }],
+                typing: None,
+                location: Location::default(),
+            }),
+            typing: None,
+            location: Location::default(),
+        };
+        let control = StreamExpression::FollowedBy {
+            constant: Constant::Integer(0),
+            expression: Box::new(StreamExpression::MapApplication {
+                function_expression: Expression::Call {
+                    id: String::from("add_one"),
+                    typing: Some(Type::Abstract(
+                        Box::new(Type::Integer),
+                        Box::new(Type::Integer),
+                    )),
+                    location: Location::default(),
+                },
+                inputs: vec![StreamExpression::SignalCall {
+                    id: String::from("x"),
+                    typing: Some(Type::Integer),
+                    location: Location::default(),
+                }],
+                typing: Some(Type::Integer),
+                location: Location::default(),
+            }),
+            typing: Some(Type::Integer),
+            location: Location::default(),
+        };
+
+        stream_expression
+            .typing(
+                &signals_context,
+                &elements_context,
+                &user_types_context,
+                &mut errors,
+            )
+            .unwrap();
+
+        assert_eq!(stream_expression, control);
+    }
+
+    #[test]
+    fn should_raise_error_for_incompatible_type_in_followed_by() {
+        let mut errors = vec![];
+        let mut signals_context = HashMap::new();
+        signals_context.insert(String::from("x"), Type::Integer);
+        let mut elements_context = HashMap::new();
+        elements_context.insert(
+            String::from("add_one"),
+            Type::Abstract(Box::new(Type::Integer), Box::new(Type::Integer)),
+        );
+        let user_types_context = HashMap::new();
+
+        let mut stream_expression = StreamExpression::FollowedBy {
+            constant: Constant::Float(0.0),
+            expression: Box::new(StreamExpression::MapApplication {
+                function_expression: Expression::Call {
+                    id: String::from("add_one"),
+                    typing: None,
+                    location: Location::default(),
+                },
+                inputs: vec![StreamExpression::SignalCall {
+                    id: String::from("x"),
+                    typing: None,
+                    location: Location::default(),
+                }],
+                typing: None,
+                location: Location::default(),
+            }),
+            typing: None,
+            location: Location::default(),
+        };
+
+        stream_expression
+            .typing(
+                &signals_context,
+                &elements_context,
+                &user_types_context,
+                &mut errors,
+            )
+            .unwrap_err();
     }
 }
 
