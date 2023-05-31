7c28b0f68db693a1b516a8eef8b21e9ae01da025
Emilie Thome <emilie.thome@hotmail.fr>
2023-05-31 09:20:00 +0200

    feat: typing stream expression constant

---
+++ src/ast/stream_expression/constant.rs
@@ -0,0 +1,43 @@
+use crate::ast::stream_expression::StreamExpression;
+use crate::error::Error;
+
+impl StreamExpression {
+    /// Add a [Type] to the constant stream expression.
+    pub fn typing_constant(&mut self) -> Result<(), Error> {
+        match self {
+            // typing a constant stream expression consist of getting the type of the constant
+            StreamExpression::Constant {
+                constant,
+                typing,
+                location: _,
+            } => {
+                *typing = Some(constant.get_type());
+                Ok(())
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod typing_constant {
+    use crate::ast::{constant::Constant, stream_expression::StreamExpression, location::Location};
+
+    #[test]
+    fn should_type_constant_stream_expression() {
+        let mut stream_expression = StreamExpression::Constant {
+            constant: Constant::Integer(0),
+            typing: None,
+            location: Location::default(),
+        };
+        let control = StreamExpression::Constant {
+            constant: Constant::Integer(0),
+            typing: Some(Constant::Integer(0).get_type()),
+            location: Location::default(),
+        };
+
+        stream_expression.typing_constant().unwrap();
+
+        assert_eq!(stream_expression, control);
+    }
+}
