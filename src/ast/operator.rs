7828fd3b3007a1668d3d5eaa8300d29ba1a105ed
emilie.e.thome <emilie.e.thome@renault.com>
2023-05-31 17:47:33 +0200

    feat: create a function generating the global context

--- src/ast/operator.rs
+++ src/ast/operator.rs
@@ -1,3 +1,7 @@
+use strum::EnumIter;
+
+use crate::ast::type_system::Type;
+
 /// LanGrust binary operators.
 ///
 /// [BinaryOperator] enumeration represents all possible binary operations
@@ -14,6 +18,7 @@
 /// - [BinaryOperator::Leq], "lower or equal" `<=`
 /// - [BinaryOperator::Grt], "greater" `>`
 /// - [BinaryOperator::Low], "lower" `<`
+#[derive(EnumIter)]
 pub enum BinaryOperator {
     /// Multiplication, `x * y`.
     Mul,
@@ -58,6 +63,75 @@ impl ToString for BinaryOperator {
         }
     }
 }
+impl BinaryOperator {
+    /// Get the [Type] of a binary operator.
+    /// 
+    /// # Example
+    /// ```rust
+    /// use grustine::ast::operator::BinaryOperator;
+    /// let add_type = BinaryOperator::Add.get_type();
+    /// ```
+    pub fn get_type(&self) -> Type {
+        let number_types = vec![Type::Integer, Type::Float];
+        let all_basic_types = vec![Type::Integer, Type::Float, Type::Boolean, Type::String, Type::Unit];
+
+        match self {
+            // If self is an operator over numbers then its type can either
+            // be `int -> int -> int` or `float -> float -> float`
+            // then it is a [Type::Choice]
+              BinaryOperator::Mul | BinaryOperator::Div 
+            | BinaryOperator::Add | BinaryOperator::Sub => {
+                let v_t = number_types.into_iter()
+                    .map(
+                        |t| Type::Abstract(
+                            Box::new(t.clone()),
+                            Box::new(Type::Abstract(
+                                Box::new(t.clone()),
+                                Box::new(t))))
+                    ).collect();
+                Type::Choice(v_t)
+            },
+            // If self is a comparison over numbers then its type can either
+            // be `int -> int -> bool` or `float -> float -> bool`
+            // then it is a [Type::Choice]
+              BinaryOperator::Geq | BinaryOperator::Leq
+            | BinaryOperator::Grt | BinaryOperator::Low => {
+                let v_t = number_types.into_iter()
+                    .map(
+                        |t| Type::Abstract(
+                            Box::new(t.clone()),
+                            Box::new(Type::Abstract(
+                                Box::new(t),
+                                Box::new(Type::Boolean))))
+                    ).collect();
+                Type::Choice(v_t)
+            },
+            // If self is an equality or inequality test then its type can
+            // be `t -> t -> bool` for any t
+            // then it is a [Type::Choice]
+            BinaryOperator::Eq | BinaryOperator::Dif => {
+                let v_t = all_basic_types.into_iter()
+                    .map(
+                        |t| Type::Abstract(
+                            Box::new(t.clone()),
+                            Box::new(Type::Abstract(
+                                Box::new(t),
+                                Box::new(Type::Boolean))))
+                    ).collect();
+                Type::Choice(v_t)
+            },
+            // If self is a logical operator then its type 
+            // is `bool -> bool -> bool`
+            BinaryOperator::And | BinaryOperator::Or => {
+                Type::Abstract(
+                    Box::new(Type::Boolean),
+                    Box::new(Type::Abstract(
+                        Box::new(Type::Boolean),
+                        Box::new(Type::Boolean))))
+            },
+        }
+    }
+}
 
 /// LanGrust unary operators.
 ///
@@ -66,6 +140,7 @@ impl ToString for BinaryOperator {
 /// - [UnaryOperator::Neg] is the numerical negation `-`
 /// - [UnaryOperator::Not], the logical negation `!`
 /// - [UnaryOperator::Brackets], is the use of brackets `(_)`
+#[derive(EnumIter)]
 pub enum UnaryOperator {
     /// Numerical negation, `-x`.
     Neg,
@@ -83,6 +158,45 @@ impl ToString for UnaryOperator {
         }
     }
 }
+impl UnaryOperator {
+    /// Get the [Type] of a unary operator.
+    /// 
+    /// # Example
+    /// ```rust
+    /// use grustine::ast::operator::UnaryOperator;
+    /// let neg_type = UnaryOperator::Neg.get_type();
+    /// ```
+    pub fn get_type(&self) -> Type {
+        let number_types = vec![Type::Integer, Type::Float];
+        let all_basic_types = vec![Type::Integer, Type::Float, Type::Boolean, Type::String, Type::Unit];
+
+        match self {
+            // If self is the numerical negation then its type can either
+            // be `int -> int` or `float -> float`
+            // then it is a [Type::Choice]
+            UnaryOperator::Neg => {
+                let v_t = number_types.into_iter()
+                    .map(
+                        |t| Type::Abstract(Box::new(t.clone()), Box::new(t))
+                    ).collect();
+                Type::Choice(v_t)
+            },
+            // If self is "brackets" then its type can be `t -> t` for any t
+            // then it is a [Type::Choice]
+            UnaryOperator::Brackets => {
+                let v_t = all_basic_types.into_iter()
+                    .map(
+                        |t| Type::Abstract(Box::new(t.clone()), Box::new(t))
+                    ).collect();
+                Type::Choice(v_t)
+            },
+            // If self is the logical negation then its type is `bool -> bool`
+            UnaryOperator::Not => {
+                Type::Abstract(Box::new(Type::Boolean), Box::new(Type::Boolean))
+            },
+        }
+    }
+}
 
 /// Other builtin operators in LanGrust.
 ///
@@ -90,6 +204,7 @@ impl ToString for UnaryOperator {
 /// that can be used in a LanGRust program:
 /// - [OtherOperator::IfThenElse] is `if _ then _ else _`
 /// - [OtherOperator::Print] is the usual `print` function
+#[derive(EnumIter)]
 pub enum OtherOperator {
     /// The `if b then x else y` LanGRust expression.
     IfThenElse,
@@ -101,6 +216,41 @@ impl ToString for OtherOperator {
         match self {
             OtherOperator::IfThenElse => String::from("if_then_else"),
             OtherOperator::Print => String::from("print"),
+        }
+    }
+}
+impl OtherOperator {
+    /// Get the [Type] of the other operators.
+    /// 
+    /// # Example
+    /// ```rust
+    /// use grustine::ast::operator::OtherOperator;
+    /// let ifthenelse_type = OtherOperator::IfThenElse.get_type();
+    /// ```
+    pub fn get_type(&self) -> Type {
+        let all_basic_types = vec![Type::Integer, Type::Float, Type::Boolean];
+        match self {
+            // If self is "if _ then _ else _" its type can be
+            // `bool -> t -> t` for any type t
+            // then it is a [Type::Choice]
+            OtherOperator::IfThenElse => {
+                let v_t = all_basic_types.into_iter()
+                    .map(
+                        |t| Type::Abstract(
+                            Box::new(Type::Boolean),
+                            Box::new(Type::Abstract(
+                                Box::new(t.clone()),
+                                Box::new(Type::Abstract(
+                                    Box::new(t.clone()),
+                                    Box::new(t))
+                                ))
+                            ))
+                    ).collect();
+                Type::Choice(v_t)
+            },
+            OtherOperator::Print => {
+                Type::Abstract(Box::new(Type::String), Box::new(Type::Unit))
+            }
         }
     }
 }
