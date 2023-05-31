08c24699b873bddc7f8eaf0b9bec330552265f1f
emilie.e.thome <emilie.e.thome@renault.com>
2023-05-31 14:25:40 +0200

    feat: add typing equation and check types

--- src/ast/equation.rs
+++ src/ast/equation.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
+
 use crate::ast::{
-    location::Location, scope::Scope, stream_expression::StreamExpression, type_system::Type,
+    location::Location, scope::Scope, stream_expression::node_description::NodeDescription,
+    stream_expression::StreamExpression, type_system::Type, user_defined_type::UserDefinedType,
 };
+use crate::error::Error;
 
 #[derive(Debug, PartialEq, Clone)]
 /// LanGRust equation AST.
@@ -15,4 +19,52 @@ pub struct Equation {
     pub expression: StreamExpression,
     /// Equation location.
     pub location: Location,
+}
+
+impl Equation {
+    /// Add a [Type] to the equation.
+    ///
+    /// # Example
+    /// ```rust
+    /// use std::collections::HashMap;
+    /// use grustine::ast::{constant::Constant, stream_expression::StreamExpression, location::Location};
+    /// let mut errors = vec![];
+    /// let nodes_context = HashMap::new();
+    /// let signals_context = HashMap::new();
+    /// let elements_context = HashMap::new();
+    /// let user_types_context = HashMap::new();
+    /// let mut stream_expression = StreamExpression::Constant {
+    ///     constant: Constant::Integer(0),
+    ///     typing: None,
+    ///     location: Location::default(),
+    /// };
+    /// stream_expression.typing(&nodes_context, &signals_context, &elements_context, &user_types_context, &mut errors).unwrap();
+    /// ```
+    pub fn typing(
+        &mut self,
+        nodes_context: &HashMap<String, NodeDescription>,
+        signals_context: &HashMap<String, Type>,
+        elements_context: &HashMap<String, Type>,
+        user_types_context: &HashMap<String, UserDefinedType>,
+        errors: &mut Vec<Error>,
+    ) -> Result<(), Error> {
+        let Equation {
+            signal_type,
+            expression,
+            location,
+            ..
+        } = self;
+
+        expression.typing(
+            nodes_context,
+            signals_context,
+            elements_context,
+            user_types_context,
+            errors,
+        )?;
+
+        let expression_type = expression.get_type().unwrap();
+
+        expression_type.eq_check(signal_type, location.clone(), errors)
+    }
 }
