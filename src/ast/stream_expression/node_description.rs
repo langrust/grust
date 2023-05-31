9bf9fdf2a8ae8a89b014f36b47f10581160e7ff5
Emilie Thome <emilie.thome@hotmail.fr>
2023-05-31 11:40:14 +0200

    feat: add node description context to typing function

---
+++ src/ast/stream_expression/node_description.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+use crate::ast::type_system::Type;
+
+#[derive(Debug)]
+/// The description of a node's signals.
+pub struct NodeDescription {
+    /// Node's input signals.
+    pub inputs: Vec<(String, Type)>,
+    /// Node's output signals.
+    pub outputs: HashMap<String, Type>,
+    /// Node's local signals.
+    pub locals: HashMap<String, Type>,
+}
