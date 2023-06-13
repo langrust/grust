2f16360a36e158bb5a20ecc95fcdd9c888456515
Emilie THOME <emilie.e.thome@renault.com>
2023-06-13 08:36:25 +0200

    feat: add colors to mark processing status

---
+++ src/common/color.rs
@@ -0,0 +1,9 @@
+/// [Color] enumeration used to identify the processing status of an element.
+pub enum Color {
+    /// Computation has ended.
+    Black,
+    /// Currently being processed.
+    Grey,
+    /// Element not processed.
+    White,
+}
