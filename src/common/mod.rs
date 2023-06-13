3c83bfc9e3d388c701dfd33771a94e60b961b766
Emilie Thome <emilie.thome@hotmail.fr>
2023-06-13 08:36:25 +0200

    feat: add colors to mark processing status

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,5 +1,8 @@
 /// [Context] trait definition.
 pub mod context;
 
-/// Graph API.
+/// [Graph] API.
 pub mod graph;
+
+/// [Color] enumeration used to identify the processing status of an element.
+pub mod color;
