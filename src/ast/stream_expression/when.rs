0ccd3ff6d7f1cca0be30c92a276dbd76f6881e5c
Emilie Thome <emilie.thome@hotmail.fr>
2023-06-14 11:06:01 +0200

    feat: fragment get_dependencies function from when

--- src/ast/stream_expression/when.rs
+++ src/ast/stream_expression/when.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 
 use crate::ast::{
-    node_description::NodeDescription, stream_expression::StreamExpression, type_system::Type,
-    user_defined_type::UserDefinedType,
+    node::Node, node_description::NodeDescription, stream_expression::StreamExpression,
+    type_system::Type, user_defined_type::UserDefinedType,
 };
+use crate::common::{color::Color, graph::Graph};
 use crate::error::Error;
 
 impl StreamExpression {
@@ -71,6 +72,48 @@ impl StreamExpression {
                         Err(())
                     }
                 }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    /// Get dependencies of a when stream expression.
+    pub fn get_dependencies_when(
+        &self,
+        nodes_context: &HashMap<String, Node>,
+        nodes_graphs: &mut HashMap<String, Graph<Color>>,
+        nodes_reduced_graphs: &mut HashMap<String, Graph<Color>>,
+        errors: &mut Vec<Error>,
+    ) -> Result<Vec<(String, usize)>, ()> {
+        match self {
+            // dependencies of when are dependencies of the optional expression
+            // plus present and default expressions (without the new local signal)
+            StreamExpression::When {
+                id: local_signal,
+                option,
+                present,
+                default,
+                ..
+            } => {
+                let mut option_dependencies = option.get_dependencies(
+                    nodes_context,
+                    nodes_graphs,
+                    nodes_reduced_graphs,
+                    errors,
+                )?;
+                let mut present_dependencies = present
+                    .get_dependencies(nodes_context, nodes_graphs, nodes_reduced_graphs, errors)?
+                    .into_iter()
+                    .filter(|(signal, _)| !signal.eq(local_signal))
+                    .collect();
+                let mut default_dependencies = default
+                    .get_dependencies(nodes_context, nodes_graphs, nodes_reduced_graphs, errors)?
+                    .into_iter()
+                    .filter(|(signal, _)| !signal.eq(local_signal))
+                    .collect();
+                option_dependencies.append(&mut present_dependencies);
+                option_dependencies.append(&mut default_dependencies);
+                Ok(option_dependencies)
             }
             _ => unreachable!(),
         }
@@ -187,5 +230,95 @@ mod typing_when {
                 &mut errors,
             )
             .unwrap_err();
+    }
+}
+
+#[cfg(test)]
+mod get_dependencies_when {
+    use crate::ast::{constant::Constant, location::Location, stream_expression::StreamExpression};
+    use std::collections::HashMap;
+
+    #[test]
+    fn should_get_dependencies_of_when_expressions_with_duplicates() {
+        let nodes_context = HashMap::new();
+        let mut nodes_graphs = HashMap::new();
+        let mut nodes_reduced_graphs = HashMap::new();
+        let mut errors = vec![];
+
+        let stream_expression = StreamExpression::When {
+            id: String::from("x"),
+            option: Box::new(StreamExpression::SignalCall {
+                id: String::from("x"),
+                typing: None,
+                location: Location::default(),
+            }),
+            present: Box::new(StreamExpression::Constant {
+                constant: Constant::Integer(0),
+                typing: None,
+                location: Location::default(),
+            }),
+            default: Box::new(StreamExpression::Constant {
+                constant: Constant::Integer(1),
+                typing: None,
+                location: Location::default(),
+            }),
+            typing: None,
+            location: Location::default(),
+        };
+
+        let dependencies = stream_expression
+            .get_dependencies_when(
+                &nodes_context,
+                &mut nodes_graphs,
+                &mut nodes_reduced_graphs,
+                &mut errors,
+            )
+            .unwrap();
+
+        let control = vec![(String::from("x"), 0)];
+
+        assert_eq!(dependencies, control)
+    }
+
+    #[test]
+    fn should_get_dependencies_of_when_expressions_without_local_signal() {
+        let nodes_context = HashMap::new();
+        let mut nodes_graphs = HashMap::new();
+        let mut nodes_reduced_graphs = HashMap::new();
+        let mut errors = vec![];
+
+        let stream_expression = StreamExpression::When {
+            id: String::from("x"),
+            option: Box::new(StreamExpression::SignalCall {
+                id: String::from("y"),
+                typing: None,
+                location: Location::default(),
+            }),
+            present: Box::new(StreamExpression::SignalCall {
+                id: String::from("x"),
+                typing: None,
+                location: Location::default(),
+            }),
+            default: Box::new(StreamExpression::Constant {
+                constant: Constant::Integer(1),
+                typing: None,
+                location: Location::default(),
+            }),
+            typing: None,
+            location: Location::default(),
+        };
+
+        let dependencies = stream_expression
+            .get_dependencies_when(
+                &nodes_context,
+                &mut nodes_graphs,
+                &mut nodes_reduced_graphs,
+                &mut errors,
+            )
+            .unwrap();
+
+        let control = vec![(String::from("y"), 0)];
+
+        assert_eq!(dependencies, control)
     }
 }
