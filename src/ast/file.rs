0247729bf4dcef73f91141688fa1047c39f0b95d
emilie.e.thome <emilie.e.thome@renault.com>
2023-06-19 11:35:51 +0200

    feat: ast files into ir files

--- src/ast/file.rs
+++ src/ast/file.rs
@@ -5,6 +5,7 @@ use crate::common::{
     context::Context, location::Location, type_system::Type, user_defined_type::UserDefinedType,
 };
 use crate::error::Error;
+use crate::ir::file::File as IRFile;
 
 #[derive(Debug, PartialEq)]
 /// A LanGRust [File] is composed of functions nodes,
@@ -299,6 +300,28 @@ impl File {
             .collect::<Vec<Result<(), ()>>>()
             .into_iter()
             .collect::<Result<(), ()>>()
+    }
+
+    /// Transform AST files into IR files.
+    pub fn into_ir(self) -> IRFile {
+        let File {
+            user_defined_types,
+            functions,
+            nodes,
+            component,
+            location,
+        } = self;
+
+        IRFile {
+            user_defined_types,
+            functions: functions
+                .into_iter()
+                .map(|function| function.into_ir())
+                .collect(),
+            nodes: nodes.into_iter().map(|node| node.into_ir()).collect(),
+            component: component.map(|component| component.into_ir()),
+            location,
+        }
     }
 }
 
