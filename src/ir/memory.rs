4a39414a80cee9d9db96a7b402b8b3f280a426ee
Emilie Thome <emilie.thome@hotmail.fr>
2023-06-20 08:43:40 +0200

    feat: memory adders

--- src/ir/memory.rs
+++ src/ir/memory.rs
@@ -15,7 +15,7 @@ pub struct Memory {
 ///
 /// Buffer initialized by a constant.
 pub struct Buffer {
-    id: String,
+    memory_id: String,
     typing: Type,
     initial_value: Constant,
 }
@@ -25,6 +25,27 @@ pub struct Buffer {
 /// Unitary node's name is composed of the name of the mother
 /// node and the name of the called output signal.
 pub struct CalledNode {
+    memory_id: String,
     node_id: String,
     signal_id: String,
+}
+
+impl Memory {
+    /// Add an initialized buffer to memory.
+    pub fn add_buffer(&mut self, memory_id: String, initial_value: Constant) {
+        self.buffers.push(Buffer {
+            memory_id,
+            typing: initial_value.get_type(),
+            initial_value,
+        })
+    }
+
+    /// Add called node to memory.
+    pub fn add_called_node(&mut self, memory_id: String, node_id: String, signal_id: String) {
+        self.called_nodes.push(CalledNode {
+            memory_id,
+            node_id,
+            signal_id,
+        })
+    }
 }
