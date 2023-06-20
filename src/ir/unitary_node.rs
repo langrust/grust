0bce505fd05d603550c80200a99546653f625ce5
emilie.e.thome <emilie.e.thome@renault.com>
2023-06-20 10:32:01 +0200

    feat: memorize unitary nodes

--- src/ir/unitary_node.rs
+++ src/ir/unitary_node.rs
@@ -48,7 +48,7 @@ impl UnitaryNode {
     /// This example is tested in the following code.
     ///
     /// ```rust
-    /// use std::collections::{HashSet, HashMap};
+    /// use std::collections::HashMap;
     ///
     /// use grustine::common::{constant::Constant, location::Location, scope::Scope, type_system::Type};
     /// use grustine::ir::{
@@ -230,5 +230,274 @@ impl UnitaryNode {
                 equation.normalize(&mut identifier_creator, unitary_nodes_used_inputs)
             })
             .collect();
+    }
+
+    /// Create memory for IR unitary nodes.
+    ///
+    /// Store buffer for followed by expressions and unitary node applications.
+    /// Transform followed by expressions in signal call.
+    ///
+    /// # Example
+    ///
+    /// ```GR
+    /// node test(s: int, v: int) {
+    ///     x_1: int = 0 fby v;
+    ///     x_2: int = my_node(s, x_1).o;
+    ///     out x: int = 1 + x_2;
+    /// }
+    /// ```
+    ///
+    /// The above example becomes:
+    ///
+    /// ```GR
+    /// node test(s: int, v: int) {
+    ///     x_1: int = mem;
+    ///     x_2: int = my_node(s, x_1).o;
+    ///     out x: int = 1 + x_2;
+    /// }
+    /// memory test {
+    ///     buffers: {
+    ///         mem: int = 0 fby v;
+    ///     },
+    ///     called_nodes: {
+    ///         memmy_nodeo: (my_node, o);
+    ///     },
+    /// }
+    /// ```
+    ///
+    /// This example is tested in source.
+    pub fn memorize(&mut self) {
+        let mut identifier_creator = IdentifierCreator::new(self);
+        let mut memory = Memory::new();
+
+        self.scheduled_equations
+            .iter_mut()
+            .for_each(|equation| equation.memorize(&mut identifier_creator, &mut memory));
+
+        self.memory = memory;
+    }
+}
+
+#[cfg(test)]
+mod memorize {
+    use crate::common::{constant::Constant, location::Location, scope::Scope, type_system::Type};
+    use crate::ir::{
+        equation::Equation, expression::Expression, memory::Memory,
+        stream_expression::StreamExpression, unitary_node::UnitaryNode,
+    };
+
+    #[test]
+    fn should_memorize_followed_by() {
+        let equation = Equation {
+            scope: Scope::Output,
+            id: String::from("x"),
+            signal_type: Type::Integer,
+            expression: StreamExpression::MapApplication {
+                function_expression: Expression::Call {
+                    id: String::from("+"),
+                    typing: Type::Abstract(
+                        vec![Type::Integer, Type::Integer],
+                        Box::new(Type::Integer),
+                    ),
+                    location: Location::default(),
+                },
+                inputs: vec![
+                    StreamExpression::SignalCall {
+                        id: String::from("s"),
+                        typing: Type::Integer,
+                        location: Location::default(),
+                    },
+                    StreamExpression::FollowedBy {
+                        constant: Constant::Integer(0),
+                        expression: Box::new(StreamExpression::SignalCall {
+                            id: String::from("v"),
+                            typing: Type::Integer,
+                            location: Location::default(),
+                        }),
+                        typing: Type::Integer,
+                        location: Location::default(),
+                    },
+                ],
+                typing: Type::Integer,
+                location: Location::default(),
+            },
+            location: Location::default(),
+        };
+        let mut unitary_node = UnitaryNode {
+            node_id: String::from("test"),
+            output_id: String::from("x"),
+            inputs: vec![
+                (String::from("s"), Type::Integer),
+                (String::from("v"), Type::Integer),
+            ],
+            scheduled_equations: vec![equation],
+            memory: Memory::new(),
+            location: Location::default(),
+        };
+        unitary_node.memorize();
+
+        let equation = Equation {
+            scope: Scope::Output,
+            id: String::from("x"),
+            signal_type: Type::Integer,
+            expression: StreamExpression::MapApplication {
+                function_expression: Expression::Call {
+                    id: String::from("+"),
+                    typing: Type::Abstract(
+                        vec![Type::Integer, Type::Integer],
+                        Box::new(Type::Integer),
+                    ),
+                    location: Location::default(),
+                },
+                inputs: vec![
+                    StreamExpression::SignalCall {
+                        id: String::from("s"),
+                        typing: Type::Integer,
+                        location: Location::default(),
+                    },
+                    StreamExpression::SignalCall {
+                        id: String::from("mem"),
+                        typing: Type::Integer,
+                        location: Location::default(),
+                    },
+                ],
+                typing: Type::Integer,
+                location: Location::default(),
+            },
+            location: Location::default(),
+        };
+        let mut memory = Memory::new();
+        memory.add_buffer(
+            String::from("mem"),
+            Constant::Integer(0),
+            StreamExpression::SignalCall {
+                id: String::from("v"),
+                typing: Type::Integer,
+                location: Location::default(),
+            },
+        );
+        let control = UnitaryNode {
+            node_id: String::from("test"),
+            output_id: String::from("x"),
+            inputs: vec![
+                (String::from("s"), Type::Integer),
+                (String::from("v"), Type::Integer),
+            ],
+            scheduled_equations: vec![equation],
+            memory,
+            location: Location::default(),
+        };
+        assert_eq!(unitary_node, control);
+    }
+
+    #[test]
+    fn should_memorize_node_expression() {
+        let equations = vec![
+            Equation {
+                scope: Scope::Local,
+                id: String::from("x_1"),
+                signal_type: Type::Integer,
+                expression: StreamExpression::MapApplication {
+                    function_expression: Expression::Call {
+                        id: String::from("*2"),
+                        typing: Type::Abstract(vec![Type::Integer], Box::new(Type::Integer)),
+                        location: Location::default(),
+                    },
+                    inputs: vec![StreamExpression::SignalCall {
+                        id: String::from("v"),
+                        typing: Type::Integer,
+                        location: Location::default(),
+                    }],
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+                location: Location::default(),
+            },
+            Equation {
+                scope: Scope::Local,
+                id: String::from("x_2"),
+                signal_type: Type::Integer,
+                expression: StreamExpression::UnitaryNodeApplication {
+                    node: String::from("my_node"),
+                    inputs: vec![
+                        StreamExpression::SignalCall {
+                            id: String::from("s"),
+                            typing: Type::Integer,
+                            location: Location::default(),
+                        },
+                        StreamExpression::SignalCall {
+                            id: String::from("x_1"),
+                            typing: Type::Integer,
+                            location: Location::default(),
+                        },
+                    ],
+                    signal: String::from("o"),
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+                location: Location::default(),
+            },
+            Equation {
+                scope: Scope::Output,
+                id: String::from("x"),
+                signal_type: Type::Integer,
+                expression: StreamExpression::MapApplication {
+                    function_expression: Expression::Call {
+                        id: String::from("+"),
+                        typing: Type::Abstract(
+                            vec![Type::Integer, Type::Integer],
+                            Box::new(Type::Integer),
+                        ),
+                        location: Location::default(),
+                    },
+                    inputs: vec![
+                        StreamExpression::Constant {
+                            constant: Constant::Integer(1),
+                            typing: Type::Integer,
+                            location: Location::default(),
+                        },
+                        StreamExpression::SignalCall {
+                            id: String::from("x_2"),
+                            typing: Type::Integer,
+                            location: Location::default(),
+                        },
+                    ],
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+                location: Location::default(),
+            },
+        ];
+        let mut unitary_node = UnitaryNode {
+            node_id: String::from("test"),
+            output_id: String::from("x"),
+            inputs: vec![
+                (String::from("s"), Type::Integer),
+                (String::from("v"), Type::Integer),
+            ],
+            scheduled_equations: equations.clone(),
+            memory: Memory::new(),
+            location: Location::default(),
+        };
+        unitary_node.memorize();
+
+        let mut memory = Memory::new();
+        memory.add_called_node(
+            String::from("memmy_nodeo"),
+            String::from("my_node"),
+            String::from("o"),
+        );
+        let control = UnitaryNode {
+            node_id: String::from("test"),
+            output_id: String::from("x"),
+            inputs: vec![
+                (String::from("s"), Type::Integer),
+                (String::from("v"), Type::Integer),
+            ],
+            scheduled_equations: equations,
+            memory,
+            location: Location::default(),
+        };
+        assert_eq!(unitary_node, control);
     }
 }
