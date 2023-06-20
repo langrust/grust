a100ca77aceafe6fcc94ec427de3fc339bbb2e96
Emilie Thome <emilie.thome@hotmail.fr>
2023-06-20 10:31:49 +0200

    feat: memorize expression

--- src/ir/stream_expression/mod.rs
+++ src/ir/stream_expression/mod.rs
@@ -7,7 +7,8 @@ use crate::common::{
 };
 use crate::error::Error;
 use crate::ir::{
-    equation::Equation, expression::Expression, identifier_creator::IdentifierCreator, node::Node,
+    equation::Equation, expression::Expression, identifier_creator::IdentifierCreator,
+    memory::Memory, node::Node,
 };
 
 mod array;
@@ -834,6 +835,95 @@ impl StreamExpression {
                 equations.push(new_equation);
                 equations
             }
+        }
+    }
+
+    /// Increment memory with expression.
+    ///
+    /// Store buffer for followed by expressions and unitary node applications.
+    /// Transform followed by expressions in signal call.
+    ///
+    /// # Example
+    ///
+    /// An expression `0 fby v` increments memory with the buffer
+    /// `mem: int = 0 fby v;` and becomes a call to `mem`.
+    ///
+    /// An expression `my_node(s, x_1).o;` increments memory with the
+    /// node call `memmy_nodeo: (my_node, o);` and is unchanged.
+    ///
+    /// Examples are tested in source.
+    pub fn memorize(&mut self, identifier_creator: &mut IdentifierCreator, memory: &mut Memory) {
+        match self {
+            StreamExpression::FollowedBy {
+                constant,
+                expression,
+                typing,
+                location,
+            } => {
+                let memory_id = identifier_creator.new_identifier(
+                    String::from("mem"),
+                    String::from(""),
+                    String::from(""),
+                );
+                memory.add_buffer(memory_id.clone(), constant.clone(), *expression.clone());
+                *self = StreamExpression::SignalCall {
+                    id: memory_id,
+                    typing: typing.clone(),
+                    location: location.clone(),
+                }
+            }
+            StreamExpression::MapApplication { inputs, .. } => inputs
+                .iter_mut()
+                .for_each(|expression| expression.memorize(identifier_creator, memory)),
+            StreamExpression::UnitaryNodeApplication { node, signal, .. } => {
+                let memory_id = identifier_creator.new_identifier(
+                    String::from("mem"),
+                    node.clone(),
+                    signal.clone(),
+                );
+                memory.add_called_node(memory_id, node.clone(), signal.clone())
+            }
+            StreamExpression::Structure { fields, .. } => fields
+                .iter_mut()
+                .for_each(|(_, expression)| expression.memorize(identifier_creator, memory)),
+            StreamExpression::Array { elements, .. } => elements
+                .iter_mut()
+                .for_each(|expression| expression.memorize(identifier_creator, memory)),
+            StreamExpression::Match {
+                expression, arms, ..
+            } => {
+                expression.memorize(identifier_creator, memory);
+                arms.iter_mut()
+                    .for_each(|(_, bound_expression, equations, expression)| {
+                        bound_expression
+                            .as_mut()
+                            .map(|expression| expression.memorize(identifier_creator, memory));
+                        equations
+                            .iter_mut()
+                            .for_each(|equation| equation.memorize(identifier_creator, memory));
+                        expression.memorize(identifier_creator, memory)
+                    })
+            }
+            StreamExpression::When {
+                option,
+                present_body,
+                present,
+                default_body,
+                default,
+                ..
+            } => {
+                option.memorize(identifier_creator, memory);
+                present_body
+                    .iter_mut()
+                    .for_each(|equation| equation.memorize(identifier_creator, memory));
+                present.memorize(identifier_creator, memory);
+                default_body
+                    .iter_mut()
+                    .for_each(|equation| equation.memorize(identifier_creator, memory));
+                default.memorize(identifier_creator, memory)
+            }
+            StreamExpression::NodeApplication { .. } => unreachable!(),
+            _ => (),
         }
     }
 }
@@ -1716,5 +1806,143 @@ mod normalize {
             location: Location::default(),
         };
         assert_eq!(expression, control)
+    }
+}
+
+#[cfg(test)]
+mod memorize {
+    use std::collections::HashSet;
+
+    use crate::common::{constant::Constant, location::Location, type_system::Type};
+    use crate::ir::{
+        expression::Expression, identifier_creator::IdentifierCreator, memory::Memory,
+        stream_expression::StreamExpression,
+    };
+
+    #[test]
+    fn should_memorize_followed_by() {
+        let mut identifier_creator = IdentifierCreator {
+            signals: HashSet::from([String::from("x"), String::from("s"), String::from("v")]),
+        };
+        let mut memory = Memory::new();
+
+        let mut expression = StreamExpression::MapApplication {
+            function_expression: Expression::Call {
+                id: String::from("+"),
+                typing: Type::Abstract(vec![Type::Integer, Type::Integer], Box::new(Type::Integer)),
+                location: Location::default(),
+            },
+            inputs: vec![
+                StreamExpression::SignalCall {
+                    id: String::from("s"),
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+                StreamExpression::FollowedBy {
+                    constant: Constant::Integer(0),
+                    expression: Box::new(StreamExpression::SignalCall {
+                        id: String::from("v"),
+                        typing: Type::Integer,
+                        location: Location::default(),
+                    }),
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+            ],
+            typing: Type::Integer,
+            location: Location::default(),
+        };
+        expression.memorize(&mut identifier_creator, &mut memory);
+
+        let mut control = Memory::new();
+        control.add_buffer(
+            String::from("mem"),
+            Constant::Integer(0),
+            StreamExpression::SignalCall {
+                id: String::from("v"),
+                typing: Type::Integer,
+                location: Location::default(),
+            },
+        );
+        assert_eq!(memory, control);
+
+        let control = StreamExpression::MapApplication {
+            function_expression: Expression::Call {
+                id: String::from("+"),
+                typing: Type::Abstract(vec![Type::Integer, Type::Integer], Box::new(Type::Integer)),
+                location: Location::default(),
+            },
+            inputs: vec![
+                StreamExpression::SignalCall {
+                    id: String::from("s"),
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+                StreamExpression::SignalCall {
+                    id: String::from("mem"),
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+            ],
+            typing: Type::Integer,
+            location: Location::default(),
+        };
+        assert_eq!(expression, control);
+    }
+
+    #[test]
+    fn should_memorize_node_expression() {
+        let mut identifier_creator = IdentifierCreator {
+            signals: HashSet::from([String::from("x"), String::from("s"), String::from("v")]),
+        };
+        let mut memory = Memory::new();
+
+        let mut expression = StreamExpression::UnitaryNodeApplication {
+            node: String::from("my_node"),
+            inputs: vec![
+                StreamExpression::SignalCall {
+                    id: String::from("s"),
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+                StreamExpression::SignalCall {
+                    id: String::from("x_1"),
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+            ],
+            signal: String::from("o"),
+            typing: Type::Integer,
+            location: Location::default(),
+        };
+        expression.memorize(&mut identifier_creator, &mut memory);
+
+        let mut control = Memory::new();
+        control.add_called_node(
+            String::from("memmy_nodeo"),
+            String::from("my_node"),
+            String::from("o"),
+        );
+        assert_eq!(memory, control);
+
+        let control = StreamExpression::UnitaryNodeApplication {
+            node: String::from("my_node"),
+            inputs: vec![
+                StreamExpression::SignalCall {
+                    id: String::from("s"),
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+                StreamExpression::SignalCall {
+                    id: String::from("x_1"),
+                    typing: Type::Integer,
+                    location: Location::default(),
+                },
+            ],
+            signal: String::from("o"),
+            typing: Type::Integer,
+            location: Location::default(),
+        };
+        assert_eq!(expression, control);
     }
 }
