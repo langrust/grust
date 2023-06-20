d6ea9ed2c1d8d537ff106cd12cd62f90805ab985
emilie.e.thome <emilie.e.thome@renault.com>
2023-06-20 10:31:07 +0200

    test: add unitary tests

--- src/ir/memory.rs
+++ src/ir/memory.rs
@@ -2,10 +2,12 @@ use std::collections::HashMap;
 
 use crate::common::{constant::Constant, type_system::Type};
 
+use crate::ir::stream_expression::StreamExpression;
+
 /// Memory of an unitary node.
 ///
 /// Memory structure for unitary node.
-/// It stores initialzed buffers and called unitary nodes' names.
+/// It stores initial_valuezed buffers and called unitary nodes' names.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Memory {
     /// Initialized buffers.
@@ -16,11 +18,12 @@ pub struct Memory {
 
 /// Initialized buffer.
 ///
-/// Buffer initialized by a constant.
+/// Buffer initial_valueized by a constant.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Buffer {
     typing: Type,
-    initial: Constant,
+    initial_value: Constant,
+    expression: StreamExpression,
 }
 
 /// Called unitary node' name.
@@ -35,6 +38,16 @@ pub struct CalledNode {
 
 impl Memory {
     /// Create empty memory.
+    ///
+    /// ```rust
+    /// use std::collections::HashMap;
+    ///
+    /// use grustine::ir::memory::Memory;
+    ///
+    /// let memory = Memory::new();
+    /// assert!(memory.buffers.is_empty());
+    /// assert!(memory.called_nodes.is_empty());
+    /// ```
     pub fn new() -> Self {
         Memory {
             buffers: HashMap::new(),
@@ -42,16 +55,69 @@ impl Memory {
         }
     }
 
-    /// Add an initialized buffer to memory.
-    pub fn add_buffer(&mut self, memory_id: String, initial: Constant) {
-        let typing = initial.get_type();
+    /// Add an initial_valueized buffer to memory.
+    ///
+    /// ```rust
+    /// use std::collections::HashMap;
+    ///
+    /// use grustine::common::{constant::Constant, type_system::Type, location::Location};
+    /// use grustine::ir::{stream_expression::StreamExpression, memory::Memory};
+    ///
+    /// let mut memory = Memory::new();
+    ///
+    /// memory.add_buffer(
+    ///     String::from("toto"),
+    ///     Constant::Integer(0),
+    ///     StreamExpression::SignalCall {
+    ///         id: String::from("x"),
+    ///         typing: Type::Integer,
+    ///         location: Location::default(),
+    ///     });
+    ///
+    /// assert!(!memory.buffers.is_empty());
+    /// assert!(memory.buffers.contains_key(&String::from("toto")));
+    /// assert!(memory.called_nodes.is_empty());
+    /// ```
+    pub fn add_buffer(
+        &mut self,
+        memory_id: String,
+        initial_value: Constant,
+        expression: StreamExpression,
+    ) {
+        let typing = initial_value.get_type();
         assert!(self
             .buffers
-            .insert(memory_id, Buffer { typing, initial })
+            .insert(
+                memory_id,
+                Buffer {
+                    typing,
+                    initial_value,
+                    expression
+                }
+            )
             .is_none())
     }
 
     /// Add called node to memory.
+    ///
+    /// ```rust
+    /// use std::collections::HashMap;
+    ///
+    /// use grustine::common::{constant::Constant, type_system::Type, location::Location};
+    /// use grustine::ir::{stream_expression::StreamExpression, memory::Memory};
+    ///
+    /// let mut memory = Memory::new();
+    ///
+    /// memory.add_called_node(
+    ///     String::from("toto"),
+    ///     String::from("toto_node"),
+    ///     String::from("toto_signal")
+    /// );
+    ///
+    /// assert!(!memory.called_nodes.is_empty());
+    /// assert!(memory.called_nodes.contains_key(&String::from("toto")));
+    /// assert!(memory.buffers.is_empty());
+    /// ```
     pub fn add_called_node(&mut self, memory_id: String, node_id: String, signal_id: String) {
         assert!(self
             .called_nodes
