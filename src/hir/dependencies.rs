f3ae453dba1bc9165065e6d7e5b62dd263ba88ba
Emilie Thome <emilie.e.thome@renault.com>
2023-07-21 09:43:58 +0200

    feat: add dependencies module to hir

---
+++ src/hir/dependencies.rs
@@ -0,0 +1,74 @@
+use once_cell::sync::OnceCell;
+
+/// Dependencies structure.
+/// 
+/// Dependencies are stored in a OnceCell.
+/// This allows to set dependencies after creating the the structure.
+/// After setting the value, the dependencies are immutable.
+#[derive(Debug, PartialEq, Clone)]
+pub struct Dependencies(OnceCell<Vec<(String, usize)>>);
+impl Dependencies {
+    /// Create unset dependencies.
+    ///
+    /// ```rust
+    /// use grustine::hir::dependencies::Dependencies;
+    ///
+    /// let dependencies = Dependencies::new();
+    /// assert!(dependencies.get().is_none());
+    /// ```
+    pub fn new() -> Self {
+        Dependencies(OnceCell::new())
+    }
+
+    /// Create dependencies according to input.
+    ///
+    /// ```rust
+    /// use grustine::hir::dependencies::Dependencies;
+    ///
+    /// let v = vec![(String::from("x"), 0)];
+    /// let dependencies = Dependencies::from(v.clone());
+    /// assert_eq!(*dependencies.get().unwrap(), v);
+    /// ```
+    pub fn from(v: Vec<(String, usize)>) -> Self {
+        let cell = OnceCell::new();
+        cell.set(v).unwrap();
+        Dependencies(cell)
+    }
+
+    /// Set dependencies according to input.
+    /// 
+    /// Terminate nicely only if it is the first time
+    /// setting the dependencies.
+    ///
+    /// ```rust
+    /// use grustine::hir::dependencies::Dependencies;
+    ///
+    /// let v = vec![(String::from("x"), 0)];
+    /// let dependencies = Dependencies::new();
+    /// dependencies.set(v.clone());
+    /// assert_eq!(*dependencies.get().unwrap(), v);
+    /// ```
+    pub fn set(&self, v: Vec<(String, usize)>) {
+        self.0.set(v).expect("should be the first time setting dependencies")
+    }
+
+    /// Get optional dependencies.
+    /// 
+    /// Get some dependencies if it as been previously setted.
+    /// Return `None` otherwise.
+    ///
+    /// ```rust
+    /// use grustine::hir::dependencies::Dependencies;
+    ///
+    /// let v = vec![(String::from("x"), 0)];
+    /// let dependencies = Dependencies::new();
+    /// dependencies.set(v.clone());
+    /// assert_eq!(*dependencies.get().unwrap(), v);
+    /// 
+    /// let dependencies = Dependencies::new();
+    /// assert!(dependencies.get().is_none());
+    /// ```
+    pub fn get(&self) -> Option<&Vec<(String, usize)>> {
+        self.0.get()
+    }
+}
