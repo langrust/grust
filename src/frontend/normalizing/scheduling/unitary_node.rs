c0da80ad6cb1d3c7fc2f5e709209d8e6c7a99953
emilie.e.thome <emilie.e.thome@renault.com>
2023-07-26 09:22:57 +0200

    docs: add more documentation to the scheduling function

--- src/frontend/normalizing/scheduling/unitary_node.rs
+++ src/frontend/normalizing/scheduling/unitary_node.rs
@@ -2,6 +2,28 @@ use crate::{hir::unitary_node::UnitaryNode, common::graph::{Graph, color::Color}
 
 impl UnitaryNode {
     /// Schedule equations.
+    /// 
+    /// # Example.
+    /// 
+    /// ```GR
+    /// node test(v: int) {
+    ///     out y: int = x-1
+    ///     o_1: int = 0 fby x
+    ///     x: int = v*2 + o_1
+    /// }
+    /// ```
+    /// 
+    /// In the node above, signal `y` depends on the current value of `x`,
+    /// `o_1` depends on the memory of `x` and `x` depends on `v` and `o_1`.
+    /// The node is causal and should be scheduled as bellow:
+    /// 
+    /// ```GR
+    /// node test(v: int) {
+    ///     o_1: int = 0 fby x  // depends on no current values of signals
+    ///     x: int = v*2 + o_1  // depends on the computed value of `o_1` and given `v`
+    ///     out y: int = x-1    // depends on the computed value of `x`
+    /// }
+    /// ```
     pub fn schedule(
         &mut self,
         graph: &Graph<Color>,
