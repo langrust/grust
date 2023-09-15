50ccdecf1e725c52eb00c941332cb64b7d26cab2
emilie.e.thome <emilie.e.thome@renault.com>
2023-09-15 09:52:32 +0200

    feat: impl display for signature

--- src/lir/item/signature.rs
+++ src/lir/item/signature.rs
@@ -14,10 +14,40 @@ pub struct Signature {
     pub output: Type,
 }
 
+impl std::fmt::Display for Signature {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let visibility = if self.public_visibility { "pub " } else { "" };
+        let receiver = if let Some(receiver) = &self.receiver {
+            format!("{receiver}, ")
+        } else {
+            "".to_string()
+        };
+        let inputs = self
+            .inputs
+            .iter()
+            .map(|(id, r#type)| format!("{id}: {}", r#type))
+            .collect::<Vec<_>>()
+            .join(", ");
+        write!(
+            f,
+            "{}fn {}({}{}) -> {}",
+            visibility, self.name, receiver, inputs, self.output
+        )
+    }
+}
+
 /// The `self` argument of a method.
 pub struct Receiver {
     /// Reference: `true` is reference, `false` is owned.
     pub reference: bool,
     /// Mutability: `true` is mutable, `false` is immutable.
     pub mutable: bool,
+}
+
+impl std::fmt::Display for Receiver {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let reference = if self.reference { "&" } else { "" };
+        let mutable = if self.mutable { "mut " } else { "" };
+        write!(f, "{}{}self", reference, mutable)
+    }
 }
