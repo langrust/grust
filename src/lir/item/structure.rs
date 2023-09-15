46f6e281ec3b83f464897afdc76149972da246d4
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-15 10:08:34 +0200

    feat: impl display for structure

--- src/lir/item/structure.rs
+++ src/lir/item/structure.rs
@@ -10,6 +10,19 @@ pub struct Structure {
     pub fields: Vec<Field>,
 }
 
+impl std::fmt::Display for Structure {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let visibility = if self.public_visibility { "pub " } else { "" };
+        let fields = self
+            .fields
+            .iter()
+            .map(|field| format!("{field}"))
+            .collect::<Vec<_>>()
+            .join(", ");
+        write!(f, "{}struct {} {{{}}}", visibility, self.name, fields)
+    }
+}
+
 /// A structure's field.
 pub struct Field {
     /// Visibility: `true` is public, `false` is private.
@@ -18,4 +31,11 @@ pub struct Field {
     pub name: String,
     /// Type of the field.
     pub r#type: Type,
+}
+
+impl std::fmt::Display for Field {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let visibility = if self.public_visibility { "pub " } else { "" };
+        write!(f, "{}{}: {}", visibility, self.name, self.r#type)
+    }
 }
