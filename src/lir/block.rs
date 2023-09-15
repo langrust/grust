cc118a33b451229fa2cea13a9f915b30b7b3c514
emilie.e.thome <emilie.e.thome@renault.com>
2023-09-15 10:26:18 +0200

    feat: impl display for block

--- src/lir/block.rs
+++ src/lir/block.rs
@@ -5,3 +5,15 @@ pub struct Block {
     /// Statements of the block.
     pub statements: Vec<Statement>,
 }
+
+impl std::fmt::Display for Block {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let statements = self
+            .statements
+            .iter()
+            .map(|statement| format!("{statement}"))
+            .collect::<Vec<_>>()
+            .join(" ");
+        write!(f, "{{{statements}}}")
+    }
+}
