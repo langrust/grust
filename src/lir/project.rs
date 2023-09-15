56a5f049953454ee8fb30337d90fb6e83503ca05
Emilie THOME <emilie.e.thome@renault.com>
2023-09-15 17:23:15 +0200

    feat: generate rust project from LIR construct

--- src/lir/project.rs
+++ src/lir/project.rs
@@ -5,3 +5,11 @@ pub struct Project {
     /// All the files contained by the project.
     pub files: Vec<File>,
 }
+impl Project {
+    /// Generate Rust project.
+    pub fn generate(&self) {
+        for file in &self.files {
+            file.generate()
+        }
+    }
+}
