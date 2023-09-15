233022fa5a6cff21527f9679f2dc581eedb71ed9
emilie.e.thome <emilie.e.thome@renault.com>
2023-09-15 10:49:37 +0200

    feat: add literal expression

--- src/lir/expression.rs
+++ src/lir/expression.rs
@@ -1,10 +1,18 @@
+use crate::common::constant::Constant;
+
 /// Rust expressions.
 pub enum Expression {
-    // TODO
+    /// A literal expression: `1` or `"hello world"`.
+    Literal {
+        /// The literal.
+        literal: Constant,
+    },
 }
 
 impl std::fmt::Display for Expression {
-    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expression::Literal { literal } => write!(f, "{literal}"),
+        }
     }
 }
