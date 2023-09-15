1647fb7399e5729a50bddf7ed0b29675272d3a69
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-15 11:45:26 +0200

    feat: add identifier call expression

--- src/lir/expression.rs
+++ src/lir/expression.rs
@@ -7,12 +7,18 @@ pub enum Expression {
         /// The literal.
         literal: Constant,
     },
+    /// An identifier call: `x`.
+    Identifier {
+        /// The identifier.
+        identifier: String,
+    },
 }
 
 impl std::fmt::Display for Expression {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Expression::Literal { literal } => write!(f, "{literal}"),
+            Expression::Identifier { identifier } => write!(f, "{identifier}"),
         }
     }
 }
