43c82eb1798256631c82a00bc474de453e9f62a3
emilie.e.thome <emilie.e.thome@renault.com>
2023-09-15 11:52:26 +0200

    feat: add structure expression

--- src/lir/expression.rs
+++ src/lir/expression.rs
@@ -12,6 +12,13 @@ pub enum Expression {
         /// The identifier.
         identifier: String,
     },
+    /// A structure literal expression: `Point { x: 1, y: 1 }`.
+    Structure {
+        /// The name of the structure.
+        name: String,
+        /// The filled fields.
+        fields: Vec<FieldExpression>,
+    },
 }
 
 impl std::fmt::Display for Expression {
@@ -19,6 +26,28 @@ impl std::fmt::Display for Expression {
         match self {
             Expression::Literal { literal } => write!(f, "{literal}"),
             Expression::Identifier { identifier } => write!(f, "{identifier}"),
+            Expression::Structure { name, fields } => {
+                let fields = fields
+                    .iter()
+                    .map(|field| format!("{field}"))
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                write!(f, "{} {{{}}}", name, fields)
+            }
         }
+    }
+}
+
+/// A structure's field filled with an expression.
+pub struct FieldExpression {
+    /// Name of the field.
+    pub name: String,
+    /// Expression stored in the field.
+    pub expression: Expression,
+}
+
+impl std::fmt::Display for FieldExpression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {}", self.name, self.expression)
     }
 }
