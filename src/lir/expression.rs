418511d625f8cffb6e9aecfac55fcdc3ef0d07f7
emilie.e.thome <emilie.e.thome@renault.com>
2023-09-15 10:19:52 +0200

    feat: pretend to impl display for expression

--- src/lir/expression.rs
+++ src/lir/expression.rs
@@ -2,3 +2,9 @@
 pub enum Expression {
     // TODO
 }
+
+impl std::fmt::Display for Expression {
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
