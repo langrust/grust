3d52e1679c139983c4e53c587acdd19c6cb62e96
emilie.e.thome <emilie.e.thome@renault.com>
2023-09-15 09:58:11 +0200

    feat: impl display for enumeration

--- src/lir/item/enumeration.rs
+++ src/lir/item/enumeration.rs
@@ -7,3 +7,11 @@ pub struct Enumeration {
     /// All the elements of the enumeration.
     pub elements: Vec<String>,
 }
+
+impl std::fmt::Display for Enumeration {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let visibility = if self.public_visibility { "pub " } else { "" };
+        let elements = self.elements.join(", ");
+        write!(f, "{}enum {} {{{}}}", visibility, self.name, elements)
+    }
+}
