4b5e4644901941b0882d6302c4aa69bc09418a06
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-18 09:03:42 +0200

    test: add test for code generation of enumeration

--- src/lir/item/enumeration.rs
+++ src/lir/item/enumeration.rs
@@ -12,6 +12,25 @@ impl std::fmt::Display for Enumeration {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let visibility = if self.public_visibility { "pub " } else { "" };
         let elements = self.elements.join(", ");
-        write!(f, "{}enum {} {{{}}}", visibility, self.name, elements)
+
+#[cfg(test)]
+mod fmt {
+    use super::Enumeration;
+
+    #[test]
+    fn should_format_enumeration_definition() {
+        let enumeration = Enumeration {
+            public_visibility: true,
+            name: String::from("Color"),
+            elements: vec![
+                String::from("Blue"),
+                String::from("Red"),
+                String::from("Yellow"),
+                String::from("Green"),
+                String::from("Purple"),
+            ],
+        };
+        let control = String::from("pub enum Color { Blue, Red, Yellow, Green, Purple }");
+        assert_eq!(format!("{}", enumeration), control)
     }
 }
