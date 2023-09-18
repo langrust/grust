fb91440f8f7a7678570c29fb4a1fe442ab2b12ca
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-18 10:13:41 +0200

    test: add test for code generation of block

--- src/lir/block.rs
+++ src/lir/block.rs
@@ -14,6 +14,53 @@ impl std::fmt::Display for Block {
             .map(|statement| format!("{statement}"))
             .collect::<Vec<_>>()
             .join(" ");
-        write!(f, "{{{statements}}}")
+        write!(f, "{{ {statements} }}")
+    }
+}
+
+#[cfg(test)]
+mod fmt {
+    use crate::{
+        common::{constant::Constant, operator::BinaryOperator},
+        lir::{
+            block::Block,
+            expression::Expression,
+            statement::{r#let::Let, Statement},
+        },
+    };
+
+    #[test]
+    fn should_format_block_expression() {
+        let block = Block {
+            statements: vec![
+                Statement::Let(Let {
+                    reference: false,
+                    mutable: true,
+                    identifiant: String::from("x"),
+                    expression: Expression::Literal {
+                        literal: Constant::Integer(1),
+                    },
+                }),
+                Statement::ExpressionIntern(Expression::Assignement {
+                    left: Box::new(Expression::Identifier {
+                        identifier: String::from("x"),
+                    }),
+                    right: Box::new(Expression::Binary {
+                        left: Box::new(Expression::Identifier {
+                            identifier: String::from("x"),
+                        }),
+                        operator: BinaryOperator::Add,
+                        right: Box::new(Expression::Literal {
+                            literal: Constant::Integer(1),
+                        }),
+                    }),
+                }),
+                Statement::ExpressionLast(Expression::Identifier {
+                    identifier: String::from("x"),
+                }),
+            ],
+        };
+        let control = String::from("{ let mut x = 1i64; x = x + 1i64; x }");
+        assert_eq!(format!("{}", block), control)
     }
 }
