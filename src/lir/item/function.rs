d2c82837b2aa0af8bbc0adfd780b538198b6a7d6
Emilie Thome <emilie.e.thome@renault.com>
2023-09-18 14:01:05 +0200

    test: add test for code generation of function

--- src/lir/item/function.rs
+++ src/lir/item/function.rs
@@ -13,3 +13,72 @@ impl std::fmt::Display for Function {
         write!(f, "{} {}", self.signature, self.body)
     }
 }
+
+#[cfg(test)]
+mod fmt {
+    use crate::{
+        common::{constant::Constant, operator::BinaryOperator, r#type::Type as DSLType},
+        lir::{
+            block::Block,
+            expression::Expression,
+            item::{function::Function, signature::Signature},
+            r#type::Type,
+            statement::{r#let::Let, Statement},
+        },
+    };
+
+    #[test]
+    fn should_format_function_definition() {
+        let function = Function {
+            signature: Signature {
+                public_visibility: true,
+                name: String::from("foo"),
+                receiver: None,
+                inputs: vec![
+                    (String::from("x"), Type::Owned(DSLType::Integer)),
+                    (String::from("y"), Type::Owned(DSLType::Integer)),
+                ],
+                output: Type::Owned(DSLType::Integer),
+            },
+            body: Block {
+                statements: vec![
+                    Statement::Let(Let {
+                        reference: false,
+                        mutable: true,
+                        identifiant: String::from("z"),
+                        expression: Expression::Binary {
+                            left: Box::new(Expression::Identifier {
+                                identifier: String::from("x"),
+                            }),
+                            operator: BinaryOperator::Add,
+                            right: Box::new(Expression::Identifier {
+                                identifier: String::from("y"),
+                            }),
+                        },
+                    }),
+                    Statement::ExpressionIntern(Expression::Assignement {
+                        left: Box::new(Expression::Identifier {
+                            identifier: String::from("z"),
+                        }),
+                        right: Box::new(Expression::Binary {
+                            left: Box::new(Expression::Identifier {
+                                identifier: String::from("z"),
+                            }),
+                            operator: BinaryOperator::Add,
+                            right: Box::new(Expression::Literal {
+                                literal: Constant::Integer(1),
+                            }),
+                        }),
+                    }),
+                    Statement::ExpressionLast(Expression::Identifier {
+                        identifier: String::from("z"),
+                    }),
+                ],
+            },
+        };
+        let control = String::from(
+            "pub fn foo(x: i64, y: i64) -> i64 { let mut z = x + y; z = z + 1i64; z }",
+        );
+        assert_eq!(format!("{}", function), control)
+    }
+}
