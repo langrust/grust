35e51daf152f2a4dd003df1734820273e311ec34
Emilie Thome <emilie.e.thome@renault.com>
2023-09-18 08:57:43 +0200

    test: add tests for code generation of types

--- src/lir/type.rs
+++ src/lir/type.rs
@@ -22,3 +22,37 @@ impl std::fmt::Display for Type {
         }
     }
 }
+
+#[cfg(test)]
+mod fmt {
+    use super::Type;
+    use crate::common::r#type::Type as DSLType;
+
+    #[test]
+    fn should_format_rust_owned_type() {
+        let r#type = Type::Owned(DSLType::Integer);
+        let control = String::from("i64");
+        assert_eq!(format!("{}", r#type), control)
+    }
+
+    #[test]
+    fn should_format_rust_mutable_type() {
+        let r#type = Type::Mutable(DSLType::Integer);
+        let control = String::from("mut i64");
+        assert_eq!(format!("{}", r#type), control)
+    }
+
+    #[test]
+    fn should_format_rust_reference_type() {
+        let r#type = Type::Reference(DSLType::Integer);
+        let control = String::from("&i64");
+        assert_eq!(format!("{}", r#type), control)
+    }
+
+    #[test]
+    fn should_format_rust_mutable_reference_type() {
+        let r#type = Type::MutableReference(DSLType::Integer);
+        let control = String::from("&mut i64");
+        assert_eq!(format!("{}", r#type), control)
+    }
+}
