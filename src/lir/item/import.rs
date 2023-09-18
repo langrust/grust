908a7b5b01f9467152b498ef7b0ff3b95de0f4fa
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-18 09:16:27 +0200

    test: add tests for code generation of imports

--- src/lir/item/import.rs
+++ src/lir/item/import.rs
@@ -86,3 +86,107 @@ impl std::fmt::Display for PathTree {
         }
     }
 }
+
+#[cfg(test)]
+mod fmt {
+    use crate::lir::item::import::{Import, PathTree};
+
+    #[test]
+    fn should_format_use_import_definition_with_name_path() {
+        let use_import = Import::Use {
+            public_visibility: true,
+            tree: PathTree::Path {
+                module_name: String::from("std"),
+                tree: Box::new(PathTree::Path {
+                    module_name: String::from("fmt"),
+                    tree: Box::new(PathTree::Name {
+                        name: String::from("Debug"),
+                        alias: None,
+                    }),
+                }),
+            },
+        };
+        let control = String::from("pub use std::fmt::Debug;");
+        assert_eq!(format!("{}", use_import), control)
+    }
+
+    #[test]
+    fn should_format_use_import_definition_with_alias_name_path() {
+        let use_import = Import::Use {
+            public_visibility: true,
+            tree: PathTree::Path {
+                module_name: String::from("std"),
+                tree: Box::new(PathTree::Path {
+                    module_name: String::from("future"),
+                    tree: Box::new(PathTree::Name {
+                        name: String::from("Future"),
+                        alias: Some(String::from("AliasFuture")),
+                    }),
+                }),
+            },
+        };
+        let control = String::from("pub use std::future::Future as AliasFuture;");
+        assert_eq!(format!("{}", use_import), control)
+    }
+
+    #[test]
+    fn should_format_use_import_definition_with_star_path() {
+        let use_import = Import::Use {
+            public_visibility: true,
+            tree: PathTree::Path {
+                module_name: String::from("std"),
+                tree: Box::new(PathTree::Path {
+                    module_name: String::from("sync"),
+                    tree: Box::new(PathTree::Star),
+                }),
+            },
+        };
+        let control = String::from("pub use std::sync::*;");
+        assert_eq!(format!("{}", use_import), control)
+    }
+
+    #[test]
+    fn should_format_use_import_definition_with_group_path() {
+        let use_import = Import::Use {
+            public_visibility: true,
+            tree: PathTree::Path {
+                module_name: String::from("std"),
+                tree: Box::new(PathTree::Group {
+                    trees: vec![
+                        PathTree::Path {
+                            module_name: String::from("sync"),
+                            tree: Box::new(PathTree::Star),
+                        },
+                        PathTree::Path {
+                            module_name: String::from("fmt"),
+                            tree: Box::new(PathTree::Name {
+                                name: String::from("Debug"),
+                                alias: None,
+                            }),
+                        },
+                        PathTree::Path {
+                            module_name: String::from("future"),
+                            tree: Box::new(PathTree::Name {
+                                name: String::from("Future"),
+                                alias: Some(String::from("AliasFuture")),
+                            }),
+                        },
+                    ],
+                }),
+            },
+        };
+        let control =
+            String::from("pub use std::{ sync::*, fmt::Debug, future::Future as AliasFuture };");
+        assert_eq!(format!("{}", use_import), control)
+    }
+
+    #[test]
+    fn should_format_module_import_definition() {
+        let mod_import = Import::Module {
+            public_visibility: true,
+            name: String::from("my_module"),
+        };
+        let control = String::from("pub mod my_module;");
+        assert_eq!(format!("{}", mod_import), control)
+    }
+}
