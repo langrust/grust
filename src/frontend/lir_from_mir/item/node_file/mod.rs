374520441485591d583ee50f983fb6b8af2a34fa
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-19 17:34:07 +0200

    feat: transform MIR node_file into LIR file

--- src/frontend/lir_from_mir/item/node_file/mod.rs
+++ src/frontend/lir_from_mir/item/node_file/mod.rs
@@ -1,6 +1,32 @@
+use crate::lir::file::File;
+use crate::lir::item::Item;
+use crate::mir::item::node_file::NodeFile;
+
+use self::import::lir_from_mir as import_lir_from_mir;
+use self::input::lir_from_mir as input_lir_from_mir;
+use self::state::lir_from_mir as state_lir_from_mir;
+
 /// LIR node and function import construction from MIR import.
 pub mod import;
 /// LIR input structure construction from MIR input.
 pub mod input;
 /// LIR state structure and implementation construction from MIR state.
 pub mod state;
+
+/// Transform MIR node_file into LIR file.
+pub fn lir_from_mir(node_file: NodeFile) -> File {
+    let mut items = node_file
+        .imports
+        .into_iter()
+        .map(|import| Item::Import(import_lir_from_mir(import)))
+        .collect::<Vec<_>>();
+    let input_structure = input_lir_from_mir(node_file.input);
+    let (state_structure, state_implementation) = state_lir_from_mir(node_file.state);
+    items.push(Item::Structure(input_structure));
+    items.push(Item::Structure(state_structure));
+    items.push(Item::Implementation(state_implementation));
+    File {
+        path: node_file.name,
+        items,
+    }
+}
