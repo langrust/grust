fcacac065a58f892489dd249c984a7084b2413c0
Emilie THOME <emilie.e.thome@renault.com>
2023-09-19 14:47:15 +0200

    feat: add project MIR

--- src/mir/mod.rs
+++ src/mir/mod.rs
@@ -4,5 +4,7 @@ pub mod block;
 pub mod expression;
 /// MIR [Item](crate::mir::item::Item) module.
 pub mod item;
+/// MIR [Project](crate::mir::project::Project) module.
+pub mod project;
 /// MIR [Statement](crate::mir::statement::Statement) module.
 pub mod statement;
