cd32591090580b151b13b25f23c45b1f4035c8f6
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-19 14:47:00 +0200

    feat: add item MIR

--- src/mir/item/mod.rs
+++ src/mir/item/mod.rs
@@ -1,3 +1,8 @@
+use self::{
+    array_alias::ArrayAlias, enumeration::Enumeration, function::Function, node_file::NodeFile,
+    structure::Structure,
+};
+
 /// MIR [ArrayAlias](crate::mir::item::array_alias::ArrayAlias) module.
 pub mod array_alias;
 /// MIR [Enumeration](crate::mir::item::enumeration::Enumeration) module.
@@ -8,3 +13,17 @@ pub mod function;
 pub mod node_file;
 /// MIR [Structure](crate::mir::item::structure::Structure) module.
 pub mod structure;
+
+/// An item of the project.
+pub enum Item {
+    /// A node file.
+    NodeFile(NodeFile),
+    /// A function definition.
+    Function(Function),
+    /// An enumeration definition.
+    Enumeration(Enumeration),
+    /// A structure definition.
+    Structure(Structure),
+    /// An array alias definition.
+    ArrayAlias(ArrayAlias),
+}
