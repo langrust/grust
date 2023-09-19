c02fcc44ea0f60cd42d9e74808559d0428c09b32
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-19 15:59:43 +0200

    feat: transform MIR statement into LIR statement

--- src/frontend/lir_from_mir/mod.rs
+++ src/frontend/lir_from_mir/mod.rs
@@ -1,4 +1,8 @@
+/// LIR expression construction from MIR expression.
+pub mod expression;
 /// LIR pattern construction from MIR pattern.
 pub mod pattern;
+/// LIR statement construction from MIR statement.
+pub mod statement;
 /// LIR type construction from MIR type.
 pub mod r#type;
