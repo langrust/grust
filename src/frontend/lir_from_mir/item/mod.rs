267c5fd49db79775ff3b6c8f2c9801ca571d165a
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-19 16:21:50 +0200

    feat: transform MIR array alias into LIR type alias

--- src/frontend/lir_from_mir/item/mod.rs
+++ src/frontend/lir_from_mir/item/mod.rs
@@ -1,3 +1,5 @@
+/// LIR array alias construction from MIR array alias.
+pub mod array_alias;
 /// LIR enumeration construction from MIR enumeration.
 pub mod enumeration;
 /// LIR structure construction from MIR structure.
