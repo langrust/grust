39191fadad7e35620c3361c2cb2e4e81157cfefe
Emilie THOME <emilie.e.thome@renault.com>
2023-09-19 17:26:01 +0200

    feat: transform MIR state into LIR structure and implementation

--- src/frontend/lir_from_mir/item/node_file/state/mod.rs
+++ src/frontend/lir_from_mir/item/node_file/state/mod.rs
@@ -1,4 +1,49 @@
+use crate::common::r#type::Type;
+use crate::frontend::lir_from_mir::item::node_file::state::init::lir_from_mir as init_lir_from_mir;
+use crate::frontend::lir_from_mir::item::node_file::state::step::lir_from_mir as step_lir_from_mir;
+use crate::frontend::lir_from_mir::r#type::lir_from_mir as type_lir_from_mir;
+use crate::lir::item::implementation::Implementation;
+use crate::lir::item::structure::{Field, Structure};
+use crate::lir::r#type::Type as LIRType;
+use crate::mir::item::node_file::state::{State, StateElement};
+
 /// LIR init method construction from MIR init.
 pub mod init;
 /// LIR step method construction from MIR step.
 pub mod step;
+
+/// Transform MIR state into LIR structure and implementation.
+pub fn lir_from_mir(state: State) -> (Structure, Implementation) {
+    let fields = state
+        .elements
+        .into_iter()
+        .map(|element| match element {
+            StateElement::Buffer { identifier, r#type } => Field {
+                public_visibility: false,
+                name: identifier,
+                r#type: type_lir_from_mir(r#type),
+            },
+            StateElement::CalledNode {
+                identifier,
+                node_name,
+            } => Field {
+                public_visibility: false,
+                name: identifier,
+                r#type: LIRType::Owned(Type::Structure(node_name + "State")),
+            },
+        })
+        .collect();
+    let structure = Structure {
+        public_visibility: false,
+        name: state.node_name.clone() + "State",
+        fields,
+    };
+
+    let implementation = Implementation {
+        trait_name: None,
+        type_name: state.node_name + "State",
+        items: vec![init_lir_from_mir(state.init), step_lir_from_mir(state.step)],
+    };
+
+    (structure, implementation)
+}
