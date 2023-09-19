43c4d19563b5c3aec3f4f381bf02c85fffb76e93
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-19 14:45:44 +0200

    feat: add node state MIR

---
+++ src/mir/item/node_file/state/mod.rs
@@ -0,0 +1,26 @@
+
+/// A node state structure.
+pub struct State {
+    /// The node's name.
+    pub node_name: String,
+    /// The state's elements.
+    pub elements: Vec<StateElement>,
+}
+
+/// A state element structure.
+pub enum StateElement {
+    /// A buffer.
+    Buffer {
+        /// The name of the buffer.
+        identifier: String,
+        /// The type of the buffer.
+        r#type: Type,
+    },
+    /// A called node memory.
+    CalledNode {
+        /// The name of the memory storage.
+        identifier: String,
+        /// The name of the called node.
+        node_name: String,
+    },
+}
