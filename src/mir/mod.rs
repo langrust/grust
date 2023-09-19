2e70e477c3af1bb1f00b3b8cd583c9ae9e47b6c4
Emilie THOME <emilie.e.thome@renault.com>
2023-09-19 14:42:29 +0200

    feat: add statement MIR

--- src/mir/mod.rs
+++ src/mir/mod.rs
@@ -2,3 +2,5 @@
 pub mod block;
 /// MIR [Expression](crate::mir::expression::Expression) module.
 pub mod expression;
+/// MIR [Statement](crate::mir::statement::Statement) module.
+pub mod statement;
