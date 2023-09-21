ec255274a264b6989456aad25adc3f613948af0d
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-21 12:19:55 +0200

    refactor: adapt lir type formatting

--- src/lir/type.rs
+++ src/lir/type.rs
@@ -1,5 +1,3 @@
-use crate::common::r#type::Type as DSLType;
-
 /// The four different kind of type in Rust.
 #[derive(Debug, PartialEq)]
 pub enum Type {
@@ -53,10 +51,54 @@ pub enum Type {
 impl std::fmt::Display for Type {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Type::Owned(r#type) => write!(f, "{}", r#type),
-            Type::Mutable(r#type) => write!(f, "mut {}", r#type),
-            Type::Reference(r#type) => write!(f, "&{}", r#type),
-            Type::MutableReference(r#type) => write!(f, "&mut {}", r#type),
+            Type::Identifier { identifier } => write!(f, "{}", identifier),
+            Type::Array { element, size } => write!(f, "[{}; {}]", *element, size),
+            Type::Function { arguments, output } => {
+                let arguments = arguments
+                    .iter()
+                    .map(|argument| format!("{argument}"))
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                match output.as_ref() {
+                    Type::Identifier { identifier } if identifier == &String::from("()") => {
+                        write!(f, "fn({})", arguments)
+                    }
+                    _ => write!(f, "fn({}) -> {}", arguments, *output),
+                }
+            }
+            Type::Closure { arguments, output } => {
+                let arguments = arguments
+                    .iter()
+                    .map(|argument| format!("{argument}"))
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                match output.as_ref() {
+                    Type::Identifier { identifier } if identifier == &String::from("()") => {
+                        write!(f, "impl Fn({})", arguments)
+                    }
+                    _ => write!(f, "impl Fn({}) -> {}", arguments, *output),
+                }
+            }
+            Type::Generic { generic, arguments } => {
+                let arguments = arguments
+                    .iter()
+                    .map(|argument| format!("{argument}"))
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                write!(f, "{}<{}>", generic, arguments)
+            }
+            Type::Reference { mutable, element } => {
+                let mutable = if *mutable { "mut " } else { "" };
+                write!(f, "&{}{}", mutable, *element)
+            }
+            Type::Tuple { elements } => {
+                let elements = elements
+                    .iter()
+                    .map(|element| format!("{element}"))
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                write!(f, "({})", elements)
+            }
         }
     }
 }
@@ -64,33 +106,90 @@ impl std::fmt::Display for Type {
 #[cfg(test)]
 mod fmt {
     use super::Type;
-    use crate::common::r#type::Type as DSLType;
 
     #[test]
-    fn should_format_rust_owned_type() {
-        let r#type = Type::Owned(DSLType::Integer);
+    fn should_format_rust_simple_type() {
+        let r#type = Type::Identifier {
+            identifier: String::from("i64"),
+        };
         let control = String::from("i64");
         assert_eq!(format!("{}", r#type), control)
     }
 
     #[test]
-    fn should_format_rust_mutable_type() {
-        let r#type = Type::Mutable(DSLType::Integer);
-        let control = String::from("mut i64");
+    fn should_format_rust_array_type() {
+        let r#type = Type::Array {
+            element: Box::new(Type::Identifier {
+                identifier: String::from("i64"),
+            }),
+            size: 5,
+        };
+        let control = String::from("[i64; 5]");
+        assert_eq!(format!("{}", r#type), control)
+    }
+
+    #[test]
+    fn should_format_rust_function_type() {
+        let r#type = Type::Function {
+            arguments: vec![Type::Identifier {
+                identifier: String::from("i64"),
+            }],
+            output: Box::new(Type::Identifier {
+                identifier: String::from("i64"),
+            }),
+        };
+        let control = String::from("fn(i64) -> i64");
+        assert_eq!(format!("{}", r#type), control)
+    }
+
+    #[test]
+    fn should_format_rust_closure_type() {
+        let r#type = Type::Closure {
+            arguments: vec![Type::Identifier {
+                identifier: String::from("i64"),
+            }],
+            output: Box::new(Type::Identifier {
+                identifier: String::from("()"),
+            }),
+        };
+        let control = String::from("impl Fn(i64)");
+        assert_eq!(format!("{}", r#type), control)
+    }
+
+    #[test]
+    fn should_format_rust_generic_type() {
+        let r#type = Type::Generic {
+            generic: Box::new(Type::Identifier {
+                identifier: String::from("Option"),
+            }),
+            arguments: vec![Type::Identifier {
+                identifier: String::from("i64"),
+            }],
+        };
+        let control = String::from("Option<i64>");
         assert_eq!(format!("{}", r#type), control)
     }
 
     #[test]
     fn should_format_rust_reference_type() {
-        let r#type = Type::Reference(DSLType::Integer);
-        let control = String::from("&i64");
+        let r#type = Type::Reference {
+            mutable: true,
+            element: Box::new(Type::Identifier {
+                identifier: String::from("i64"),
+            }),
+        };
+        let control = String::from("&mut i64");
         assert_eq!(format!("{}", r#type), control)
     }
 
     #[test]
-    fn should_format_rust_mutable_reference_type() {
-        let r#type = Type::MutableReference(DSLType::Integer);
-        let control = String::from("&mut i64");
+    fn should_format_rust_tuple_type() {
+        let r#type = Type::Tuple {
+            elements: vec![Type::Identifier {
+                identifier: String::from("i64"),
+            }],
+        };
+        let control = String::from("(i64)");
         assert_eq!(format!("{}", r#type), control)
     }
 }
