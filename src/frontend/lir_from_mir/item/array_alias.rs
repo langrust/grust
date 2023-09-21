f3e5eee50959aacc059baf72fd83edcd385b8da8
Emilie THOME <emilie.e.thome@renault.com>
2023-09-21 18:04:39 +0200

    test: add test for MIR array alias transformation into LIR

--- src/frontend/lir_from_mir/item/array_alias.rs
+++ src/frontend/lir_from_mir/item/array_alias.rs
@@ -14,3 +14,35 @@ pub fn lir_from_mir(array_alias: ArrayAlias) -> TypeAlias {
         },
     }
 }
+
+#[cfg(test)]
+mod lir_from_mir {
+    use crate::common::r#type::Type;
+    use crate::frontend::lir_from_mir::item::array_alias::lir_from_mir;
+    use crate::lir::item::type_alias::TypeAlias;
+    use crate::lir::r#type::Type as LIRType;
+    use crate::mir::item::array_alias::ArrayAlias;
+
+    #[test]
+    fn should_create_lir_type_alias_from_mir_array_alias() {
+        let array_alias = ArrayAlias {
+            name: String::from("Matrix5x5"),
+            array_type: Type::Array(Box::new(Type::Integer), 5),
+            size: 5,
+        };
+        let control = TypeAlias {
+            public_visibility: true,
+            name: String::from("Matrix5x5"),
+            r#type: LIRType::Array {
+                element: Box::new(LIRType::Array {
+                    element: Box::new(LIRType::Identifier {
+                        identifier: String::from("i64"),
+                    }),
+                    size: 5,
+                }),
+                size: 5,
+            },
+        };
+        assert_eq!(lir_from_mir(array_alias), control)
+    }
+}
