1e3bbd935c93c264df058d4449b40da132dbe0ed
Emilie THOME <emilie.e.thome@renault.com>
2023-09-21 15:12:46 +0200

    test: add test for MIR enumeration definition transformation into LIR

--- src/frontend/lir_from_mir/item/enumeration.rs
+++ src/frontend/lir_from_mir/item/enumeration.rs
@@ -9,3 +9,32 @@ pub fn lir_from_mir(enumeration: Enumeration) -> LIREnumeration {
         elements: enumeration.elements,
     }
 }
+
+#[cfg(test)]
+mod lir_from_mir {
+    use crate::frontend::lir_from_mir::item::enumeration::lir_from_mir;
+    use crate::lir::item::enumeration::Enumeration as LIREnumeration;
+    use crate::mir::item::enumeration::Enumeration;
+
+    #[test]
+    fn should_create_lir_enumeration_from_mir_enumeration() {
+        let enumeration = Enumeration {
+            name: String::from("Color"),
+            elements: vec![
+                String::from("Blue"),
+                String::from("Red"),
+                String::from("Green"),
+            ],
+        };
+        let control = LIREnumeration {
+            public_visibility: true,
+            name: String::from("Color"),
+            elements: vec![
+                String::from("Blue"),
+                String::from("Red"),
+                String::from("Green"),
+            ],
+        };
+        assert_eq!(lir_from_mir(enumeration), control)
+    }
+}
