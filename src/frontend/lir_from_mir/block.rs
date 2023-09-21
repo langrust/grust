7ba3c28528de34864c2d592bdd7035f0fb77e889
Emilie THOME <emilie.e.thome@renault.com>
2023-09-21 15:02:50 +0200

    test: add test for MIR block transformation into LIR

--- src/frontend/lir_from_mir/block.rs
+++ src/frontend/lir_from_mir/block.rs
@@ -12,3 +12,54 @@ pub fn lir_from_mir(block: Block) -> LIRBlock {
         .collect();
     LIRBlock { statements }
 }
+
+#[cfg(test)]
+mod lir_from_mir {
+    use crate::common::constant::Constant;
+    use crate::frontend::lir_from_mir::block::lir_from_mir;
+    use crate::lir::block::Block as LIRBlock;
+    use crate::lir::expression::Expression as LIRExpression;
+    use crate::lir::pattern::Pattern as LIRPattern;
+    use crate::lir::statement::r#let::Let;
+    use crate::lir::statement::Statement as LIRStatement;
+    use crate::mir::block::Block;
+    use crate::mir::expression::Expression;
+    use crate::mir::statement::Statement;
+
+    #[test]
+    fn should_create_lir_block_from_mir_block() {
+        let block = Block {
+            statements: vec![
+                Statement::Let {
+                    identifier: String::from("x"),
+                    expression: Expression::Literal {
+                        literal: Constant::Integer(1),
+                    },
+                },
+                Statement::ExpressionLast {
+                    expression: Expression::Identifier {
+                        identifier: String::from("x"),
+                    },
+                },
+            ],
+        };
+        let control = LIRBlock {
+            statements: vec![
+                LIRStatement::Let(Let {
+                    pattern: LIRPattern::Identifier {
+                        reference: false,
+                        mutable: false,
+                        identifier: String::from("x"),
+                    },
+                    expression: LIRExpression::Literal {
+                        literal: Constant::Integer(1),
+                    },
+                }),
+                LIRStatement::ExpressionLast(LIRExpression::Identifier {
+                    identifier: String::from("x"),
+                }),
+            ],
+        };
+        assert_eq!(lir_from_mir(block), control)
+    }
+}
