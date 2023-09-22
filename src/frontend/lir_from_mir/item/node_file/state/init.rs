8a5e9fb22b863922d7713dc7b0b32536bf9e6214
Emilie Thome <emilie.thome@hotmail.fr>
2023-09-22 12:05:59 +0200

    test: add test for MIR init transformation into LIR implementation item

--- src/frontend/lir_from_mir/item/node_file/state/init.rs
+++ src/frontend/lir_from_mir/item/node_file/state/init.rs
@@ -53,3 +53,67 @@ pub fn lir_from_mir(init: Init) -> AssociatedItem {
     };
     AssociatedItem::AssociatedMethod { signature, body }
 }
+
+#[cfg(test)]
+mod lir_from_mir {
+    use crate::common::constant::Constant;
+    use crate::frontend::lir_from_mir::item::node_file::state::init::lir_from_mir;
+    use crate::lir::block::Block;
+    use crate::lir::expression::{Expression, FieldExpression};
+    use crate::lir::item::implementation::AssociatedItem;
+    use crate::lir::item::signature::Signature;
+    use crate::lir::r#type::Type as LIRType;
+    use crate::lir::statement::Statement;
+    use crate::mir::item::node_file::state::init::{Init, StateElementInit};
+
+    #[test]
+    fn should_create_lir_associated_method_from_mir_node_init() {
+        let init = Init {
+            node_name: format!("Node"),
+            state_elements_init: vec![
+                StateElementInit::BufferInit {
+                    identifier: format!("mem_i"),
+                    initial_value: Constant::Integer(0),
+                },
+                StateElementInit::CalledNodeInit {
+                    identifier: format!("called_node_state"),
+                    node_name: format!("CalledNode"),
+                },
+            ],
+        };
+        let control = AssociatedItem::AssociatedMethod {
+            signature: Signature {
+                public_visibility: true,
+                name: format!("init"),
+                receiver: None,
+                inputs: vec![],
+                output: LIRType::Identifier {
+                    identifier: format!("NodeState"),
+                },
+            },
+            body: Block {
+                statements: vec![Statement::ExpressionLast(Expression::Structure {
+                    name: format!("NodeState"),
+                    fields: vec![
+                        FieldExpression {
+                            name: format!("mem_i"),
+                            expression: Expression::Literal {
+                                literal: Constant::Integer(0),
+                            },
+                        },
+                        FieldExpression {
+                            name: format!("called_node_state"),
+                            expression: Expression::FunctionCall {
+                                function: Box::new(Expression::Identifier {
+                                    identifier: format!("CalledNodeState::init"),
+                                }),
+                                arguments: vec![],
+                            },
+                        },
+                    ],
+                })],
+            },
+        };
+        assert_eq!(lir_from_mir(init), control)
+    }
+}
