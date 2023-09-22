7cdb5eb64d251a6f9b78f0ca740ff7cf6c8909e7
Emilie THOME <emilie.e.thome@renault.com>
2023-09-22 14:32:12 +0200

    test: add test for MIR step transformation into LIR implementation item

--- src/frontend/lir_from_mir/item/node_file/state/step.rs
+++ src/frontend/lir_from_mir/item/node_file/state/step.rs
@@ -67,3 +67,195 @@ pub fn lir_from_mir(step: Step) -> AssociatedItem {
     let body = Block { statements };
     AssociatedItem::AssociatedMethod { signature, body }
 }
+
+#[cfg(test)]
+mod lir_from_mir {
+    use crate::common::constant::Constant;
+    use crate::common::operator::BinaryOperator;
+    use crate::common::r#type::Type;
+    use crate::frontend::lir_from_mir::item::node_file::state::step::lir_from_mir;
+    use crate::lir::block::Block;
+    use crate::lir::expression::{Expression as LIRExpression, FieldExpression};
+    use crate::lir::item::implementation::AssociatedItem;
+    use crate::lir::item::signature::{Receiver, Signature};
+    use crate::lir::pattern::Pattern;
+    use crate::lir::r#type::Type as LIRType;
+    use crate::lir::statement::r#let::Let;
+    use crate::lir::statement::Statement as LIRStatement;
+    use crate::mir::expression::Expression;
+    use crate::mir::item::node_file::state::step::{StateElementStep, Step};
+    use crate::mir::statement::Statement;
+
+    #[test]
+    fn should_create_lir_associated_method_from_mir_node_init() {
+        let init = Step {
+            node_name: format!("Node"),
+            output_type: Type::Integer,
+            body: vec![
+                Statement::Let {
+                    identifier: format!("o"),
+                    expression: Expression::FieldAccess {
+                        expression: Box::new(Expression::Identifier {
+                            identifier: format!("self"),
+                        }),
+                        field: format!("mem_i"),
+                    },
+                },
+                Statement::LetTuple {
+                    identifiers: vec![format!("new_called_node_state"), format!("y")],
+                    expression: Expression::NodeCall {
+                        node_identifier: format!("called_node_state"),
+                        input_name: format!("CalledNodeInput"),
+                        input_fields: vec![],
+                    },
+                },
+            ],
+            state_elements_step: vec![
+                StateElementStep {
+                    identifier: format!("mem_i"),
+                    expression: Expression::FunctionCall {
+                        function: Box::new(Expression::Identifier {
+                            identifier: format!(" + "),
+                        }),
+                        arguments: vec![
+                            Expression::Identifier {
+                                identifier: format!("o"),
+                            },
+                            Expression::Literal {
+                                literal: Constant::Integer(1),
+                            },
+                        ],
+                    },
+                },
+                StateElementStep {
+                    identifier: format!("called_node_state"),
+                    expression: Expression::Identifier {
+                        identifier: format!("new_called_node_state"),
+                    },
+                },
+            ],
+            output_expression: Expression::FunctionCall {
+                function: Box::new(Expression::Identifier {
+                    identifier: format!(" + "),
+                }),
+                arguments: vec![
+                    Expression::Identifier {
+                        identifier: format!("o"),
+                    },
+                    Expression::Identifier {
+                        identifier: format!("y"),
+                    },
+                ],
+            },
+        };
+        let control = AssociatedItem::AssociatedMethod {
+            signature: Signature {
+                public_visibility: true,
+                name: format!("step"),
+                receiver: Some(Receiver {
+                    reference: false,
+                    mutable: false,
+                }),
+                inputs: vec![(
+                    format!("input"),
+                    LIRType::Identifier {
+                        identifier: format!("NodeInput"),
+                    },
+                )],
+                output: LIRType::Tuple {
+                    elements: vec![
+                        LIRType::Identifier {
+                            identifier: format!("NodeState"),
+                        },
+                        LIRType::Identifier {
+                            identifier: format!("i64"),
+                        },
+                    ],
+                },
+            },
+            body: Block {
+                statements: vec![
+                    LIRStatement::Let(Let {
+                        pattern: Pattern::Identifier {
+                            reference: false,
+                            mutable: false,
+                            identifier: format!("o"),
+                        },
+                        expression: LIRExpression::FieldAccess {
+                            expression: Box::new(LIRExpression::Identifier {
+                                identifier: format!("self"),
+                            }),
+                            field: format!("mem_i"),
+                        },
+                    }),
+                    LIRStatement::Let(Let {
+                        pattern: Pattern::Tuple {
+                            elements: vec![
+                                Pattern::Identifier {
+                                    reference: false,
+                                    mutable: false,
+                                    identifier: format!("new_called_node_state"),
+                                },
+                                Pattern::Identifier {
+                                    reference: false,
+                                    mutable: false,
+                                    identifier: format!("y"),
+                                },
+                            ],
+                        },
+                        expression: LIRExpression::MethodCall {
+                            receiver: Box::new(LIRExpression::FieldAccess {
+                                expression: Box::new(LIRExpression::Identifier {
+                                    identifier: format!("self"),
+                                }),
+                                field: format!("called_node_state"),
+                            }),
+                            method: format!("step"),
+                            arguments: vec![LIRExpression::Structure {
+                                name: format!("CalledNodeInput"),
+                                fields: vec![],
+                            }],
+                        },
+                    }),
+                    LIRStatement::ExpressionLast(LIRExpression::Tuple {
+                        elements: vec![
+                            LIRExpression::Structure {
+                                name: format!("NodeState"),
+                                fields: vec![
+                                    FieldExpression {
+                                        name: format!("mem_i"),
+                                        expression: LIRExpression::Binary {
+                                            left: Box::new(LIRExpression::Identifier {
+                                                identifier: format!("o"),
+                                            }),
+                                            operator: BinaryOperator::Add,
+                                            right: Box::new(LIRExpression::Literal {
+                                                literal: Constant::Integer(1),
+                                            }),
+                                        },
+                                    },
+                                    FieldExpression {
+                                        name: format!("called_node_state"),
+                                        expression: LIRExpression::Identifier {
+                                            identifier: format!("new_called_node_state"),
+                                        },
+                                    },
+                                ],
+                            },
+                            LIRExpression::Binary {
+                                left: Box::new(LIRExpression::Identifier {
+                                    identifier: format!("o"),
+                                }),
+                                operator: BinaryOperator::Add,
+                                right: Box::new(LIRExpression::Identifier {
+                                    identifier: format!("y"),
+                                }),
+                            },
+                        ],
+                    }),
+                ],
+            },
+        };
+        assert_eq!(lir_from_mir(init), control)
+    }
+}
