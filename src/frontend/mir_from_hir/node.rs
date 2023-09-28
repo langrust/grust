fa6ced5cfd54d5fc25d95ddeec6b05ea6263334a
Emilie THOME <emilie.e.thome@renault.com>
2023-09-28 14:48:32 +0200

    feat: create MIR node files construction from HIR node

---
+++ src/frontend/mir_from_hir/node.rs
@@ -0,0 +1,11 @@
+use crate::{hir::node::Node, mir::item::node_file::NodeFile};
+
+use super::unitary_node::mir_from_hir as unitary_node_mir_from_hir;
+
+/// Transform HIR node into MIR node files.
+pub fn mir_from_hir(node: Node) -> Vec<NodeFile> {
+    node.unitary_nodes
+        .into_iter()
+        .map(|(_, unitary_node)| unitary_node_mir_from_hir(unitary_node))
+        .collect()
+}
