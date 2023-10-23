22ec878f97950b7080ebb368fe7d25cffe7eca26
Emilie THOME <emilie.e.thome@renault.com>
2023-10-23 16:05:49 +0200

    test: add tests check for HIR equations transformations into MIR statements

--- src/frontend/mir_from_hir/equation.rs
+++ src/frontend/mir_from_hir/equation.rs
@@ -21,3 +21,98 @@ pub fn mir_from_hir(equation: Equation) -> Statement {
         },
     }
 }
+
+#[cfg(test)]
+mod mir_from_hir {
+    use crate::{
+        common::{constant::Constant, location::Location, r#type::Type, scope::Scope},
+        frontend::mir_from_hir::equation::mir_from_hir,
+        hir::{
+            dependencies::Dependencies, equation::Equation, stream_expression::StreamExpression,
+        },
+        mir::{expression::Expression, statement::Statement},
+    };
+
+    #[test]
+    fn should_transform_hir_equation_of_constant_into_mir_let_statement() {
+        let equation = Equation {
+            scope: Scope::Local,
+            id: format!("y"),
+            expression: StreamExpression::Constant {
+                constant: Constant::Integer(1),
+                typing: Type::Integer,
+                location: Location::default(),
+                dependencies: Dependencies::new(),
+            },
+            signal_type: Type::Integer,
+            location: Location::default(),
+        };
+        let control = Statement::Let {
+            identifier: format!("y"),
+            expression: Expression::Literal {
+                literal: Constant::Integer(1),
+            },
+        };
+        assert_eq!(mir_from_hir(equation), control)
+    }
+
+    #[test]
+    fn should_transform_hir_equation_of_unitary_node_application_into_mir_tuple_statement() {
+        let equation = Equation {
+            scope: Scope::Local,
+            id: format!("y"),
+            expression: StreamExpression::UnitaryNodeApplication {
+                id: Some(format!("my_nodeoy")),
+                node: format!("my_node"),
+                signal: format!("o"),
+                inputs: vec![
+                    (
+                        format!("i"),
+                        StreamExpression::SignalCall {
+                            id: format!("x"),
+                            typing: Type::Integer,
+                            location: Location::default(),
+                            dependencies: Dependencies::from(vec![(format!("x"), 0)]),
+                        },
+                    ),
+                    (
+                        format!("j"),
+                        StreamExpression::Constant {
+                            constant: Constant::Integer(1),
+                            typing: Type::Integer,
+                            location: Location::default(),
+                            dependencies: Dependencies::new(),
+                        },
+                    ),
+                ],
+                typing: Type::Integer,
+                location: Location::default(),
+                dependencies: Dependencies::from(vec![(format!("x"), 0)]),
+            },
+            signal_type: Type::Integer,
+            location: Location::default(),
+        };
+        let control = Statement::LetTuple {
+            identifiers: vec![format!("my_nodeoy"), format!("y")],
+            expression: Expression::NodeCall {
+                node_identifier: format!("my_nodeoy"),
+                input_name: format!("my_nodeoInput"),
+                input_fields: vec![
+                    (
+                        format!("i"),
+                        Expression::Identifier {
+                            identifier: format!("x"),
+                        },
+                    ),
+                    (
+                        format!("j"),
+                        Expression::Literal {
+                            literal: Constant::Integer(1),
+                        },
+                    ),
+                ],
+            },
+        };
+        assert_eq!(mir_from_hir(equation), control)
+    }
+}
