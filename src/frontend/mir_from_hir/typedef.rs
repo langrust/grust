ea5d26cc5c611be6518525e3f173fe1ffb139285
Emilie THOME <emilie.e.thome@renault.com>
2023-10-23 15:02:31 +0200

    test: add tests to check HIR typedefs tranformations into MIR items

--- src/frontend/mir_from_hir/typedef.rs
+++ src/frontend/mir_from_hir/typedef.rs
@@ -22,3 +22,59 @@ pub fn mir_from_hir(typedef: Typedef) -> Item {
         }),
     }
 }
+
+#[cfg(test)]
+mod mir_from_hir {
+    use crate::{
+        ast::typedef::Typedef,
+        common::{location::Location, r#type::Type},
+        frontend::mir_from_hir::typedef::mir_from_hir,
+        mir::item::{
+            array_alias::ArrayAlias, enumeration::Enumeration, structure::Structure, Item,
+        },
+    };
+
+    #[test]
+    fn should_transform_hir_structure_type_definition_into_mir_structure_item() {
+        let structure = Typedef::Structure {
+            id: format!("Point"),
+            fields: vec![(format!("x"), Type::Integer), (format!("y"), Type::Integer)],
+            location: Location::default(),
+        };
+        let control = Item::Structure(Structure {
+            name: format!("Point"),
+            fields: vec![(format!("x"), Type::Integer), (format!("y"), Type::Integer)],
+        });
+        assert_eq!(mir_from_hir(structure), control)
+    }
+
+    #[test]
+    fn should_transform_hir_enumeration_type_definition_into_mir_enumeration_item() {
+        let enumeration = Typedef::Enumeration {
+            id: format!("Color"),
+            elements: vec![format!("Red"), format!("Bleu"), format!("Green")],
+            location: Location::default(),
+        };
+        let control = Item::Enumeration(Enumeration {
+            name: format!("Color"),
+            elements: vec![format!("Red"), format!("Bleu"), format!("Green")],
+        });
+        assert_eq!(mir_from_hir(enumeration), control)
+    }
+
+    #[test]
+    fn should_transform_hir_array_type_definition_into_mir_array_alias_item() {
+        let array = Typedef::Array {
+            id: format!("M5x5"),
+            array_type: Type::Array(Box::new(Type::Integer), 5),
+            size: 5,
+            location: Location::default(),
+        };
+        let control = Item::ArrayAlias(ArrayAlias {
+            name: format!("M5x5"),
+            array_type: Type::Array(Box::new(Type::Integer), 5),
+            size: 5,
+        });
+        assert_eq!(mir_from_hir(array), control)
+    }
+}
