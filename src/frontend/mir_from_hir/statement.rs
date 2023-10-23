c9803b7e9e7bdb0c31f746f8b737459b3d102759
Emilie THOME <emilie.e.thome@renault.com>
2023-10-23 16:19:20 +0200

    test: add test check AST statement transformation into MIR statement

--- src/frontend/mir_from_hir/statement.rs
+++ src/frontend/mir_from_hir/statement.rs
@@ -10,3 +10,34 @@ pub fn mir_from_hir(statement: Statement) -> MIRStatement {
         expression: expression_mir_from_hir(expression),
     }
 }
+
+#[cfg(test)]
+mod mir_from_hir {
+    use crate::{
+        ast::{expression::Expression as ASTExpression, statement::Statement as ASTStatement},
+        common::{constant::Constant, location::Location, r#type::Type},
+        frontend::mir_from_hir::statement::mir_from_hir,
+        mir::{expression::Expression, statement::Statement},
+    };
+
+    #[test]
+    fn should_transform_ast_statement_of_constant_into_mir_let_statement() {
+        let statement = ASTStatement {
+            id: format!("y"),
+            expression: ASTExpression::Constant {
+                constant: Constant::Integer(1),
+                typing: Some(Type::Integer),
+                location: Location::default(),
+            },
+            element_type: Type::Integer,
+            location: Location::default(),
+        };
+        let control = Statement::Let {
+            identifier: format!("y"),
+            expression: Expression::Literal {
+                literal: Constant::Integer(1),
+            },
+        };
+        assert_eq!(mir_from_hir(statement), control)
+    }
+}
