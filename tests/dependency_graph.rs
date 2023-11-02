d70d8d870492a0308ca962159ddf82b19b9814d7
Emilie Thome <emilie.thome@hotmail.fr>
2023-11-02 15:05:23 +0100

    test: snapshot testing blinking program dependency graph construction

--- tests/dependency_graph.rs
+++ tests/dependency_graph.rs
@@ -24,3 +24,45 @@ fn dependency_graph_of_counter() {
 
     insta::assert_yaml_snapshot!(file);
 }
+
+#[test]
+fn dependency_graph_of_blinking() {
+    let mut files = SimpleFiles::new();
+    let mut errors = vec![];
+
+    let blinking_id = files.add(
+        "blinking.gr",
+        std::fs::read_to_string("tests/fixture/blinking.gr").expect("unkown file"),
+    );
+
+    let mut file: File = langrust::fileParser::new()
+        .parse(blinking_id, &files.source(blinking_id).unwrap())
+        .unwrap();
+    file.typing(&mut errors).unwrap();
+    let file = hir_from_ast(file);
+
+    file.generate_dependency_graphs(&mut errors).unwrap();
+
+    insta::assert_yaml_snapshot!(file);
+}
+
+// #[test]
+// fn dependency_graph_of_button_management() {
+//     let mut files = SimpleFiles::new();
+//     let mut errors = vec![];
+
+//     let blinking_id = files.add(
+//         "button_management.gr",
+//         std::fs::read_to_string("tests/fixture/button_management.gr").expect("unkown file"),
+//     );
+
+//     let mut file: File = langrust::fileParser::new()
+//         .parse(blinking_id, &files.source(blinking_id).unwrap())
+//         .unwrap();
+//     file.typing(&mut errors).unwrap();
+//     let file = hir_from_ast(file);
+
+//     file.generate_dependency_graphs(&mut errors).unwrap();
+
+//     insta::assert_yaml_snapshot!(file);
+// }
