312b6ff7aea05f689c836c944d545768fcbf73fa
Emilie Thome <emilie.thome@hotmail.fr>
2023-11-02 15:29:46 +0100

    test: causality analysis of counter blinking button_management

---
+++ tests/causality_analysis.rs
@@ -0,0 +1,65 @@
+use codespan_reporting::files::{Files, SimpleFiles};
+
+use grustine::ast::file::File;
+use grustine::frontend::hir_from_ast::file::hir_from_ast;
+use grustine::parser::langrust;
+
+#[test]
+fn causality_analysis_of_counter() {
+    let mut files = SimpleFiles::new();
+    let mut errors = vec![];
+
+    let counter_id = files.add(
+        "counter.gr",
+        std::fs::read_to_string("tests/fixture/counter.gr").expect("unkown file"),
+    );
+
+    let mut file: File = langrust::fileParser::new()
+        .parse(counter_id, &files.source(counter_id).unwrap())
+        .unwrap();
+    file.typing(&mut errors).unwrap();
+    let file = hir_from_ast(file);
+    file.generate_dependency_graphs(&mut errors).unwrap();
+
+    file.causality_analysis(&mut errors).unwrap();
+}
+
+#[test]
+fn dependency_graph_of_blinking() {
+    let mut files = SimpleFiles::new();
+    let mut errors = vec![];
+
+    let blinking_id = files.add(
+        "blinking.gr",
+        std::fs::read_to_string("tests/fixture/blinking.gr").expect("unkown file"),
+    );
+
+    let mut file: File = langrust::fileParser::new()
+        .parse(blinking_id, &files.source(blinking_id).unwrap())
+        .unwrap();
+    file.typing(&mut errors).unwrap();
+    let file = hir_from_ast(file);
+    file.generate_dependency_graphs(&mut errors).unwrap();
+
+    file.causality_analysis(&mut errors).unwrap();
+}
+
+#[test]
+fn dependency_graph_of_button_management() {
+    let mut files = SimpleFiles::new();
+    let mut errors = vec![];
+
+    let blinking_id = files.add(
+        "button_management.gr",
+        std::fs::read_to_string("tests/fixture/button_management.gr").expect("unkown file"),
+    );
+
+    let mut file: File = langrust::fileParser::new()
+        .parse(blinking_id, &files.source(blinking_id).unwrap())
+        .unwrap();
+    file.typing(&mut errors).unwrap();
+    let file = hir_from_ast(file);
+    file.generate_dependency_graphs(&mut errors).unwrap();
+
+    file.causality_analysis(&mut errors).unwrap();
+}
