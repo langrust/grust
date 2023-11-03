a8a088d19709d7c71ac4f2affaab76dd4b8c0263
Emilie Thome <emilie.e.thome@renault.com>
2023-11-03 10:58:49 +0100

    feat: compoennt should also be scheduled

--- src/frontend/normalizing/scheduling/file.rs
+++ src/frontend/normalizing/scheduling/file.rs
@@ -29,6 +29,12 @@ impl File {
             node.unitary_nodes
                 .iter_mut()
                 .for_each(|(_, unitary_node)| unitary_node.schedule())
+        });
+        self.component.as_mut().map_or((), |component| {
+            component
+                .unitary_nodes
+                .iter_mut()
+                .for_each(|(_, unitary_node)| unitary_node.schedule())
         })
     }
 }
