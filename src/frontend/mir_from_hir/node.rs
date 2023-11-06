1714d0ec6613e04272374e07da68d50d12e349a2
Emilie Thome <emilie.thome@hotmail.fr>
2023-11-06 14:58:52 +0100

    fix: deterministic order of unitary nodes

--- src/frontend/mir_from_hir/node.rs
+++ src/frontend/mir_from_hir/node.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 use crate::{hir::node::Node, mir::item::node_file::NodeFile};
 
 use super::unitary_node::mir_from_hir as unitary_node_mir_from_hir;
@@ -5,7 +7,8 @@ use super::unitary_node::mir_from_hir as unitary_node_mir_from_hir;
 /// Transform HIR node into MIR node files.
 pub fn mir_from_hir(node: Node) -> Vec<NodeFile> {
     node.unitary_nodes
-        .into_values()
-        .map(unitary_node_mir_from_hir)
+        .into_iter()
+        .sorted_by_key(|(id, _)| id.clone())
+        .map(|(_, unitary_node)| unitary_node_mir_from_hir(unitary_node))
         .collect()
 }
