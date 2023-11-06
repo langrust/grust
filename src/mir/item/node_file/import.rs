c22e9cb4da74a1926dc984ce1513ec49f9e5900f
Emilie THOME <emilie.e.thome@renault.com>
2023-11-06 13:21:08 +0100

    feat: import structures and enumerations used by a node into its file

--- src/mir/item/node_file/import.rs
+++ src/mir/item/node_file/import.rs
@@ -1,8 +1,14 @@
 /// A node's import.
-#[derive(Debug, PartialEq, serde::Serialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Serialize)]
 pub enum Import {
     /// Import of another node.
     NodeFile(String),
     /// Import of a function.
     Function(String),
+    /// Import of an enumeration.
+    Enumeration(String),
+    /// Import of a structure.
+    Structure(String),
+    /// Import of an array alias.
+    ArrayAlias(String),
 }
