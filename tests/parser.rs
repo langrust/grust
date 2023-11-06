8ddbf71c0de88873188887f2eb0ba9f2d08519ab
Emilie THOME <emilie.e.thome@renault.com>
2023-11-06 16:08:15 +0100

    refactor: move parser snapshot tests into parser file

--- tests/parser.rs
+++ tests/parser.rs
@@ -1,3 +1,8 @@
+use codespan_reporting::files::{Files, SimpleFiles};
+
+use grustine::ast::file::File;
+use grustine::parser::langrust;
+
 #[cfg(test)]
 mod langrust_ast_constructs {
     use codespan_reporting::files::{Files, SimpleFiles};
@@ -2194,4 +2199,52 @@ mod langrust_ast_constructs {
             constant
         );
     }
+}
+
+#[test]
+fn parse_counter() {
+    let mut files = SimpleFiles::new();
+
+    let counter_id = files.add(
+        "counter.gr",
+        std::fs::read_to_string("tests/fixture/counter.gr").expect("unkown file"),
+    );
+
+    let file: File = langrust::fileParser::new()
+        .parse(counter_id, &files.source(counter_id).unwrap())
+        .unwrap();
+
+    insta::assert_yaml_snapshot!(file);
+}
+
+#[test]
+fn parse_blinking() {
+    let mut files = SimpleFiles::new();
+
+    let blinking_id = files.add(
+        "blinking.gr",
+        std::fs::read_to_string("tests/fixture/blinking.gr").expect("unkown file"),
+    );
+
+    let file: File = langrust::fileParser::new()
+        .parse(blinking_id, &files.source(blinking_id).unwrap())
+        .unwrap();
+
+    insta::assert_yaml_snapshot!(file);
+}
+
+#[test]
+fn parse_button_management() {
+    let mut files = SimpleFiles::new();
+
+    let blinking_id = files.add(
+        "button_management.gr",
+        std::fs::read_to_string("tests/fixture/button_management.gr").expect("unkown file"),
+    );
+
+    let file: File = langrust::fileParser::new()
+        .parse(blinking_id, &files.source(blinking_id).unwrap())
+        .unwrap();
+
+    insta::assert_yaml_snapshot!(file);
 }
