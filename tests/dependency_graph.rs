c39ba3db558762159817009927d14946adb84549
Emilie Thome <emilie.thome@hotmail.fr>
2023-11-07 15:18:12 +0100

    test: pid causality tests

--- tests/dependency_graph.rs
+++ tests/dependency_graph.rs
@@ -110,3 +110,24 @@ fn dependency_graph_of_button_management_using_function() {
 
     insta::assert_yaml_snapshot!(file);
 }
+
+#[test]
+fn dependency_graph_of_pid() {
+    let mut files = SimpleFiles::new();
+    let mut errors = vec![];
+
+    let pid_id = files.add(
+        "pid.gr",
+        std::fs::read_to_string("tests/fixture/pid.gr").expect("unkown file"),
+    );
+
+    let mut file: File = langrust::fileParser::new()
+        .parse(pid_id, &files.source(pid_id).unwrap())
+        .unwrap();
+    file.typing(&mut errors).unwrap();
+    let file = hir_from_ast(file);
+
+    file.generate_dependency_graphs(&mut errors).unwrap();
+
+    insta::assert_yaml_snapshot!(file);
+}
