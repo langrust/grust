bc265dc89fa6ac40c237c5c12537ec889e827af1
Emilie THOME <emilie.e.thome@renault.com>
2023-11-14 15:44:20 +0100

    test: code generation for counter project

---
+++ tests/generate_rust_project.rs
@@ -0,0 +1,32 @@
+use codespan_reporting::files::{Files, SimpleFiles};
+
+use grustine::ast::file::File;
+use grustine::backend::rust_ast_from_lir::project::rust_ast_from_lir;
+use grustine::frontend::hir_from_ast::file::hir_from_ast;
+use grustine::frontend::lir_from_hir::file::lir_from_hir;
+use grustine::parser::langrust;
+
+#[test]
+fn generate_rust_project_for_counter() {
+    let mut files = SimpleFiles::new();
+    let mut errors = vec![];
+
+    let counter_id = files.add(
+        "counter.gr",
+        std::fs::read_to_string("tests/fixture/counter.gr").expect("unkown file"),
+    );
+
+    let mut file: File = langrust::fileParser::new()
+        .parse(counter_id, &files.source(counter_id).unwrap())
+        .unwrap();
+    file.typing(&mut errors).unwrap();
+    let mut file = hir_from_ast(file);
+    file.generate_dependency_graphs(&mut errors).unwrap();
+    file.causality_analysis(&mut errors).unwrap();
+    file.normalize(&mut errors).unwrap();
+    let project = lir_from_hir(file);
+    let mut project = rust_ast_from_lir(project);
+    project.set_parent("tests/generated/counter/");
+
+    project.generate()
+}
