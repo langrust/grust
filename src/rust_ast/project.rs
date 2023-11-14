336111b30198037bbee2bd3a9b42d08289b4f45d
Emilie THOME <emilie.e.thome@renault.com>
2023-11-14 15:28:09 +0100

    feat: add directory to project

--- src/rust_ast/project.rs
+++ src/rust_ast/project.rs
@@ -3,13 +3,18 @@ use super::file::File;
 #[derive(serde::Serialize)]
 /// A Rust source code project.
 pub struct Project {
+    /// Project's directory.
+    pub directory: String,
     /// All the files contained by the project.
     pub files: Vec<File>,
 }
 impl Project {
     /// Create a new project.
     pub fn new() -> Self {
-        Project { files: vec![] }
+        Project {
+            files: vec![],
+            directory: String::new(),
+        }
     }
 
     /// Add file.
@@ -22,6 +27,13 @@ impl Project {
     where
         P: AsRef<std::path::Path>,
     {
+        let subdirectory = std::mem::take(&mut self.directory);
+        self.directory = path
+            .as_ref()
+            .join(subdirectory)
+            .into_os_string()
+            .into_string()
+            .unwrap();
         self.files
             .iter_mut()
             .for_each(|file| file.set_parent(&path))
