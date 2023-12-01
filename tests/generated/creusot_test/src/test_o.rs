f2310e4bb65d12a36d39f363d101381dfc37ed73
Emilie Thome <emilie.thome@hotmail.fr>
2023-12-01 16:12:53 +0100

    feat: creusot prove the correctness of the grrust file

---
+++ tests/generated/creusot_test/src/test_o.rs
@@ -0,0 +1,39 @@
+use creusot_contracts::{ensures, requires};
+
+pub struct TestOInput {
+    pub i1: i64,
+    pub i2: i64,
+}
+pub struct TestOState {
+    pub mem_z: i64,
+}
+impl TestOState {
+    #[ensures(1000i64 >= result.mem_z)]
+    #[ensures(result.mem_z > 0i64)]
+    pub fn init() -> TestOState {
+        TestOState { mem_z: 1i64 }
+    }
+    #[requires(1000i64 > input.i1)]
+    #[requires(input.i1 > 0i64)]
+    #[requires(1000i64 >= input.i2)]
+    #[requires(input.i2 > input.i1)]
+    #[requires(1000i64 >= self.mem_z)]
+    #[requires(self.mem_z > 0i64)]
+    #[ensures(1000i64 >= result.0.mem_z)]
+    #[ensures(result.0.mem_z > 0i64)]
+    #[ensures(input.i1 >= result.1)]
+    #[ensures(result.1 >= 0i64)]
+    pub fn step(self, input: TestOInput) -> (TestOState, i64) {
+        let x = input.i2 - input.i1;
+        let y = input.i1 / x;
+        let z = self.mem_z;
+        let o = y / z;
+        let z_prime = z + x;
+        (
+            TestOState {
+                mem_z: if z_prime > 1000i64 { 1i64 } else { z_prime },
+            },
+            o,
+        )
+    }
+}
