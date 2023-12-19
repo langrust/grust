b54afdeefa05857538e92ef0949a533f2e2f0bb1
Emilie THOME <emilie.e.thome@renault.com>
2023-12-19 17:27:27 +0100

    refactor: remove completly tuples from LIR statements

--- src/lir/statement.rs
+++ src/lir/statement.rs
@@ -10,13 +10,6 @@ pub enum Statement {
         /// The expression associated to the variable.
         expression: Expression,
     },
-    /// A let-statement creating a tuple of variables: `let (o, new_state) = self.state.step(inputs);`.
-    LetTuple {
-        /// The variables created.
-        identifiers: Vec<String>,
-        /// The expression associated to the variables.
-        expression: Expression,
-    },
     /// A returned expression.
     ExpressionLast {
         /// The returned expression.
