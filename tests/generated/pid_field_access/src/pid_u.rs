b947fe6655a253b5b8c45d6a20c3431ff4da6721
Emilie Thome <emilie.thome@hotmail.fr>
2023-12-22 09:48:20 +0100

    test: integration test for field access

---
+++ tests/generated/pid_field_access/src/pid_u.rs
@@ -0,0 +1,37 @@
+use crate::derive_d::*;
+use crate::integrate_i::*;
+use crate::typedefs::GainPID;
+pub struct PidUInput {
+    pub v_c: f64,
+    pub v: f64,
+    pub dt: f64,
+}
+pub struct PidUState {
+    derive_d_e_d: DeriveDState,
+    integrate_i_e_i: IntegrateIState,
+}
+impl PidUState {
+    pub fn init() -> PidUState {
+        PidUState {
+            derive_d_e_d: DeriveDState::init(),
+            integrate_i_e_i: IntegrateIState::init(),
+        }
+    }
+    pub fn step(&mut self, input: PidUInput) -> f64 {
+        let e = input.v_c - input.v;
+        let e_d = self.derive_d_e_d.step(DeriveDInput { x: e, dt: input.dt });
+        let e_i = self
+            .integrate_i_e_i
+            .step(IntegrateIInput {
+                x: e,
+                dt: input.dt,
+            });
+        let gain = GainPID {
+            k_p: 0.2f64,
+            k_i: 1.5f64,
+            k_d: 6f64,
+        };
+        let u = gain.k_p * e + gain.k_i * e_i + gain.k_d * e_d;
+        u
+    }
+}
---
+++ tests/generated/pid_field_access/src/typedefs.rs
@@ -0,0 +1,5 @@
+pub struct GainPID {
+    pub k_p: f64,
+    pub k_i: f64,
+    pub k_d: f64,
+}
