4911efbafd5b0705c411b41d61dfc8c7be96eaf8
Emilie Thome <emilie.thome@hotmail.fr>
2023-12-22 14:44:07 +0100

    test: integration test of sorting expression

--- tests/generate_rust_project.rs
+++ tests/generate_rust_project.rs
@@ -328,3 +328,31 @@ fn generate_rust_project_for_alarm_counter() {
 
     project.generate()
 }
+
+#[test]
+fn generate_rust_project_for_alarm_sort_function() {
+    let mut files = SimpleFiles::new();
+    let mut errors = vec![];
+
+    let alarm_sort_function_id = files.add(
+        "alarm_sort_function.gr",
+        std::fs::read_to_string("tests/fixture/alarm_sort_function.gr").expect("unkown file"),
+    );
+
+    let mut file: File = langrust::fileParser::new()
+        .parse(
+            alarm_sort_function_id,
+            &files.source(alarm_sort_function_id).unwrap(),
+        )
+        .unwrap();
+    file.typing(&mut errors).unwrap();
+    let mut file = hir_from_ast(file);
+    file.generate_dependency_graphs(&mut errors).unwrap();
+    file.causality_analysis(&mut errors).unwrap();
+    file.normalize(&mut errors).unwrap();
+    let project = lir_from_hir(file);
+    let mut project = rust_ast_from_lir(project);
+    project.set_parent("tests/generated/alarm_sort_function/");
+
+    project.generate()
+}
---
+++ tests/generated/alarm_sort_function/src/functions.rs
@@ -0,0 +1,36 @@
+pub fn priority_order(priority: Priority, other: Priority) -> i64 {
+    let higher_priority = match priority {
+        Priority::High => {
+            match other {
+                Priority::High => 0i64,
+                _ => 1i64,
+            }
+        }
+        Priority::Medium => {
+            match other {
+                Priority::High => -1i64,
+                Priority::Medium => 0i64,
+                Priority::Low => 1i64,
+            }
+        }
+        Priority::Low => {
+            match other {
+                Priority::Low => 1i64,
+                _ => -1i64,
+            }
+        }
+    };
+    higher_priority
+}
+pub fn prioritize(alarm: Alarm, other: Alarm) -> i64 {
+    let raise_difference = alarm.raised != other.raised;
+    let priority_ordering = priority_order(alarm.priority, other.priority);
+    if raise_difference {
+        (if alarm.raised { 1i64 } else { -1i64 })
+    } else {
+        priority_ordering
+    }
+}
+pub fn sorting(alarms: [Alarm; 10]) -> [Alarm; 10] {
+    alarms.sort(prioritize)
+}
