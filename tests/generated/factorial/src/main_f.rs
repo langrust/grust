cd0bbc34f7de10cc0e0c2166ff7827f66e5ed254
Emilie Thome <emilie.e.thome@renault.com>
2024-01-08 09:03:14 +0100

    test: recursive definition of factorial compiles

---
+++ tests/generated/factorial/src/main_f.rs
@@ -0,0 +1,16 @@
+use crate::functions::factorial;
+pub struct MainFInput {}
+pub struct MainFState {
+    mem_n: i64,
+}
+impl MainFState {
+    pub fn init() -> MainFState {
+        MainFState { mem_n: 0i64 }
+    }
+    pub fn step(&mut self, input: MainFInput) -> i64 {
+        let n = self.mem_n;
+        let f = factorial(n);
+        self.mem_n = n + 1i64;
+        f
+    }
+}
