fb561057ac95763baf42c0685439ce28332dbbd1
Emilie Thome <emilie.e.thome@renault.com>
2024-01-16 15:45:35 +0100

    feat: implement interface using FRP like signals

---
+++ tests/generated/adas_example/interface_frp_signals/src/signals_system/object_tracking.rs
@@ -0,0 +1,25 @@
+use futures_signals::{
+    map_ref,
+    signal::{Signal, SignalExt},
+};
+
+use object_tracking::object_tracking_object_motion::{
+    ObjectTrackingObjectMotionInput, ObjectTrackingObjectMotionState,
+};
+
+pub fn object_tracking_object_motion<A>(
+    fused_information: A,
+    mut state: ObjectTrackingObjectMotionState,
+) -> impl Signal<Item = [i64; 10]>
+where
+    A: Signal<Item = [i64; 10]>,
+{
+    let object_motion = map_ref! {
+        fused_information => {
+            ObjectTrackingObjectMotionInput { fused_information: *fused_information }
+        }
+    }
+    .map(move |input| state.step(input));
+
+    object_motion
+}
