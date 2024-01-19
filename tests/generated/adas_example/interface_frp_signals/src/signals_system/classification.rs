a250b318c02e7ed7e9eb75d915a16e1e84416d13
Emilie Thome <emilie.e.thome@renault.com>
2024-01-19 18:03:25 +0100

    refactor: expand macro `map_ref!` to understand

--- tests/generated/adas_example/interface_frp_signals/src/signals_system/classification.rs
+++ tests/generated/adas_example/interface_frp_signals/src/signals_system/classification.rs
@@ -1,5 +1,7 @@
+use std::task::Poll;
+
 use futures_signals::{
-    map_ref,
+    internal::{MapRef1, MapRefSignal},
     signal::{Broadcaster, Signal, SignalExt},
 };
 
@@ -16,13 +18,36 @@ where
     A: Signal<Item = [i64; 10]>,
     B: Signal<Item = i64>,
 {
-    let classification = map_ref! {
-        rgb_images, regions_of_interest => {
-            ClassificationClassificationInput { rgb_images: *rgb_images, regions_of_interest: *regions_of_interest }
-        }
-    }.map(move |input| {
-        state.step(input)
-    });
+    let classification = {
+        let mut rgb_images = MapRef1::new(rgb_images);
+        let mut regions_of_interest = MapRef1::new(regions_of_interest);
+
+        MapRefSignal::new(move |cx| {
+            let mut rgb_images = rgb_images.unsafe_pin();
+            let mut regions_of_interest = regions_of_interest.unsafe_pin();
+
+            let result = rgb_images
+                .as_mut()
+                .poll(cx)
+                .merge(regions_of_interest.as_mut().poll(cx));
+
+            if result.changed {
+                let rgb_images = rgb_images.value_ref();
+                let regions_of_interest = regions_of_interest.value_ref();
+                Poll::Ready(Some({
+                    ClassificationClassificationInput {
+                        rgb_images: *rgb_images,
+                        regions_of_interest: *regions_of_interest,
+                    }
+                }))
+            } else if result.done {
+                Poll::Ready(None)
+            } else {
+                Poll::Pending
+            }
+        })
+    }
+    .map(move |input| state.step(input));
 
     Broadcaster::new(classification)
 }
