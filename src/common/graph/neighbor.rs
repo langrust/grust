300b0a03bc1f0e853046e24a423570eeccdd4a90
Emilie Thome <emilie.e.thome@renault.com>
2024-02-06 17:09:47 +0100

    refactor: add labels to neighbors in graph

--- src/common/graph/neighbor.rs
+++ src/common/graph/neighbor.rs
@@ -3,27 +3,33 @@
 pub struct Neighbor {
     /// The id of the neighbor.
     pub id: String,
-    /// The weight of the edge between the two vertices.
-    pub weight: usize,
+    /// The label of the edge between the two vertices.
+    pub label: Label,
+}
+
+#[derive(Debug, PartialEq, Clone, serde::Serialize)]
+pub enum Label {
+    Contract,
+    Weight(usize),
 }
 
 impl Neighbor {
     /// Creates a new neighbor.
-    pub fn new(id: String, weight: usize) -> Self {
-        Neighbor { id, weight }
+    pub fn new(id: String, label: Label) -> Self {
+        Neighbor { id, label }
     }
 }
 
 #[cfg(test)]
 mod new {
-    use crate::common::graph::neighbor::Neighbor;
+    use crate::common::graph::neighbor::{Neighbor, Label};
 
     #[test]
-    fn should_create_a_neighbor_with_corresponding_id_and_weight() {
-        let neighbor = Neighbor::new(String::from("v1"), 1);
+    fn should_create_a_neighbor_with_corresponding_id_and_label() {
+        let neighbor = Neighbor::new(String::from("v1"), Label::Weight(1));
         let control = Neighbor {
             id: String::from("v1"),
-            weight: 1,
+            label: Label::Weight(1),
         };
 
         assert_eq!(neighbor, control)
