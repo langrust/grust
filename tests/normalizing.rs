81770a0f6593cb5d8fb139980d300ae3fa06d5ca
Emilie Thome <emilie.thome@hotmail.fr>
2024-02-18 17:52:17 +0100

    fix: begin to debug normalizing stage

--- tests/normalizing.rs
+++ tests/normalizing.rs
@@ -1,165 +1,111 @@
-use codespan_reporting::files::{Files, SimpleFiles};
+use codespan_reporting::files::SimpleFiles;
 
-use grustine::ast::file::File;
 use grustine::error::display;
-use grustine::frontend::hir_from_ast::file::hir_from_ast;
-use grustine::parser::langrust;
+use grustine::normalizing;
 
 #[test]
 fn normalize_counter() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
     let counter_id = files.add(
         "counter.gr",
-        std::fs::read_to_string("tests/fixture/counter.gr").expect("unkown file"),
+        std::fs::read_to_string("tests/fixture/normalizing/success/counter.gr").expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(counter_id, &files.source(counter_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-
-    file.normalize(&mut errors).unwrap();
-    insta::assert_yaml_snapshot!(file);
+    match normalizing(counter_id, &mut files) {
+        Ok(file) => insta::assert_yaml_snapshot!(file),
+        Err(errors) => display(&errors, &files),
+    }
 }
 
 #[test]
 fn normalize_blinking() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
     let blinking_id = files.add(
         "blinking.gr",
-        std::fs::read_to_string("tests/fixture/blinking.gr").expect("unkown file"),
+        std::fs::read_to_string("tests/fixture/normalizing/success/blinking.gr").expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(blinking_id, &files.source(blinking_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-
-    file.normalize(&mut errors).unwrap();
-    insta::assert_yaml_snapshot!(file);
+    match normalizing(blinking_id, &mut files) {
+        Ok(file) => insta::assert_yaml_snapshot!(file),
+        Err(errors) => display(&errors, &files),
+    }
 }
 
 #[test]
 fn normalize_button_management() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
-    let blinking_id = files.add(
+    let button_management_id = files.add(
         "button_management.gr",
-        std::fs::read_to_string("tests/fixture/button_management.gr").expect("unkown file"),
+        std::fs::read_to_string("tests/fixture/normalizing/success/button_management.gr").expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(blinking_id, &files.source(blinking_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-
-    file.normalize(&mut errors).unwrap();
-    insta::assert_yaml_snapshot!(file);
+    match normalizing(button_management_id, &mut files) {
+        Ok(file) => insta::assert_yaml_snapshot!(file),
+        Err(errors) => display(&errors, &files),
+    }
 }
 
 #[test]
 fn normalize_button_management_condition_match() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
-    let blinking_id = files.add(
+    let button_management_condition_match_id = files.add(
         "button_management_condition_match.gr",
-        std::fs::read_to_string("tests/fixture/button_management_condition_match.gr")
+        std::fs::read_to_string("tests/fixture/normalizing/success/button_management_condition_match.gr")
             .expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(blinking_id, &files.source(blinking_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-
-    file.normalize(&mut errors).unwrap();
-    insta::assert_yaml_snapshot!(file);
+    match normalizing(button_management_condition_match_id, &mut files) {
+        Ok(file) => insta::assert_yaml_snapshot!(file),
+        Err(errors) => display(&errors, &files),
+    }
 }
 
 #[test]
 fn normalize_button_management_using_function() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
-    let blinking_id = files.add(
+    let button_management_using_function_id = files.add(
         "button_management_using_function.gr",
-        std::fs::read_to_string("tests/fixture/button_management_using_function.gr")
+        std::fs::read_to_string("tests/fixture/normalizing/success/button_management_using_function.gr")
             .expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(blinking_id, &files.source(blinking_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-
-    file.normalize(&mut errors).unwrap();
-    insta::assert_yaml_snapshot!(file);
+    match normalizing(button_management_using_function_id, &mut files) {
+        Ok(file) => insta::assert_yaml_snapshot!(file),
+        Err(errors) => display(&errors, &files),
+    }
 }
 
 #[test]
 fn normalize_pid() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
     let pid_id = files.add(
         "pid.gr",
-        std::fs::read_to_string("tests/fixture/pid.gr").expect("unkown file"),
+        std::fs::read_to_string("tests/fixture/normalizing/success/pid.gr").expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(pid_id, &files.source(pid_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-
-    file.normalize(&mut errors).unwrap();
-    insta::assert_yaml_snapshot!(file);
+    match normalizing(pid_id, &mut files) {
+        Ok(file) => insta::assert_yaml_snapshot!(file),
+        Err(errors) => display(&errors, &files),
+    }
 }
 
 #[test]
 fn error_when_normalize_pid_unused_signal() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
     let pid_unused_signal_id = files.add(
         "pid_unused_signal.gr",
-        std::fs::read_to_string("tests/fixture/pid_unused_signal.gr").expect("unkown file"),
+        std::fs::read_to_string("tests/fixture/normalizing/error/pid_unused_signal.gr").expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(
-            pid_unused_signal_id,
-            &files.source(pid_unused_signal_id).unwrap(),
-        )
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-    file.normalize(&mut errors).unwrap_err();
-
-    display(&errors, &files);
+    match normalizing(pid_unused_signal_id, &mut files) {
+        Ok(file) => insta::assert_yaml_snapshot!(file),
+        Err(errors) => display(&errors, &files),
+    }
 }
