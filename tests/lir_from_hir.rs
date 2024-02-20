414eb69199d58884981968a76b9b39b64280ebb8
Emilie Thome <emilie.thome@hotmail.fr>
2024-02-20 10:27:20 +0100

    feat: starting  debug lir_from_hir

--- tests/lir_from_hir.rs
+++ tests/lir_from_hir.rs
@@ -1,146 +1,83 @@
-use codespan_reporting::files::{Files, SimpleFiles};
+use codespan_reporting::files::SimpleFiles;
 
-use grustine::ast::file::File;
-use grustine::frontend::hir_from_ast::file::hir_from_ast;
-use grustine::frontend::lir_from_hir::file::lir_from_hir;
-use grustine::parser::langrust;
+use grustine::lir_from_hir;
 
 #[test]
 fn lir_from_hir_transformation_for_counter() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
     let counter_id = files.add(
         "counter.gr",
-        std::fs::read_to_string("tests/fixture/counter.gr").expect("unkown file"),
+        std::fs::read_to_string("tests/fixture/lir_from_hir/success/counter.gr").expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(counter_id, &files.source(counter_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-    file.normalize(&mut errors).unwrap();
-
-    let project = lir_from_hir(file);
-    insta::assert_yaml_snapshot!(project);
+    let file = lir_from_hir(counter_id, &mut files);
+    insta::assert_yaml_snapshot!(file)
 }
 
 #[test]
 fn lir_from_hir_transformation_for_blinking() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
     let blinking_id = files.add(
         "blinking.gr",
-        std::fs::read_to_string("tests/fixture/blinking.gr").expect("unkown file"),
+        std::fs::read_to_string("tests/fixture/lir_from_hir/success/blinking.gr").expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(blinking_id, &files.source(blinking_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-    file.normalize(&mut errors).unwrap();
-
-    let project = lir_from_hir(file);
-    insta::assert_yaml_snapshot!(project);
+    let file = lir_from_hir(blinking_id, &mut files);
+    insta::assert_yaml_snapshot!(file)
 }
 
 #[test]
 fn lir_from_hir_transformation_for_button_management() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
-    let blinking_id = files.add(
+    let button_management_id = files.add(
         "button_management.gr",
-        std::fs::read_to_string("tests/fixture/button_management.gr").expect("unkown file"),
+        std::fs::read_to_string("tests/fixture/lir_from_hir/success/button_management.gr").expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(blinking_id, &files.source(blinking_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-    file.normalize(&mut errors).unwrap();
-
-    let project = lir_from_hir(file);
-    insta::assert_yaml_snapshot!(project);
+    let file = lir_from_hir(button_management_id, &mut files);
+    insta::assert_yaml_snapshot!(file)
 }
 
 #[test]
 fn lir_from_hir_transformation_for_button_management_condition_match() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
-    let blinking_id = files.add(
+    let button_management_condition_match_id = files.add(
         "button_management_condition_match.gr",
-        std::fs::read_to_string("tests/fixture/button_management_condition_match.gr")
+        std::fs::read_to_string("tests/fixture/lir_from_hir/success/button_management_condition_match.gr")
             .expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(blinking_id, &files.source(blinking_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-    file.normalize(&mut errors).unwrap();
-
-    let project = lir_from_hir(file);
-    insta::assert_yaml_snapshot!(project);
+    let file = lir_from_hir(button_management_condition_match_id, &mut files);
+    insta::assert_yaml_snapshot!(file)
 }
 
 #[test]
 fn lir_from_hir_transformation_for_button_management_using_function() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
-    let blinking_id = files.add(
+    let button_management_using_function_id = files.add(
         "button_management_using_function.gr",
-        std::fs::read_to_string("tests/fixture/button_management_using_function.gr")
+        std::fs::read_to_string("tests/fixture/lir_from_hir/success/button_management_using_function.gr")
             .expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(blinking_id, &files.source(blinking_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-    file.normalize(&mut errors).unwrap();
-
-    let project = lir_from_hir(file);
-    insta::assert_yaml_snapshot!(project);
+    let file = lir_from_hir(button_management_using_function_id, &mut files);
+    insta::assert_yaml_snapshot!(file)
 }
 
 #[test]
 fn lir_from_hir_transformation_for_pid() {
     let mut files = SimpleFiles::new();
-    let mut errors = vec![];
 
     let pid_id = files.add(
         "pid.gr",
-        std::fs::read_to_string("tests/fixture/pid.gr").expect("unkown file"),
+        std::fs::read_to_string("tests/fixture/lir_from_hir/success/pid.gr").expect("unkown file"),
     );
 
-    let mut file: File = langrust::fileParser::new()
-        .parse(pid_id, &files.source(pid_id).unwrap())
-        .unwrap();
-    file.typing(&mut errors).unwrap();
-    let mut file = hir_from_ast(file);
-    file.generate_dependency_graphs(&mut errors).unwrap();
-    file.causality_analysis(&mut errors).unwrap();
-    file.normalize(&mut errors).unwrap();
-
-    let project = lir_from_hir(file);
-    insta::assert_yaml_snapshot!(project);
+    let file = lir_from_hir(pid_id, &mut files);
+    insta::assert_yaml_snapshot!(file)
 }
