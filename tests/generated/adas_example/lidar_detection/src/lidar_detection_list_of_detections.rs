678099d3a6099b85a3be07d41d27b7ed4229ba4f
Emilie Thome <emilie.thome@hotmail.fr>
2024-02-22 12:09:03 +0100

    fix: closure rust expression returns block expression

--- tests/generated/adas_example/lidar_detection/src/lidar_detection_list_of_detections.rs
+++ tests/generated/adas_example/lidar_detection/src/lidar_detection_list_of_detections.rs
@@ -1,22 +1,18 @@
 use crate::functions::fibonacci;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-
 pub struct LidarDetectionListOfDetectionsInput {
-    pub point_cloud: [i64; 10],
+    pub point_cloud: [i64; 10usize],
 }
 pub struct LidarDetectionListOfDetectionsState {}
 impl LidarDetectionListOfDetectionsState {
     pub fn init() -> LidarDetectionListOfDetectionsState {
-        LidarDetectionListOfDetectionsState {}
+        LidarDetectionListOfDetectionsState {
+        }
     }
-    pub fn step(&mut self, input: LidarDetectionListOfDetectionsInput) -> [i64; 10] {
-        let list_of_detections = input
-            .point_cloud
-            .into_par_iter()
-            .map(fibonacci)
-            .collect::<Vec<_>>()
-            .try_into()
-            .unwrap();
+    pub fn step(
+        &mut self,
+        input: LidarDetectionListOfDetectionsInput,
+    ) -> [i64; 10usize] {
+        let list_of_detections = input.point_cloud.map(fibonacci);
         list_of_detections
     }
 }
--- tests/generated/adas_example/lidar_detection/src/lidar_detection_regions_of_interest.rs
+++ tests/generated/adas_example/lidar_detection/src/lidar_detection_regions_of_interest.rs
@@ -1,17 +1,18 @@
 use crate::functions::factorial;
 pub struct LidarDetectionRegionsOfInterestInput {
-    pub point_cloud: [i64; 10],
+    pub point_cloud: [i64; 10usize],
 }
 pub struct LidarDetectionRegionsOfInterestState {}
 impl LidarDetectionRegionsOfInterestState {
     pub fn init() -> LidarDetectionRegionsOfInterestState {
-        LidarDetectionRegionsOfInterestState {}
+        LidarDetectionRegionsOfInterestState {
+        }
     }
     pub fn step(&mut self, input: LidarDetectionRegionsOfInterestInput) -> i64 {
         let regions_of_interest = input
             .point_cloud
             .into_iter()
-            .fold(0i64, |acc: i64, n: i64| -> i64 { acc + factorial(n) });
+            .fold(0i64, move |acc: i64, n: i64| -> i64 { acc + factorial(n) });
         regions_of_interest
     }
 }
