a6d6cddc76bc83c0c1be85774ca1368a77b86a7f
Emilie Thome <emilie.thome@hotmail.fr>
2024-02-22 12:13:15 +0100

    test: all adas examples can be properly generated

--- tests/generated/adas_example/fusion/src/fusion_fused_information.rs
+++ tests/generated/adas_example/fusion/src/fusion_fused_information.rs
@@ -1,26 +1,21 @@
-use crate::par_zip;
-use rayon::iter::ParallelIterator;
-
 pub struct FusionFusedInformationInput {
-    pub radar_detections: [i64; 10],
-    pub classification: [i64; 10],
-    pub lidar_detections: [i64; 10],
+    pub radar_detections: [i64; 10usize],
+    pub classification: [i64; 10usize],
+    pub lidar_detections: [i64; 10usize],
 }
 pub struct FusionFusedInformationState {}
 impl FusionFusedInformationState {
     pub fn init() -> FusionFusedInformationState {
         FusionFusedInformationState {}
     }
-    pub fn step(&mut self, input: FusionFusedInformationInput) -> [i64; 10] {
-        let fused_information = par_zip!(
-            input.radar_detections,
-            input.classification,
-            input.lidar_detections
-        )
-        .map(|r_c_l: (i64, i64, i64)| -> i64 { r_c_l.0 + r_c_l.1 + r_c_l.2 })
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
+    pub fn step(&mut self, input: FusionFusedInformationInput) -> [i64; 10usize] {
+        let fused_information = {
+            let mut iter = itertools::izip!(
+                input.radar_detections, input.classification, input.lidar_detections
+            );
+            std::array::from_fn(|_| iter.next().unwrap())
+        }
+            .map(move |r_c_l: (i64, i64, i64)| -> i64 { r_c_l.0 + r_c_l.1 + r_c_l.2 });
         fused_information
     }
 }
--- tests/generated/adas_example/object_tracking/src/lib.rs
+++ tests/generated/adas_example/object_tracking/src/lib.rs
@@ -1,4 +1,3 @@
 pub mod functions;
 pub mod object_tracking_object_motion;
-mod par_sort;
 pub mod typedefs;
