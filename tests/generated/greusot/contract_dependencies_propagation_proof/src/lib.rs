29f9d900dc234902e5eae07e77c278cc17a6270a
Emilie Thome <emilie.thome@hotmail.fr>
2024-02-23 11:58:45 +0100

    test: why3 prove program when contracts are properly propagated

---
+++ tests/generated/greusot/contract_dependencies_propagation_proof/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod functions;
+pub mod transitive_o;
+pub mod typedefs;
+pub mod using_o;
---
+++ tests/generated/greusot/contract_dependencies_propagation_proof/src/transitive_o.rs
@@ -0,0 +1,19 @@
+use creusot_contracts::ensures;
+use creusot_contracts::requires;
+pub struct TransitiveOInput {
+    pub i: i64,
+    pub j: i64,
+}
+pub struct TransitiveOState {}
+impl TransitiveOState {
+    pub fn init() -> TransitiveOState {
+        TransitiveOState {}
+    }
+    #[requires(input.i<input.j)]
+    #[requires(input.j<1000i64)]
+    #[ensures(result<1000i64)]
+    pub fn step(&mut self, input: TransitiveOInput) -> i64 {
+        let o = input.i;
+        o
+    }
+}
