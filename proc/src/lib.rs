62ddadff6e69194603cd2bd3e89e6b352a5933d1
Emilie Thome <emilie.thome@hotmail.fr>
2024-04-25 17:55:36 +0200

    docs: proc macro grust documentation

--- proc/src/lib.rs
+++ proc/src/lib.rs
@@ -1,5 +1,15 @@
+//! This crate provides the [grust!] macro for generating Rust code
+//! from GRust programs.
+
 use compiler::{handle_tokens, TokenStream};
 
+/// The GRust compiler as an integrated Rust macro.
+/// 
+/// Calls the GRust compiler and performs analysis that guarantee
+/// the correct execution of the program.
+/// 
+/// The generated structures and functions are accessible immediately
+/// in the Rust program, which eases the interface with other programs.
 #[proc_macro]
 pub fn grust(tokens: TokenStream) -> TokenStream {
     handle_tokens(tokens)
