67c0647a01eaa2a5d3d299e9e83eaf8061cae38d
Emilie Thome <emilie.e.thome@renault.com>
2024-04-25 13:01:43 +0200

    feat: init proc macro for grust compiler

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,14 +1,26 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+extern crate proc_macro;
+
+pub use proc_macro::TokenStream;
+
+pub fn handle_tokens(_tokens: TokenStream) -> TokenStream {
+    todo!()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn dump_ast(path_name: &str, tokens: &proc_macro2::TokenStream) {
+    use std::{fs::OpenOptions, io::Write, path::Path, process::Command};
+    let path = Path::new(path_name);
+    
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path)
+        .expect(&format!("failed to open `{path_name}`"));
+    writeln!(&mut file, "{}", tokens).expect(&format!("failed to write to `{path_name}`"));
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let mut rustfmt = Command::new("rustfmt")
+        .arg(path)
+        .spawn()
+        .expect("failed to spawn `rustfmt`");
+    rustfmt.wait().expect("`rustfmt` failed");
 }
