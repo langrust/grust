b9953c80b96ba1457ed24bd6617e48bb629a1328
Emilie Thome <emilie.e.thome@renault.com>
2024-04-26 09:37:12 +0200

    refactor: syn like statement

--- compiler/src/ast/statement.rs
+++ compiler/src/ast/statement.rs
@@ -1,15 +1,15 @@
-use crate::ast::expression::Expression;
-use crate::common::{location::Location, r#type::Type};
+use syn::Token;
 
-#[derive(Debug, PartialEq, Clone, serde::Serialize)]
-/// LanGRust statement AST.
+use crate::ast::expression::Expression;
+
+#[derive(Debug, PartialEq, Clone)]
+/// GRust statement AST.
 pub struct Statement {
-    /// Identifier of the new element.
-    pub id: String,
-    /// Element type.
-    pub element_type: Type,
-    /// The expression defining the element.
+    pub let_token: Token![let],
+    /// Identifier of the variable and its type.
+    pub typed_ident: syn::PatType,
+    pub eq_token: Token![=],
+    /// The expression defining the variable.
     pub expression: Expression,
-    /// Statement location.
-    pub location: Location,
+    pub comma_token: Token![;],
 }
