a3d914f551f71ed38be6b166926a5bd69c83c007
Emilie Thome <emilie.e.thome@renault.com>
2024-04-26 09:19:08 +0200

    refactor: contract syn to be parsed

--- compiler/src/ast/contract.rs
+++ compiler/src/ast/contract.rs
@@ -1,62 +1,40 @@
-use crate::common::{
-    constant::Constant,
-    location::Location,
-    operator::{BinaryOperator, UnaryOperator},
-};
+use syn::{token, Token};
 
+use super::keyword;
+
+#[derive(Debug, PartialEq, Clone)]
+/// GRust contract's term.
+pub struct Implication {
+    pub left: Box<Term>,
+    pub arrow: Token![=>],
+    pub right: Box<Term>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+/// GRust term's kind.
+pub enum Term {
+    Expression(syn::Expr),
+    Implication(Implication),
+}
+
+#[derive(Debug, PartialEq, Clone)]
 pub enum ClauseKind {
-    Invariant,
-    Requires,
-    Ensures,
+    Requires(keyword::requires),
+    Ensures(keyword::ensures),
+    Invariant(keyword::invariant),
+    Assert(keyword::assert),
 }
 
-#[derive(Debug, PartialEq, Clone, serde::Serialize)]
-/// LanGRust term's kind.
-pub enum TermKind {
-    /// Constant term: 3
-    Constant {
-        /// The constant
-        constant: Constant,
-    },
-    /// Identifier term: x
-    Identifier {
-        /// The identifier
-        id: String,
-    },
-    /// Unary term: !x
-    Unary {
-        /// The operator
-        op: UnaryOperator,
-        /// The term
-        term: Box<Term>,
-    },
-    /// Binary term: x == y
-    Binary {
-        /// The operator
-        op: BinaryOperator,
-        /// Left term
-        left: Box<Term>,
-        /// Right term
-        right: Box<Term>,
-    },
+#[derive(Debug, PartialEq, Clone)]
+pub struct Clause {
+    pub kind: ClauseKind,
+    pub brace: token::Brace,
+    pub term: Term,
 }
 
-#[derive(Debug, PartialEq, Clone, serde::Serialize)]
-/// LanGRust contract's term.
-pub struct Term {
-    /// The kind of the term
-    pub kind: TermKind,
-    /// The location in source code
-    pub location: Location,
-}
-
-#[derive(Debug, Default, PartialEq, Clone, serde::Serialize)]
-/// LanGRust contract to prove using Creusot.
+#[derive(Debug, Default, PartialEq, Clone)]
+/// GRust contract to prove using Creusot.
 pub struct Contract {
-    /// Requirements clauses to suppose
-    pub requires: Vec<Term>,
-    /// Ensures clauses to prove
-    pub ensures: Vec<Term>,
-    /// Invariant clauses to prove
-    pub invariant: Vec<Term>,
+    /// Contract's clauses.
+    pub clauses: Vec<Clause>,
 }
