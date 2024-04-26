0b34bcac351cb26a47d3844a4abb8e36d5956282
Emilie Thome <emilie.e.thome@renault.com>
2024-04-26 09:31:12 +0200

    refactor: function syn style

--- compiler/src/ast/function.rs
+++ compiler/src/ast/function.rs
@@ -1,17 +1,22 @@
-use crate::ast::{expression::Expression, statement::Statement};
-use crate::common::{location::Location, r#type::Type};
+use syn::punctuated::Punctuated;
+use syn::{token, Token};
 
-#[derive(Debug, PartialEq, serde::Serialize)]
-/// LanGRust function AST.
+use crate::ast::{expression::Expression, statement::Statement};
+
+#[derive(Debug, PartialEq)]
+/// GRust function AST.
 pub struct Function {
     /// Function identifier.
-    pub id: String,
-    /// Function's inputs identifiers and their types.
-    pub inputs: Vec<(String, Type)>,
+    pub ident: syn::Ident,
+    pub args_paren: token::Paren,
+    /// Component's inputs identifiers and their types.
+    pub args: Punctuated<syn::PatType, Token![,]>,
+    pub arrow_token: Token![->],
+    pub output_type: syn::PatType,
+    pub brace: token::Brace,
     /// Function's statements.
     pub statements: Vec<Statement>,
+    pub return_token: Token![return],
     /// Function's returned expression and its type.
-    pub returned: (Type, Expression),
-    /// Function location.
-    pub location: Location,
+    pub returned: Expression,
 }
