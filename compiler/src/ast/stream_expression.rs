56895370de25bb63baeb1393621049eaa0dac3c3
Emilie Thome <emilie.e.thome@renault.com>
2024-04-29 14:31:36 +0200

    refactor: expressions ready to be parsed

--- compiler/src/ast/stream_expression.rs
+++ compiler/src/ast/stream_expression.rs
@@ -1,25 +1,53 @@
-use crate::ast::expression::ExpressionKind;
-use crate::common::location::Location;
+use crate::ast::expression::{
+    Application, Array, Enumeration, FieldAccess, Fold, Map, Match, Sort, Structure, Tuple,
+    TupleElementAccess, TypedAbstraction, When, Zip,
+};
+use crate::common::constant::Constant;
+
+/// Initialized buffer stream expression.
+#[derive(Debug, PartialEq, Clone)]
+pub struct FollowedBy {
+    /// The initialization constant.
+    constant: Box<StreamExpression>,
+    /// The buffered expression.
+    expression: Box<StreamExpression>,
+}
 
 #[derive(Debug, PartialEq, Clone)]
 /// GRust stream expression kind AST.
-pub enum StreamExpressionKind {
-    /// Expression.
-    Expression {
-        /// The expression kind.
-        expression: ExpressionKind<StreamExpression>,
-    },
+pub enum StreamExpression {
+    /// Constant expression.
+    Constant(Constant),
+    /// Identifier expression.
+    Identifier(syn::Ident),
+    /// Application expression.
+    Application(Application<StreamExpression>),
+    /// Abstraction expression with inputs types.
+    TypedAbstraction(TypedAbstraction<StreamExpression>),
+    /// Structure expression.
+    Structure(Structure<StreamExpression>),
+    /// Tuple expression.
+    Tuple(Tuple<StreamExpression>),
+    /// Enumeration expression.
+    Enumeration(Enumeration),
+    /// Array expression.
+    Array(Array<StreamExpression>),
+    /// Pattern matching expression.
+    Match(Match<StreamExpression>),
+    /// When present expression.
+    When(When<StreamExpression>),
+    /// Field access expression.
+    FieldAccess(FieldAccess<StreamExpression>),
+    /// Tuple element access expression.
+    TupleElementAccess(TupleElementAccess<StreamExpression>),
+    /// Array map operator expression.
+    Map(Map<StreamExpression>),
+    /// Array fold operator expression.
+    Fold(Fold<StreamExpression>),
+    /// Array sort operator expression.
+    Sort(Sort<StreamExpression>),
+    /// Arrays zip operator expression.
+    Zip(Zip<StreamExpression>),
     /// Initialized buffer stream expression.
-    FollowedBy {
-        /// The initialization constant.
-        constant: Box<StreamExpression>,
-        /// The buffered expression.
-        expression: Box<StreamExpression>,
-    },
-}
-#[derive(Debug, PartialEq, Clone)]
-/// GRust stream expression AST.
-pub struct StreamExpression {
-    /// Stream expression kind.
-    pub kind: StreamExpressionKind,
+    FollowedBy(FollowedBy),
 }
