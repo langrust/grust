ced71c9228c470ec201e21584cbd82548b71f675
Emilie Thome <emilie.thome@hotmail.fr>
2024-04-29 14:31:46 +0200

    feat: parse constant

--- compiler/src/common/constant.rs
+++ compiler/src/common/constant.rs
@@ -1,4 +1,4 @@
-use std::fmt::{self, Display};
+use syn::parse::Parse;
 
 use crate::common::r#type::Type;
 
@@ -9,88 +9,53 @@ use crate::common::r#type::Type;
 /// It reprensents all possible constant:
 /// - [Constant::Integer] are [i64] integers, `1` becomes `Constant::Integer(1)`
 /// - [Constant::Float] are [f64] floats, `1.0` becomes `Constant::Float(1.0)`
-/// - [Constant::Boolean] is the [bool] type for booleans, `true` becomes
-/// `Constant::Boolean(true)`
-/// - [Constant::String] are strings of type [String], `"hello world"` becomes
-/// `Constant::String(String::from("hello world"))`
+/// - [Constant::Boolean] is the [bool] type for booleans, `true` becomes `Constant::Boolean(true)`
 /// - [Constant::Unit] is the unit type, `()` becomes `Constant::Unit`
-///
-/// # Example
-/// ```rust
-/// use grustine::common::constant::Constant;
-///
-/// let constant = Constant::String(String::from("Hello world"));
-/// ```
 #[derive(Debug, Clone, PartialEq)]
 pub enum Constant {
     /// [i64] integers
-    Integer(i64),
+    Integer(syn::LitInt),
     /// [f64] floats
-    Float(f64),
+    Float(syn::LitFloat),
     /// [bool] booleans
-    Boolean(bool),
-    /// [String] strings
-    String(String),
+    Boolean(syn::LitBool),
     /// Unit constant
-    Unit,
+    Unit(syn::token::Paren),
 }
 impl Constant {
     /// Get the [Type] of the constant.
-    ///
-    /// # Example
-    /// ```rust
-    /// use grustine::common::{constant::Constant, r#type::Type};
-    ///
-    /// let c: Constant = Constant::Integer(6);
-    /// assert_eq!(c.get_type(), Type::Integer);
-    /// ```
     pub fn get_type(&self) -> Type {
         match self {
             Constant::Integer(_) => Type::Integer,
             Constant::Float(_) => Type::Float,
             Constant::Boolean(_) => Type::Boolean,
-            Constant::String(_) => Type::String,
-            Constant::Unit => Type::Unit,
+            Constant::Unit(_) => Type::Unit,
         }
     }
 }
-impl Display for Constant {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Constant::Integer(i) => write!(f, "{i}i64"),
-            Constant::Float(l) => write!(f, "{l}f64"),
-            Constant::Boolean(b) => write!(f, "{b}"),
-            Constant::String(s) => write!(f, "\"{s}\""),
-            Constant::Unit => write!(f, "()"),
+impl Parse for Constant {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        if input.fork().call(syn::LitInt::parse).is_ok() {
+            let i: syn::LitInt = input.parse()?;
+            Ok(Constant::Integer(i))
+        } else if input.fork().call(syn::LitFloat::parse).is_ok() {
+            let f: syn::LitFloat = input.parse()?;
+            Ok(Constant::Float(f))
+        } else if input.fork().call(syn::LitBool::parse).is_ok() {
+            let b: syn::LitBool = input.parse()?;
+            Ok(Constant::Boolean(b))
+        } else {
+            let content;
+            let parens = syn::parenthesized!(content in input);
+            if content.is_empty() {
+                if input.is_empty() {
+                    Ok(Constant::Unit(parens))
+                } else {
+                    Err(input.error("expected constant: integer, float, boolean or unit"))
+                }
+            } else {
+                Err(input.error("expected unit `()`"))
+            }
         }
-    }
-}
-
-#[cfg(test)]
-mod get_type {
-    use crate::common::{constant::Constant, r#type::Type};
-
-    #[test]
-    fn should_return_integer_type_to_integer_constant() {
-        assert_eq!(Type::Integer, Constant::Integer(0).get_type());
-    }
-    #[test]
-    fn should_return_float_type_to_float_constant() {
-        assert_eq!(Type::Float, Constant::Float(0.0).get_type());
-    }
-    #[test]
-    fn should_return_boolean_type_to_boolean_constant() {
-        assert_eq!(Type::Boolean, Constant::Boolean(true).get_type());
-    }
-    #[test]
-    fn should_return_string_type_to_string_constant() {
-        assert_eq!(
-            Type::String,
-            Constant::String(String::from("Hello world!")).get_type()
-        );
-    }
-    #[test]
-    fn should_return_unit_type_to_unit_constant() {
-        assert_eq!(Type::Unit, Constant::Unit.get_type());
     }
 }
