3af568ec7e8d7483ddad4baabcc33c90b1aa08b3
Emilie Thome <emilie.e.thome@renault.com>
2024-04-30 10:52:16 +0200

    feat: parsing typdefs

--- compiler/src/ast/typedef.rs
+++ compiler/src/ast/typedef.rs
@@ -1,10 +1,9 @@
-use syn::{punctuated::Punctuated, token, Token};
+use syn::{braced, bracketed, parse::Parse, punctuated::Punctuated, token, Token};
 
 use crate::common::r#type::Type;
 
-use super::keyword;
+use super::{ident_colon::IdentColon, keyword};
 
-#[derive(Debug, PartialEq, Clone)]
 /// GRust user defined type AST.
 pub enum Typedef {
     /// Represents a structure definition.
@@ -14,7 +13,7 @@ pub enum Typedef {
         ident: syn::Ident,
         brace: token::Brace,
         /// The structure's fields: a field has an identifier and a type.
-        fields: Punctuated<(syn::Ident, Token![:], Type), Token![,]>,
+        fields: Punctuated<IdentColon<Type>, Token![,]>,
     },
     /// Represents an enumeration definition.
     Enumeration {
@@ -37,4 +36,57 @@ pub enum Typedef {
         /// The array's size.
         size: syn::LitInt,
     },
+}
+impl Parse for Typedef {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        if input.peek(Token![struct]) {
+            let struct_token: Token![struct] = input.parse()?;
+            let ident: syn::Ident = input.parse()?;
+            let content;
+            let brace: token::Brace = braced!(content in input);
+            let fields: Punctuated<IdentColon<Type>, Token![,]> =
+                Punctuated::parse_terminated(&content)?;
+            Ok(Typedef::Structure {
+                struct_token,
+                ident,
+                brace,
+                fields,
+            })
+        } else if input.peek(Token![enum]) {
+            let enum_token: Token![enum] = input.parse()?;
+            let ident: syn::Ident = input.parse()?;
+            let content;
+            let brace: token::Brace = braced!(content in input);
+            let elements: Punctuated<syn::Ident, Token![,]> =
+                Punctuated::parse_terminated(&content)?;
+            Ok(Typedef::Enumeration {
+                enum_token,
+                ident,
+                brace,
+                elements,
+            })
+        } else if input.peek(keyword::array) {
+            let array_token: keyword::array = input.parse()?;
+            let ident: syn::Ident = input.parse()?;
+            let content;
+            let bracket_token: token::Bracket = bracketed!(content in input);
+            let array_type: Type = content.parse()?;
+            let semi_token: Token![;] = content.parse()?;
+            let size: syn::LitInt = content.parse()?;
+            if content.is_empty() {
+                Ok(Typedef::Array {
+                    array_token,
+                    ident,
+                    bracket_token,
+                    array_type,
+                    semi_token,
+                    size,
+                })
+            } else {
+                Err(input.error("expected array alias defition"))
+            }
+        } else {
+            Err(input.error("expected type definition"))
+        }
+    }
 }
