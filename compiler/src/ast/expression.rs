ff85472e019e951b702b5d3f207175c5b5b9f618
Emilie Thome <emilie.thome@hotmail.fr>
2024-04-30 09:39:33 +0200

    feat: parsing expressions

--- compiler/src/ast/expression.rs
+++ compiler/src/ast/expression.rs
@@ -5,6 +5,8 @@ use syn::{braced, bracketed, parenthesized, token, Token};
 use crate::ast::{ident_colon::IdentColon, pattern::Pattern};
 use crate::common::{constant::Constant, r#type::Type};
 
+use super::keyword;
+
 /// Application expression.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Application<E> {
@@ -215,26 +217,75 @@ where
     }
 }
 
+/// Arm for matching expression.
+#[derive(Debug, PartialEq, Clone)]
+pub struct Arm<E> {
+    /// The pattern to match.
+    pattern: Pattern,
+    /// The optional guard.
+    guard: Option<E>,
+    /// The expression.
+    expression: E,
+}
+impl<E> Parse for Arm<E>
+where
+    E: Parse,
+{
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let pattern = input.parse()?;
+        let guard = {
+            if input.fork().peek(Token![if]) {
+                let _: Token![if] = input.parse()?;
+                let guard = input.parse()?;
+                Some(guard)
+            } else {
+                None
+            }
+        };
+        let _: Token![=>] = input.parse()?;
+        let expression = input.parse()?;
+        Ok(Arm {
+            pattern,
+            guard,
+            expression,
+        })
+    }
+}
+
 /// Pattern matching expression.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Match<E> {
     /// The expression to match.
     expression: Box<E>,
     /// The different matching cases.
-    arms: Vec<(Pattern, Option<E>, E)>,
+    arms: Vec<Arm<E>>,
 }
-/// When present expression.
-#[derive(Debug, PartialEq, Clone)]
-pub struct When<E> {
-    /// The identifier of the value when present
-    id: String,
-    /// The optional expression.
-    option: Box<E>,
-    /// The expression when present.
-    present: Box<E>,
-    /// The default expression.
-    default: Box<E>,
+impl<E> Match<E>
+where
+    E: Parse,
+{
+    pub fn peek(input: syn::parse::ParseStream) -> bool {
+        let forked = input.fork();
+        forked.peek(Token![match])
+    }
 }
+impl<E> Parse for Match<E>
+where
+    E: Parse,
+{
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let _: Token![match] = input.parse()?;
+        let expression = Box::new(input.parse()?);
+        let content;
+        let _ = braced!(content in input);
+        let arms: Punctuated<Arm<E>, Token![,]> = Punctuated::parse_terminated(&content)?;
+        Ok(Match {
+            expression,
+            arms: arms.into_iter().collect(),
+        })
+    }
+}
+
 /// Field access expression.
 #[derive(Debug, PartialEq, Clone)]
 pub struct FieldAccess<E> {
@@ -243,6 +294,36 @@ pub struct FieldAccess<E> {
     /// The field to access.
     field: String,
 }
+impl<E> FieldAccess<E>
+where
+    E: Parse,
+{
+    pub fn peek(input: syn::parse::ParseStream) -> bool {
+        let forked = input.fork();
+        if forked.call(E::parse).is_err() {
+            return false;
+        }
+        if forked.call(token::Dot::parse).is_err() {
+            return false;
+        }
+        forked.call(syn::Ident::parse).is_ok()
+    }
+}
+impl<E> Parse for FieldAccess<E>
+where
+    E: Parse,
+{
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let expression = Box::new(input.parse()?);
+        let _: Token![.] = input.parse()?;
+        let field: syn::Ident = input.parse()?;
+        Ok(FieldAccess {
+            expression,
+            field: field.to_string(),
+        })
+    }
+}
+
 /// Tuple element access expression.
 #[derive(Debug, PartialEq, Clone)]
 pub struct TupleElementAccess<E> {
@@ -251,6 +332,36 @@ pub struct TupleElementAccess<E> {
     /// The element to access.
     element_number: usize,
 }
+impl<E> TupleElementAccess<E>
+where
+    E: Parse,
+{
+    pub fn peek(input: syn::parse::ParseStream) -> bool {
+        let forked = input.fork();
+        if forked.call(E::parse).is_err() {
+            return false;
+        }
+        if forked.call(token::Dot::parse).is_err() {
+            return false;
+        }
+        forked.call(syn::LitInt::parse).is_ok()
+    }
+}
+impl<E> Parse for TupleElementAccess<E>
+where
+    E: Parse,
+{
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let expression = Box::new(input.parse()?);
+        let _: Token![.] = input.parse()?;
+        let element_number: syn::LitInt = input.parse()?;
+        Ok(TupleElementAccess {
+            expression,
+            element_number: element_number.base10_parse().unwrap(),
+        })
+    }
+}
+
 /// Array map operator expression.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Map<E> {
@@ -259,6 +370,43 @@ pub struct Map<E> {
     /// The function expression.
     function_expression: Box<E>,
 }
+impl<E> Map<E>
+where
+    E: Parse,
+{
+    pub fn peek(input: syn::parse::ParseStream) -> bool {
+        let forked = input.fork();
+        if forked.call(E::parse).is_err() {
+            return false;
+        }
+        if forked.call(token::Dot::parse).is_err() {
+            return false;
+        }
+        forked.peek(keyword::map)
+    }
+}
+impl<E> Parse for Map<E>
+where
+    E: Parse,
+{
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let expression = Box::new(input.parse()?);
+        let _: Token![.] = input.parse()?;
+        let _: keyword::map = input.parse()?;
+        let content;
+        let _ = parenthesized!(content in input);
+        let function_expression = Box::new(content.parse()?);
+        if content.is_empty() {
+            Ok(Map {
+                expression,
+                function_expression,
+            })
+        } else {
+            Err(input.error("expected only one expression"))
+        }
+    }
+}
+
 /// Array fold operator expression.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Fold<E> {
@@ -269,6 +417,46 @@ pub struct Fold<E> {
     /// The function expression.
     function_expression: Box<E>,
 }
+impl<E> Fold<E>
+where
+    E: Parse,
+{
+    pub fn peek(input: syn::parse::ParseStream) -> bool {
+        let forked = input.fork();
+        if forked.call(E::parse).is_err() {
+            return false;
+        }
+        if forked.call(token::Dot::parse).is_err() {
+            return false;
+        }
+        forked.peek(keyword::fold)
+    }
+}
+impl<E> Parse for Fold<E>
+where
+    E: Parse,
+{
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let expression = Box::new(input.parse()?);
+        let _: Token![.] = input.parse()?;
+        let _: keyword::fold = input.parse()?;
+        let content;
+        let _ = parenthesized!(content in input);
+        let initialization_expression = Box::new(content.parse()?);
+        let _: Token![,] = input.parse()?;
+        let function_expression = Box::new(content.parse()?);
+        if content.is_empty() {
+            Ok(Fold {
+                expression,
+                initialization_expression,
+                function_expression,
+            })
+        } else {
+            Err(input.error("expected only two expressions"))
+        }
+    }
+}
+
 /// Array sort operator expression.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Sort<E> {
@@ -277,11 +465,71 @@ pub struct Sort<E> {
     /// The function expression.
     function_expression: Box<E>,
 }
+impl<E> Sort<E>
+where
+    E: Parse,
+{
+    pub fn peek(input: syn::parse::ParseStream) -> bool {
+        let forked = input.fork();
+        if forked.call(E::parse).is_err() {
+            return false;
+        }
+        if forked.call(token::Dot::parse).is_err() {
+            return false;
+        }
+        forked.peek(keyword::sort)
+    }
+}
+impl<E> Parse for Sort<E>
+where
+    E: Parse,
+{
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let expression = Box::new(input.parse()?);
+        let _: Token![.] = input.parse()?;
+        let _: keyword::sort = input.parse()?;
+        let content;
+        let _ = parenthesized!(content in input);
+        let function_expression = Box::new(content.parse()?);
+        if content.is_empty() {
+            Ok(Sort {
+                expression,
+                function_expression,
+            })
+        } else {
+            Err(input.error("expected only one expression"))
+        }
+    }
+}
+
 /// Arrays zip operator expression.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Zip<E> {
     /// The array expressions.
     arrays: Vec<E>,
+}
+impl<E> Zip<E>
+where
+    E: Parse,
+{
+    pub fn peek(input: syn::parse::ParseStream) -> bool {
+        let forked = input.fork();
+        forked.peek(keyword::zip)
+    }
+}
+impl<E> Parse for Zip<E>
+where
+    E: Parse,
+{
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let _: keyword::zip = input.parse()?;
+        let content;
+        let _ = parenthesized!(content in input);
+        let arrays: Punctuated<E, Token![,]> = Punctuated::parse_terminated(&content)?;
+        Ok(Zip {
+            arrays: arrays.into_iter().collect(),
+        })
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -305,8 +553,6 @@ pub enum Expression {
     Array(Array<Expression>),
     /// Pattern matching expression.
     Match(Match<Expression>),
-    /// When present expression.
-    When(When<Expression>),
     /// Field access expression.
     FieldAccess(FieldAccess<Expression>),
     /// Tuple element access expression.
@@ -323,25 +569,39 @@ pub enum Expression {
 
 impl Parse for Expression {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        if input.fork().call(Constant::parse).is_ok() {
-            Ok(Expression::Constant(input.parse()?))
+        if TypedAbstraction::<Expression>::peek(input) {
+            Ok(Expression::TypedAbstraction(input.parse()?))
+        } else if Sort::<Expression>::peek(input) {
+            Ok(Expression::Sort(input.parse()?))
+        } else if Map::<Expression>::peek(input) {
+            Ok(Expression::Map(input.parse()?))
+        } else if Fold::<Expression>::peek(input) {
+            Ok(Expression::Fold(input.parse()?))
+        } else if TupleElementAccess::<Expression>::peek(input) {
+            Ok(Expression::TupleElementAccess(input.parse()?))
+        } else if FieldAccess::<Expression>::peek(input) {
+            Ok(Expression::FieldAccess(input.parse()?))
+        } else if Zip::<Expression>::peek(input) {
+            Ok(Expression::Zip(input.parse()?))
+        } else if Application::<Expression>::peek(input) {
+            Ok(Expression::Application(input.parse()?))
+        } else if Match::<Expression>::peek(input) {
+            Ok(Expression::Match(input.parse()?))
+        } else if Tuple::<Expression>::peek(input) {
+            Ok(Expression::Tuple(input.parse()?))
+        } else if Array::<Expression>::peek(input) {
+            Ok(Expression::Array(input.parse()?))
+        } else if Structure::<Expression>::peek(input) {
+            Ok(Expression::Structure(input.parse()?))
+        } else if Enumeration::peek(input) {
+            Ok(Expression::Enumeration(input.parse()?))
         } else if input.fork().call(syn::Ident::parse).is_ok() {
             let ident: syn::Ident = input.parse()?;
             Ok(Expression::Identifier(ident.to_string()))
-        } else if Application::<Expression>::peek(input) {
-            Ok(Expression::Application(input.parse()?))
-        } else if TypedAbstraction::<Expression>::peek(input) {
-            Ok(Expression::TypedAbstraction(input.parse()?))
-        } else if Structure::<Expression>::peek(input) {
-            Ok(Expression::Structure(input.parse()?))
-        } else if Tuple::<Expression>::peek(input) {
-            Ok(Expression::Tuple(input.parse()?))
-        } else if Enumeration::peek(input) {
-            Ok(Expression::Enumeration(input.parse()?))
-        } else if Array::<Expression>::peek(input) {
-            Ok(Expression::Array(input.parse()?))
+        } else if input.fork().call(Constant::parse).is_ok() {
+            Ok(Expression::Constant(input.parse()?))
         } else {
-            todo!()
+            Err(input.error("expected expression"))
         }
     }
 }
