13d3fb0140c44ad0fbbd3a80c41aece62c0c89af
Emilie Thome <emilie.e.thome@renault.com>
2024-04-30 14:21:38 +0200

    refactor: join statement and equation definition

--- compiler/src/ast/equation.rs
+++ compiler/src/ast/equation.rs
@@ -1,20 +1,7 @@
 use syn::parse::Parse;
 use syn::Token;
 
-use crate::ast::stream_expression::StreamExpression;
-use crate::common::r#type::Type;
-
-use super::ident_colon::IdentColon;
-
-pub struct LetDeclaration {
-    pub let_token: Token![let],
-    /// Identifier of the signal and its type.
-    pub typed_ident: IdentColon<Type>,
-    pub eq_token: Token![=],
-    /// The stream expression defining the signal.
-    pub expression: StreamExpression,
-    pub semi_token: Token![;],
-}
+use crate::ast::{statement::LetDeclaration, stream_expression::StreamExpression};
 
 pub struct Instanciation {
     /// Identifier of the signal.
@@ -24,40 +11,33 @@ pub struct Instanciation {
     pub expression: StreamExpression,
     pub semi_token: Token![;],
 }
+impl Parse for Instanciation {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let ident: syn::Ident = input.parse()?;
+        let eq_token: Token![=] = input.parse()?;
+        let expression: StreamExpression = input.parse()?;
+        let semi_token: Token![;] = input.parse()?;
+
+        Ok(Instanciation {
+            ident,
+            eq_token,
+            expression,
+            semi_token,
+        })
+    }
+}
 
 /// GRust equation AST.
 pub enum Equation {
-    LocalDef(LetDeclaration),
+    LocalDef(LetDeclaration<StreamExpression>),
     OutputDef(Instanciation),
 }
 impl Parse for Equation {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         if input.peek(Token![let]) {
-            let let_token: Token![let] = input.parse()?;
-            let typed_ident: IdentColon<Type> = input.parse()?;
-            let eq_token: Token![=] = input.parse()?;
-            let expression: StreamExpression = input.parse()?;
-            let semi_token: Token![;] = input.parse()?;
-
-            Ok(Equation::LocalDef(LetDeclaration {
-                let_token,
-                typed_ident,
-                eq_token,
-                expression,
-                semi_token,
-            }))
+            Ok(Equation::LocalDef(input.parse()?))
         } else {
-            let ident: syn::Ident = input.parse()?;
-            let eq_token: Token![=] = input.parse()?;
-            let expression: StreamExpression = input.parse()?;
-            let semi_token: Token![;] = input.parse()?;
-
-            Ok(Equation::OutputDef(Instanciation {
-                ident,
-                eq_token,
-                expression,
-                semi_token,
-            }))
+            Ok(Equation::OutputDef(input.parse()?))
         }
     }
 }
--- compiler/src/ast/statement.rs
+++ compiler/src/ast/statement.rs
@@ -5,31 +5,70 @@ use crate::ast::expression::Expression;
 use crate::common::r#type::Type;
 
 use super::ident_colon::IdentColon;
+use super::stream_expression::StreamExpression;
 
-/// GRust statement AST.
-pub struct Statement {
+/// GRust declaration AST.
+pub struct LetDeclaration<E> {
     pub let_token: Token![let],
-    /// Identifier of the variable and its type.
+    /// Identifier of the signal and its type.
     pub typed_ident: IdentColon<Type>,
     pub eq_token: Token![=],
-    /// The expression defining the variable.
-    pub expression: Expression,
+    /// The stream expression defining the signal.
+    pub expression: E,
     pub semi_token: Token![;],
 }
-impl Parse for Statement {
+impl<E> Parse for LetDeclaration<E>
+where
+    E: Parse,
+{
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let let_token: Token![let] = input.parse()?;
         let typed_ident: IdentColon<Type> = input.parse()?;
         let eq_token: Token![=] = input.parse()?;
-        let expression: Expression = input.parse()?;
+        let expression: E = input.parse()?;
         let semi_token: Token![;] = input.parse()?;
 
-        Ok(Statement {
+        Ok(LetDeclaration {
             let_token,
             typed_ident,
             eq_token,
             expression,
             semi_token,
         })
+    }
+}
+
+/// GRust return statement AST.
+pub struct ReturnInstruction {
+    pub return_token: Token![return],
+    /// The expression defining the variable.
+    pub expression: Expression,
+}
+impl Parse for ReturnInstruction {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let return_token: Token![return] = input.parse()?;
+        let expression: Expression = input.parse()?;
+
+        Ok(ReturnInstruction {
+            return_token,
+            expression,
+        })
+    }
+}
+
+/// GRust statement AST.
+pub enum Statement {
+    /// GRust declaration AST.
+    Declaration(LetDeclaration<StreamExpression>),
+    /// GRust return statement AST.
+    Return(ReturnInstruction),
+}
+impl Parse for Statement {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        if input.peek(Token![let]) {
+            Ok(Statement::Declaration(input.parse()?))
+        } else {
+            Ok(Statement::Return(input.parse()?))
+        }
     }
 }
