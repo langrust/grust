19bf015ebf80f4f91f94a0e2e07fe2b286223299
Emilie Thome <emilie.e.thome@renault.com>
2024-04-30 14:05:17 +0200

    feat: parsing equations

--- compiler/src/ast/equation.rs
+++ compiler/src/ast/equation.rs
@@ -1,20 +1,21 @@
+use syn::parse::Parse;
 use syn::Token;
 
 use crate::ast::stream_expression::StreamExpression;
 use crate::common::r#type::Type;
 
-#[derive(Debug, PartialEq, Clone)]
+use super::ident_colon::IdentColon;
+
 pub struct LetDeclaration {
     pub let_token: Token![let],
     /// Identifier of the signal and its type.
-    pub typed_ident: (syn::Ident, Token![:], Type),
+    pub typed_ident: IdentColon<Type>,
     pub eq_token: Token![=],
     /// The stream expression defining the signal.
     pub expression: StreamExpression,
     pub semi_token: Token![;],
 }
 
-#[derive(Debug, PartialEq, Clone)]
 pub struct Instanciation {
     /// Identifier of the signal.
     pub ident: syn::Ident,
@@ -24,9 +25,39 @@ pub struct Instanciation {
     pub semi_token: Token![;],
 }
 
-#[derive(Debug, PartialEq, Clone)]
 /// GRust equation AST.
 pub enum Equation {
     LocalDef(LetDeclaration),
     OutputDef(Instanciation),
+}
+impl Parse for Equation {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        if input.peek(Token![let]) {
+            let let_token: Token![let] = input.parse()?;
+            let typed_ident: IdentColon<Type> = input.parse()?;
+            let eq_token: Token![=] = input.parse()?;
+            let expression: StreamExpression = input.parse()?;
+            let semi_token: Token![;] = input.parse()?;
+
+            Ok(Equation::LocalDef(LetDeclaration {
+                let_token,
+                typed_ident,
+                eq_token,
+                expression,
+                semi_token,
+            }))
+        } else {
+            let ident: syn::Ident = input.parse()?;
+            let eq_token: Token![=] = input.parse()?;
+            let expression: StreamExpression = input.parse()?;
+            let semi_token: Token![;] = input.parse()?;
+
+            Ok(Equation::OutputDef(Instanciation {
+                ident,
+                eq_token,
+                expression,
+                semi_token,
+            }))
+        }
+    }
 }
