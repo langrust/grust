867e0fd171df2006af4df50d08c6ac6d3bbba9cd
Emilie Thome <emilie.e.thome@renault.com>
2024-05-02 18:03:25 +0200

    feat: add the compilation in the hadle_token function

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -9,21 +9,57 @@
 extern crate proc_macro;
 
 use ast::Ast;
+use backend::rust_ast_from_lir::project::{rust_ast_from_lir, RustASTProject};
+use codespan_reporting::files::SimpleFiles;
+use error::Error;
+use frontend::{lir_from_hir::LIRFromHIR, typing_analysis::TypeAnalysis};
+use hir::file::File;
+use lir::project::Project;
 pub use proc_macro::TokenStream;
+use symbol_table::SymbolTable;
 
+/// GRust AST module.
 pub mod ast;
+/// GRust backend transformations.
+pub mod backend;
+/// GRust common domain or application module.
 pub mod common;
 pub mod conf;
+/// GRust error handler module.
 pub mod error;
+/// GRust frontend transformations.
+pub mod frontend;
+/// GRust HIR module.
+pub mod hir;
+/// GRust LIR module.
+pub mod lir;
+/// GRust symbol table module.
+pub mod symbol_table;
 
 /// Compiles input GRust tokens into output Rust tokens.
 pub fn handle_tokens(tokens: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(tokens as Ast);
-    let tokens = todo!();
+    let rust = rust_from_ast(ast);
+    let tokens = todo!(); // quote::quote!(#rust)
     if let Some(path) = conf::dump_code() {
         dump_code(&path, &tokens);
     }
     TokenStream::from(tokens)
+}
+
+/// Creates RustAST from GRust file.
+pub fn rust_from_ast(ast: Ast) -> RustASTProject {
+    let mut symbol_table = SymbolTable::new();
+    let mut errors = vec![];
+
+    let mut hir: File = todo!();//ast.hir_from_ast(&mut symbol_table, &mut errors).unwrap();
+    hir.typing(&mut symbol_table, &mut errors).unwrap();
+    hir.generate_dependency_graphs(&symbol_table, &mut errors)
+        .unwrap();
+    hir.causality_analysis(&symbol_table, &mut errors).unwrap();
+    hir.normalize(&mut symbol_table, &mut errors).unwrap();
+    let lir: Project = hir.lir_from_hir(&symbol_table);
+    rust_ast_from_lir(lir)
 }
 
 /// Writes the generated code at the given filepath.
