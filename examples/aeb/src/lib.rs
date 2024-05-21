0dfe465495cf2b21f8b20bfadcfc5f6f7ac153f4
Emilie Thome <emilie.thome@hotmail.fr>
2024-05-21 10:52:50 +0200

    test: begin aeb project

---
+++ examples/aeb/src/lib.rs
@@ -0,0 +1,18 @@
+#![allow(warnings)]
+
+use grust::grust;
+mod macro_output;
+
+grust! {
+    #![dump = "C:/Users/az03049/Documents/gitlab/langrust/grustine/examples/aeb/src/macro_output.rs"]
+
+    import signal collision_collection: int;
+    import signal maneuver_acknoledgement: int;
+    import signal vehicle_data: int;
+    import signal nvm_inp: int;
+    import signal cam_obj_info: int;
+    import signal fused_context_data: int;
+    import signal common_variant_mngt: int;
+
+    // TODO
+}
