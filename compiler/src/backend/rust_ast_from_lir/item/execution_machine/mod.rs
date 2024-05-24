ff0a74c6e1ebe14165067426499d39ab96df136d
Emilie Thome <emilie.thome@hotmail.fr>
2024-05-24 18:03:17 +0200

    refactor: file renaming

--- compiler/src/backend/rust_ast_from_lir/item/execution_machine/mod.rs
+++ compiler/src/backend/rust_ast_from_lir/item/execution_machine/mod.rs
@@ -1,9 +1,9 @@
 use crate::lir::item::execution_machine::ExecutionMachine;
 use std::collections::BTreeSet;
 
-use flow_run_loop::rust_ast_from_lir as flow_run_loop_rust_ast_from_lir;
+use service_loop::rust_ast_from_lir as service_loop_rust_ast_from_lir;
 
-pub mod flow_run_loop;
+pub mod service_loop;
 pub mod instruction_flow;
 
 /// Transform LIR execution-machine into items.
@@ -11,5 +11,9 @@ pub fn rust_ast_from_lir(
     execution_machine: ExecutionMachine,
     crates: &mut BTreeSet<String>,
 ) -> Vec<syn::Item> {
-    flow_run_loop_rust_ast_from_lir(execution_machine.run_loop, crates)
+    execution_machine
+        .services_loops
+        .into_iter()
+        .flat_map(|service_loop| service_loop_rust_ast_from_lir(service_loop, crates))
+        .collect()
 }
