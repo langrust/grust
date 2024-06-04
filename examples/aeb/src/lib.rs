a79dfc66715d453744565b52ec1cf9f81d2e258d
Emilie-Thome <emilie.thome@hotmail.fr>
2024-06-04 16:57:45 +0200

    test: testing greusot in aeb example

--- examples/aeb/src/lib.rs
+++ examples/aeb/src/lib.rs
@@ -4,8 +4,8 @@ use grust::grust;
 pub mod macro_output;
 pub mod output;
 
-// AEB `server` module.
-pub mod server;
+grust! {
+    #![dump = "examples/aeb/src/macro_output.rs", greusot = true]
 
     // Branking type
     enum Braking {
@@ -19,21 +19,21 @@ pub mod server;
     // s = speed in km/h.
     // 250 = fixed figure which is always used.
     // f = coefficient of friction, approx. 0.8 on dry asphalt.
-    function compute_soft_braking_distance(speed: float) -> float {
-        return speed * speed / 100.0;
+    function compute_soft_braking_distance(speed: int) -> int {
+        return speed * speed / 100;
     }
 
     // determine braking strategy
-    function brakes(distance: float, speed: float) -> Braking {
-        let braking_distance: float = compute_soft_braking_distance(speed);
+    function brakes(distance: int, speed: int) -> Braking {
+        let braking_distance: int = compute_soft_braking_distance(speed);
         let response: Braking = if braking_distance < distance
                                 then Braking::SoftBrake
                                 else Braking::UrgentBrake;
         return response;
     }
 
-    component braking_state(pedest: float!, speed: float) -> (state: Braking)
-        requires { 0. <= speed && speed < 50. } // urban limit
+    component braking_state(pedest: int!, speed: int) -> (state: Braking)
+        requires { 0 <= speed && speed < 50 } // urban limit
         ensures { when p = pedest? => state != Braking::NoBrake } // safety
     {
         state = when d = pedest? then brakes(d, speed)
@@ -41,11 +41,11 @@ pub mod server;
         let previous_state: Braking = Braking::NoBrake fby state;
     }
 
-    import signal car::speed_km_h                   : float;
-    import event  car::detect::left::pedestrian_l   : float;
-    import event  car::detect::right::pedestrian_r  : float;
-    export signal car::urban::braking::brakes       : Braking;
+    // import signal car::speed_km_h                   : int;
+    // import event  car::detect::left::pedestrian_l   : int;
+    // import event  car::detect::right::pedestrian_r  : int;
+    // export signal car::urban::braking::brakes       : Braking;
 
-    let event pedestrian: timeout(float) = timeout(pedestrian_l, 500);
-    brakes = braking_state(pedestrian, speed_km_h);
+    // let event pedestrian: timeout(int) = timeout(pedestrian_l, 500);
+    // brakes = braking_state(pedestrian, speed_km_h);
 }
--- examples/aeb/src/macro_output.rs
+++ examples/aeb/src/macro_output.rs
@@ -1,14 +1,16 @@
-#[derive(Clone, Copy, Debug, PartialEq, Default)]
+use creusot_contracts::{ensures, requires};
+#[derive(Clone, Copy, Debug)]
 pub enum Braking {
-    #[default]
+    UrgentBrake,
+    SoftBrake,
     NoBrake,
     SoftBrake,
     UrgentBrake,
 }
-pub fn compute_soft_braking_distance(speed: f64) -> f64 {
-    speed * speed / 100.0
+pub fn compute_soft_braking_distance(speed: i64) -> i64 {
+    speed * speed / 100i64
 }
-pub fn brakes(distance: f64, speed: f64) -> Braking {
+pub fn brakes(distance: i64, speed: i64) -> Braking {
     let braking_distance = compute_soft_braking_distance(speed);
     let response = if braking_distance < distance {
         Braking::SoftBrake
@@ -18,11 +20,11 @@ pub fn brakes(distance: f64, speed: f64) -> Braking {
     response
 }
 pub enum BrakingStateEvent {
-    pedest(Result<f64, ()>),
+    pedest(Result<i64, ()>),
     NoEvent,
 }
 pub struct BrakingStateInput {
-    pub speed: f64,
+    pub speed: i64,
     pub braking_state_event: BrakingStateEvent,
 }
 pub struct BrakingStateState {
@@ -34,6 +36,9 @@ impl BrakingStateState {
             mem: Braking::NoBrake,
         }
     }
+    #[requires(0i64 <= input.speed && input.speed < 50i64)]
+    #[ensures(forall < p : i64 > BrakingStateEvent :: pedest(Ok(p)) ==
+    input.braking_state_event == > result != Braking :: NoBrake)]
     pub fn step(&mut self, input: BrakingStateInput) -> Braking {
         let previous_state = self.mem;
         let state = match input.braking_state_event {
@@ -43,208 +48,5 @@ impl BrakingStateState {
         };
         self.mem = state;
         state
-    }
-}
-#[derive(Clone, Copy, Debug, PartialEq, Default)]
-pub struct Context {
-    pub brakes: Braking,
-    pub speed_km_h: f64,
-}
-impl Context {
-    fn init() -> Context {
-        Default::default()
-    }
-}
-pub async fn run_toto_loop(
-    mut collision_collection_channel: tokio::sync::mpsc::Receiver<i64>,
-    mut maneuver_acknoledgement_channel: tokio::sync::mpsc::Receiver<i64>,
-    mut vehicle_data_channel: tokio::sync::mpsc::Receiver<i64>,
-    mut nvm_inp_channel: tokio::sync::mpsc::Receiver<i64>,
-    mut cam_obj_info_channel: tokio::sync::mpsc::Receiver<i64>,
-    mut fused_context_data_channel: tokio::sync::mpsc::Receiver<i64>,
-    mut common_variant_mngt_channel: tokio::sync::mpsc::Receiver<i64>,
-) {
-    let mut context = Context::init();
-    loop {
-        tokio::select! {
-            collision_collection = collision_collection_channel.recv() =>
-            { let collision_collection = collision_collection.unwrap() ; }
-            maneuver_acknoledgement = maneuver_acknoledgement_channel.recv()
-            =>
-            {
-                let maneuver_acknoledgement = maneuver_acknoledgement.unwrap()
-                ;
-            } vehicle_data = vehicle_data_channel.recv() =>
-            { let vehicle_data = vehicle_data.unwrap() ; } nvm_inp =
-            nvm_inp_channel.recv() => { let nvm_inp = nvm_inp.unwrap() ; }
-            cam_obj_info = cam_obj_info_channel.recv() =>
-            { let cam_obj_info = cam_obj_info.unwrap() ; } fused_context_data
-            = fused_context_data_channel.recv() =>
-            { let fused_context_data = fused_context_data.unwrap() ; }
-            common_variant_mngt = common_variant_mngt_channel.recv() =>
-            { let common_variant_mngt = common_variant_mngt.unwrap() ; }
-        }
-    }
-}
-pub mod toto_service {
-    use super::*;
-    use futures::{sink::SinkExt, stream::StreamExt};
-    use TotoServiceInput as I;
-    use TotoServiceOutput as O;
-    use TotoServiceTimer as T;
-    #[derive(PartialEq)]
-    pub enum TotoServiceTimer {
-        timeout_fresh_ident,
-    }
-    impl TotoServiceTimer {
-        pub fn get_duration(&self) -> std::time::Duration {
-            match self {
-                T::timeout_fresh_ident => std::time::Duration::from_millis(2000u64),
-            }
-        }
-    }
-    impl priority_stream::Reset for TotoServiceTimer {
-        fn do_reset(&self) -> bool {
-            match self {
-                T::timeout_fresh_ident => true,
-            }
-        }
-    }
-    pub enum TotoServiceInput {
-        speed_km_h(f64, std::time::Instant),
-        pedestrian_l(f64, std::time::Instant),
-        pedestrian_r(f64, std::time::Instant),
-        timer(T, std::time::Instant),
-    }
-    impl priority_stream::Reset for TotoServiceInput {
-        fn do_reset(&self) -> bool {
-            match self {
-                TotoServiceInput::timer(timer, _) => timer.do_reset(),
-                _ => false,
-            }
-        }
-    }
-    impl PartialEq for TotoServiceInput {
-        fn eq(&self, other: &Self) -> bool {
-            match (self, other) {
-                (I::speed_km_h(this, _), I::speed_km_h(other, _)) => this.eq(other),
-                (I::pedestrian_l(this, _), I::pedestrian_l(other, _)) => this.eq(other),
-                (I::pedestrian_r(this, _), I::pedestrian_r(other, _)) => this.eq(other),
-                (I::timer(this, _), I::timer(other, _)) => this.eq(other),
-                _ => false,
-            }
-        }
-    }
-    impl TotoServiceInput {
-        pub fn get_instant(&self) -> std::time::Instant {
-            match self {
-                I::speed_km_h(_, instant) => *instant,
-                I::pedestrian_l(_, instant) => *instant,
-                I::pedestrian_r(_, instant) => *instant,
-                I::timer(_, instant) => *instant,
-            }
-        }
-        pub fn order(v1: &Self, v2: &Self) -> std::cmp::Ordering {
-            v1.get_instant().cmp(&v2.get_instant())
-        }
-    }
-    pub enum TotoServiceOutput {
-        brakes(Braking, std::time::Instant),
-    }
-    pub struct TotoService {
-        context: Context,
-        braking_state: BrakingStateState,
-        output: futures::channel::mpsc::Sender<O>,
-        timer: futures::channel::mpsc::Sender<(T, std::time::Instant)>,
-    }
-    impl TotoService {
-        pub fn new(
-            output: futures::channel::mpsc::Sender<O>,
-            timer: futures::channel::mpsc::Sender<(T, std::time::Instant)>,
-        ) -> TotoService {
-            let context = Context::init();
-            let braking_state = BrakingStateState::init();
-            TotoService {
-                context,
-                braking_state,
-                output,
-                timer,
-            }
-        }
-        pub async fn run_loop(self, input: impl futures::Stream<Item = I>) {
-            tokio::pin!(input);
-            let mut service = self;
-            loop {
-                tokio::select! {
-                    input = input.next() => if let Some(input) = input
-                    {
-                        match input
-                        {
-                            I :: speed_km_h(speed_km_h, instant) =>
-                            service.handle_speed_km_h(instant, speed_km_h).await, I ::
-                            pedestrian_l(pedestrian_l, instant) =>
-                            service.handle_pedestrian_l(instant, pedestrian_l).await, I
-                            :: pedestrian_r(pedestrian_r, instant) =>
-                            service.handle_pedestrian_r(instant, pedestrian_r).await, I
-                            :: timer(T :: timeout_fresh_ident, instant) =>
-                            service.handle_timeout_fresh_ident(instant).await,
-                        }
-                    } else { break ; }
-                }
-            }
-        }
-        async fn handle_speed_km_h(&mut self, instant: std::time::Instant, speed_km_h: f64) {
-            self.context.speed_km_h = speed_km_h;
-        }
-        async fn handle_pedestrian_l(&mut self, instant: std::time::Instant, pedestrian_l: f64) {
-            let flow_expression_fresh_ident = pedestrian_l;
-            let pedestrian = Ok(flow_expression_fresh_ident);
-            self.timer
-                .send((T::timeout_fresh_ident, instant))
-                .await
-                .unwrap();
-            let brakes = self.braking_state.step(
-                self.context
-                    .get_braking_state_inputs(BrakingStateEvent::pedest(pedestrian)),
-            );
-            self.context.brakes = brakes;
-            self.output
-                .send(O::brakes(self.context.brakes.clone(), instant))
-                .await
-                .unwrap();
-        }
-        async fn handle_pedestrian_r(&mut self, instant: std::time::Instant, pedestrian_r: f64) {
-            let flow_expression_fresh_ident = pedestrian_r;
-            let pedestrian = Ok(flow_expression_fresh_ident);
-            self.timer
-                .send((T::timeout_fresh_ident, instant))
-                .await
-                .unwrap();
-            let brakes = self.braking_state.step(
-                self.context
-                    .get_braking_state_inputs(BrakingStateEvent::pedest(pedestrian)),
-            );
-            self.context.brakes = brakes;
-            self.output
-                .send(O::brakes(self.context.brakes.clone(), instant))
-                .await
-                .unwrap();
-        }
-        async fn handle_timeout_fresh_ident(&mut self, instant: std::time::Instant) {
-            let pedestrian = Err(());
-            self.timer
-                .send((T::timeout_fresh_ident, instant))
-                .await
-                .unwrap();
-            let brakes = self.braking_state.step(
-                self.context
-                    .get_braking_state_inputs(BrakingStateEvent::pedest(pedestrian)),
-            );
-            self.context.brakes = brakes;
-            self.output
-                .send(O::brakes(self.context.brakes.clone(), instant))
-                .await
-                .unwrap();
-        }
     }
 }
