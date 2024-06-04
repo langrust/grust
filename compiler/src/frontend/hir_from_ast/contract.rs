65b4d9b1355e790dd6befdc9360e22172e0a4fc1
Emilie Thome <emilie.e.thome@renault.com>
2024-06-04 11:11:38 +0200

    refactor: tranform ast terms into hir terms

--- compiler/src/frontend/hir_from_ast/contract.rs
+++ compiler/src/frontend/hir_from_ast/contract.rs
@@ -1,12 +1,11 @@
 prelude! {
     ast::contract::{ClauseKind, Contract},
-    hir::contract::Contract as HIRContract,
 }
 
 use super::HIRFromAST;
 
 impl HIRFromAST for Contract {
-    type HIR = HIRContract;
+    type HIR = hir::Contract;
 
     fn hir_from_ast(
         self,
@@ -32,7 +31,7 @@ impl HIRFromAST for Contract {
             },
         );
 
-        Ok(HIRContract {
+        Ok(hir::Contract {
             requires: requires.into_iter().collect::<TRes<Vec<_>>>()?,
             ensures: ensures.into_iter().collect::<TRes<Vec<_>>>()?,
             invariant: invariant.into_iter().collect::<TRes<Vec<_>>>()?,
@@ -43,14 +42,15 @@ impl HIRFromAST for Contract {
 mod term {
     prelude! {
         ast::contract::{Binary, Implication, Term, Unary},
-        operator::{BinaryOperator, UnaryOperator},
-        hir::contract::{Term as HIRTerm, TermKind},
+        operator::BinaryOperator,
     }
+
+    use ast::contract::{Enumeration, EventImplication, ForAll, TimeoutImplication};
 
     use super::HIRFromAST;
 
     impl HIRFromAST for Term {
-        type HIR = HIRTerm;
+        type HIR = hir::contract::Term;
 
         fn hir_from_ast(
             self,
@@ -60,58 +60,176 @@ mod term {
             let location = Location::default();
             match self {
                 Term::Implication(Implication { left, right, .. }) => {
-                    let left = Box::new(left.hir_from_ast(symbol_table, errors)?);
-                    let right = Box::new(right.hir_from_ast(symbol_table, errors)?);
+                    let left = left.hir_from_ast(symbol_table, errors)?;
+                    let right = right.hir_from_ast(symbol_table, errors)?;
 
-                    Ok(HIRTerm {
-                        kind: TermKind::Binary {
-                            op: BinaryOperator::Or,
-                            left: Box::new(HIRTerm {
-                                kind: TermKind::Binary {
-                                    op: BinaryOperator::And,
-                                    left: left.clone(),
-                                    right,
-                                },
-                                location: location.clone(),
-                            }),
-                            right: Box::new(HIRTerm {
-                                kind: TermKind::Unary {
-                                    op: UnaryOperator::Not,
-                                    term: left,
-                                },
-                                location: location.clone(),
-                            }),
-                        },
+                    Ok(hir::contract::Term::new(
+                        hir::contract::term::Kind::implication(left, right),
                         location,
-                    })
+                    ))
                 }
-                Term::Unary(Unary { op, term }) => Ok(HIRTerm {
-                    kind: TermKind::Unary {
-                        op,
-                        term: Box::new(term.hir_from_ast(symbol_table, errors)?),
-                    },
-                    location,
-                }),
-                Term::Binary(Binary { op, left, right }) => Ok(HIRTerm {
-                    kind: TermKind::Binary {
-                        op,
-                        left: Box::new(left.hir_from_ast(symbol_table, errors)?),
-                        right: Box::new(right.hir_from_ast(symbol_table, errors)?),
-                    },
-                    location,
-                }),
-                Term::Constant(constant) => Ok(HIRTerm {
-                    kind: TermKind::Constant { constant },
-                    location,
-                }),
-                Term::Identifier(ident) => {
-                    let name = ident.to_string();
-                    let id =
-                        symbol_table.get_signal_id(&name, true, Location::default(), errors)?;
-                    Ok(HIRTerm {
-                        kind: TermKind::Identifier { id },
+                Term::Enumeration(Enumeration {
+                    enum_name,
+                    elem_name,
+                }) => {
+                    let enum_id =
+                        symbol_table.get_enum_id(&enum_name, false, location.clone(), errors)?;
+                    let element_id = symbol_table.get_enum_elem_id(
+                        &elem_name,
+                        &enum_name,
+                        false,
+                        location.clone(),
+                        errors,
+                    )?;
+                    // TODO check elem is in enum
+                    Ok(hir::contract::Term::new(
+                        hir::contract::term::Kind::enumeration(enum_id, element_id),
                         location,
-                    })
+                    ))
+                }
+                Term::Unary(Unary { op, term }) => Ok(hir::contract::Term::new(
+                    hir::contract::term::Kind::unary(op, term.hir_from_ast(symbol_table, errors)?),
+                    location,
+                )),
+                Term::Binary(Binary { op, left, right }) => Ok(hir::contract::Term::new(
+                    hir::contract::term::Kind::binary(
+                        op,
+                        left.hir_from_ast(symbol_table, errors)?,
+                        right.hir_from_ast(symbol_table, errors)?,
+                    ),
+                    location,
+                )),
+                Term::Constant(constant) => Ok(hir::contract::Term::new(
+                    hir::contract::term::Kind::constant(constant),
+                    location,
+                )),
+                Term::Identifier(ident) => {
+                    let id = symbol_table.get_identifier_id(
+                        &ident,
+                        false,
+                        Location::default(),
+                        errors,
+                    )?;
+                    Ok(hir::contract::Term::new(
+                        hir::contract::term::Kind::ident(id),
+                        location,
+                    ))
+                }
+                Term::ForAll(ForAll {
+                    ident, ty, term, ..
+                }) => {
+                    let ty = ty.hir_from_ast(&location, symbol_table, errors)?;
+                    symbol_table.local();
+                    let id = symbol_table.insert_identifier(
+                        ident.clone(),
+                        Some(ty),
+                        true,
+                        location.clone(),
+                        errors,
+                    )?;
+                    let term = term.hir_from_ast(symbol_table, errors)?;
+                    symbol_table.global();
+                    Ok(hir::contract::Term::new(
+                        hir::contract::term::Kind::forall(id, term),
+                        location,
+                    ))
+                }
+                Term::EventImplication(EventImplication {
+                    pattern,
+                    event,
+                    term,
+                    ..
+                }) => {
+                    // get the event enumeration identifier
+                    let event_enum_id =
+                        symbol_table.get_event_enumeration_id(false, location.clone(), errors)?;
+                    // get the event element identifier
+                    let event_element_id: usize = symbol_table.get_event_element_id(
+                        &event,
+                        false,
+                        location.clone(),
+                        errors,
+                    )?;
+                    symbol_table.local();
+                    // set pattern signal in local context
+                    let pattern_id = symbol_table.insert_identifier(
+                        pattern.clone(),
+                        None,
+                        true,
+                        location.clone(),
+                        errors,
+                    )?;
+                    // transform term into HIR
+                    let right = term.hir_from_ast(symbol_table, errors)?;
+                    symbol_table.global();
+                    // construct right side of implication: `Event::E(pat) == event`
+                    let event_id = symbol_table.get_event_id(false, location.clone(), errors)?;
+                    let left = hir::contract::Term::new(
+                        hir::contract::term::Kind::binary(
+                            BinaryOperator::Eq,
+                            hir::contract::Term::new(
+                                hir::contract::term::Kind::event(
+                                    event_enum_id,
+                                    event_element_id,
+                                    pattern_id,
+                                ),
+                                location.clone(),
+                            ),
+                            hir::contract::Term::new(
+                                hir::contract::term::Kind::ident(event_id),
+                                location.clone(),
+                            ),
+                        ),
+                        location.clone(),
+                    );
+                    // construct result term: `when pat = e? => t` becomes `forall pat.idents, EventE(pat) == event => t`
+                    let term = hir::contract::Term {
+                        kind: hir::contract::term::Kind::forall(
+                            pattern_id,
+                            hir::contract::Term::new(
+                                hir::contract::term::Kind::implication(left, right),
+                                location.clone(),
+                            ),
+                        ),
+                        location,
+                    };
+                    Ok(term)
+                }
+                Term::TimeoutImplication(TimeoutImplication { event, term, .. }) => {
+                    // get the event enumeration identifier
+                    let event_enum_id =
+                        symbol_table.get_event_enumeration_id(false, location.clone(), errors)?;
+                    // get the event element identifier
+                    let event_element_id: usize = symbol_table.get_event_element_id(
+                        &event,
+                        false,
+                        location.clone(),
+                        errors,
+                    )?;
+                    // transform term into HIR
+                    let right = term.hir_from_ast(symbol_table, errors)?;
+                    // construct right side of implication: `Event::ETimeout == event`
+                    let event_id = symbol_table.get_event_id(false, location.clone(), errors)?;
+                    let left = hir::contract::Term::new(
+                        hir::contract::term::Kind::binary(
+                            BinaryOperator::Eq,
+                            hir::contract::Term::new(
+                                hir::contract::term::Kind::timeout(event_enum_id, event_element_id),
+                                location.clone(),
+                            ),
+                            hir::contract::Term::new(
+                                hir::contract::term::Kind::ident(event_id),
+                                location.clone(),
+                            ),
+                        ),
+                        location.clone(),
+                    );
+                    // construct result term: `timeout e? => t` becomes `Event::ETimeout == event => t`
+                    let term = hir::contract::Term::new(
+                        hir::contract::term::Kind::implication(left, right),
+                        location,
+                    );
+                    Ok(term)
                 }
             }
         }
