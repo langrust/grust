4dfdcc2a3e31ba7d89f4e3e00aeb7b49178b121c
Emilie Thome <emilie.thome@hotmail.fr>
2024-06-04 11:11:59 +0200

    feat: transform hir terms into lir terms

--- compiler/src/frontend/lir_from_hir/contract.rs
+++ compiler/src/frontend/lir_from_hir/contract.rs
@@ -52,29 +52,77 @@ impl LIRFromHIR for Contract {
 
 mod term {
     prelude! {
-        hir::contract::{Term, TermKind},
-        lir::contract::Term as LIRTerm,
+        hir::contract::{Term, term},
     }
 
     impl super::LIRFromHIR for Term {
-        type LIR = LIRTerm;
+        type LIR = lir::contract::Term;
 
         fn lir_from_hir(self, symbol_table: &SymbolTable) -> Self::LIR {
             match self.kind {
-                TermKind::Constant { constant } => LIRTerm::Constant { constant },
-                TermKind::Identifier { id } => LIRTerm::Identifier {
-                    name: symbol_table.get_name(id).clone(),
-                    scope: symbol_table.get_scope(id).clone(),
-                },
-                TermKind::Unary { op, term } => LIRTerm::Unary {
+                term::Kind::Constant { constant } => lir::contract::Term::literal(constant),
+                term::Kind::Identifier { id } => {
+                    let name = symbol_table.get_name(id);
+                    match symbol_table.get_scope(id) {
+                        Scope::Input => lir::contract::Term::input(name),
+                        Scope::Memory => lir::contract::Term::mem(name),
+                        Scope::Output => lir::contract::Term::ident("result"),
+                        Scope::Local => lir::contract::Term::ident(name),
+                    }
+                }
+                term::Kind::Enumeration {
+                    enum_id,
+                    element_id,
+                } => lir::contract::Term::enumeration(
+                    symbol_table.get_name(enum_id).clone(),
+                    symbol_table.get_name(element_id).clone(),
+                    None,
+                ),
+                term::Kind::Unary { op, term } => {
+                    lir::contract::Term::unop(op, term.lir_from_hir(symbol_table))
+                }
+                term::Kind::Binary { op, left, right } => lir::contract::Term::binop(
                     op,
-                    term: Box::new(term.lir_from_hir(symbol_table)),
+                    left.lir_from_hir(symbol_table),
+                    right.lir_from_hir(symbol_table),
+                ),
+                term::Kind::ForAll { id, term } => {
+                    let name = symbol_table.get_name(id);
+                    let ty = symbol_table.get_type(id).clone();
+                    let term = term.lir_from_hir(symbol_table);
+                    lir::contract::Term::forall(name, ty, term)
+                }
+                term::Kind::Implication { left, right } => lir::contract::Term::implication(
+                    left.lir_from_hir(symbol_table),
+                    right.lir_from_hir(symbol_table),
+                ),
+                term::Kind::Event {
+                    event_enum_id,
+                    event_element_id,
+                    pattern,
+                } => match symbol_table.get_type(event_element_id) {
+                    Typ::SMEvent(_) => lir::contract::Term::enumeration(
+                        symbol_table.get_name(event_enum_id).clone(),
+                        symbol_table.get_name(event_element_id).clone(),
+                        Some(lir::contract::Term::ident(symbol_table.get_name(pattern))),
+                    ),
+                    Typ::SMTimeout(_) => lir::contract::Term::enumeration(
+                        symbol_table.get_name(event_enum_id).clone(),
+                        symbol_table.get_name(event_element_id).clone(),
+                        Some(lir::contract::Term::ok(lir::contract::Term::ident(
+                            symbol_table.get_name(pattern),
+                        ))),
+                    ),
+                    _ => unreachable!(),
                 },
-                TermKind::Binary { op, left, right } => LIRTerm::Binary {
-                    op,
-                    left: Box::new(left.lir_from_hir(symbol_table)),
-                    right: Box::new(right.lir_from_hir(symbol_table)),
-                },
+                term::Kind::TimeoutEvent {
+                    event_enum_id,
+                    event_element_id,
+                } => lir::contract::Term::enumeration(
+                    symbol_table.get_name(event_enum_id).clone(),
+                    symbol_table.get_name(event_element_id).clone(),
+                    Some(lir::contract::Term::err()),
+                ),
             }
         }
     }
--- compiler_lir/src/contract.rs
+++ compiler_lir/src/contract.rs
@@ -1,38 +1,118 @@
 prelude! {
-    operator::{BinaryOperator, UnaryOperator},
+    operator::{BinaryOperator, UnaryOperator}
 }
 
 #[derive(Debug, PartialEq, Clone)]
 /// Term.
 pub enum Term {
-    /// Constant term: 3
-    Constant {
-        /// The constant
-        constant: Constant,
+    /// A literal expression: `1` or `"hello world"`.
+    Literal {
+        /// The literal.
+        literal: Constant,
+    },
+    /// An identifier call: `x`.
+    Identifier {
+        /// The identifier.
+        identifier: String,
+    },
+    /// A memory access: `self.i_mem`.
+    MemoryAccess {
+        /// The identifier to the memory.
+        identifier: String,
+    },
+    /// An input access: `self.i_mem`.
+    InputAccess {
+        /// The identifier to the input.
+        identifier: String,
+    },
+    /// An unitary operation: `!x`.
+    Unop {
+        /// The operator.
+        op: UnaryOperator,
+        /// The expression.
+        term: Box<Self>,
+    },
+    /// A binary operation: `x + y`.
+    Binop {
+        /// The operator.
+        op: BinaryOperator,
+        /// The left expression.
+        left: Box<Self>,
+        /// The right expression.
+        right: Box<Self>,
     },
     /// Identifier term: x
-    Identifier {
+    Forall {
         /// The identifier's name.
         name: String,
-        /// The identifier's scope.
-        scope: Scope,
-    },
-    /// Unary term: !x
-    Unary {
-        /// The operator
-        op: UnaryOperator,
+        /// The identifier's type.
+        ty: Typ,
         /// The term
         term: Box<Term>,
     },
-    /// Binary term: x == y
-    Binary {
-        /// The operator
-        op: BinaryOperator,
+    /// Implication term: x => y
+    Implication {
         /// Left term
         left: Box<Term>,
         /// Right term
         right: Box<Term>,
     },
+    /// Enumeration term.
+    Enumeration {
+        /// The enumeration type name.
+        enum_name: String,
+        /// The element name.
+        elem_name: String,
+        /// The optional element of the enumeration.
+        element: Option<Box<Term>>,
+    },
+    /// Ok term.
+    Ok {
+        /// The pattern matching the value.
+        term: Box<Term>,
+    },
+    /// Err term.
+    Err,
+}
+
+mk_new! { impl Term =>
+    Literal: literal {
+        literal: Constant,
+    }
+    Identifier: ident {
+        identifier: impl Into<String> = identifier.into(),
+    }
+    MemoryAccess: mem {
+        identifier: impl Into<String> = identifier.into(),
+    }
+    InputAccess: input {
+        identifier: impl Into<String> = identifier.into(),
+    }
+    Unop: unop {
+        op: UnaryOperator,
+        term: Self = term.into(),
+    }
+    Binop: binop {
+        op: BinaryOperator,
+        left: Self = left.into(),
+        right: Self = right.into(),
+    }
+    Forall: forall {
+        name: impl Into<String> = name.into(),
+        ty: Typ,
+        term: Term = term.into(),
+    }
+    Implication: implication {
+        left: Term = left.into(),
+        right: Term = right.into(),
+    }
+    Enumeration: enumeration {
+        enum_name: impl Into<String> = enum_name.into(),
+        elem_name: impl Into<String> = elem_name.into(),
+        element: Option<Term> = element.map(Term::into),
+    }
+    Ok: ok { term: Term = term.into() }
+    Err: err {}
 }
 
 #[derive(Debug, Default, PartialEq, Clone)]
