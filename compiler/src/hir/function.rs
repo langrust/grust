3a6e3d7bb87b7156122baac91ffc452a25f12ba4
Emilie Thome <emilie.e.thome@renault.com>
2024-06-05 09:54:08 +0200

    feat: hir from ast for function's contract

--- compiler/src/hir/function.rs
+++ compiler/src/hir/function.rs
@@ -1,7 +1,7 @@
 //! HIR [Function](crate::hir::function::Function) module.
 
 prelude! {
-    hir::{Expr, Stmt}
+    hir::{Expr, Stmt, Contract}
 }
 
 #[derive(Debug, PartialEq)]
@@ -9,6 +9,8 @@ prelude! {
 pub struct Function {
     /// Function identifier.
     pub id: usize,
+    /// Function's contract.
+    pub contract: Contract,
     /// Function's statements.
     pub statements: Vec<Stmt<Expr>>,
     /// Function's returned expression and its type.
