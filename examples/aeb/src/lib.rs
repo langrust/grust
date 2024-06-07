6ff6213422bc6d65577d7f6fc052faaafbc463ab
Emilie Thome <emilie.thome@hotmail.fr>
2024-06-07 09:32:37 +0200

    refactor: make it binaries

--- examples/aeb/src/lib.rs
+++ examples/aeb/src/lib.rs
@@ -1,21 +1,10 @@
 #![allow(warnings)]
 
-// `aeb` module, where grust is used.
-pub mod aeb;
-
-// include the `interface` module, which is generated from interface.proto.
-pub mod interface {
-    tonic::include_proto!("interface");
-}
-
 // AEB `client` module.
 pub mod client;
 
 // AEB `server` module.
 pub mod server;
-
-// `json` IO module
-pub mod json;
 
 #[cfg(test)]
 mod macro_output;
