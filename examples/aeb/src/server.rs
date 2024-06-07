9419e1874cc653249e2d336eaed6da33b9d6edd8
Emilie Thome <emilie.e.thome@renault.com>
2024-06-07 09:32:37 +0200

    refactor: make it binaries

--- examples/aeb/src/server.rs
+++ examples/aeb/src/server.rs
@@ -1,19 +1,78 @@
-use crate::aeb::{
-    self,
-    toto_service::{TotoService, TotoServiceInput, TotoServiceOutput},
-};
-use crate::interface::{
+mod aeb {
+    use grust::grust;
+
+    grust! {
+        #![dump = "examples/aeb/src/macro_output.rs"]
+
+        // Branking type
+        enum Braking {
+            UrgentBrake,
+            SoftBrake,
+            NoBrake,
+        }
+
+        // Formula: d = 2 * s^2 / (250 * f)
+        // d = braking distance in metres (to be calculated).
+        // s = speed in km/h.
+        // 250 = fixed figure which is always used.
+        // f = coefficient of friction, approx. 0.8 on dry asphalt.
+        function compute_soft_braking_distance(speed: float) -> float {
+            return speed * speed / 100.0;
+        }
+
+        // determine braking strategy
+        function brakes(distance: float, speed: float) -> Braking {
+            let braking_distance: float = compute_soft_braking_distance(speed);
+            let response: Braking = if braking_distance < distance
+                                    then Braking::SoftBrake
+                                    else Braking::UrgentBrake;
+            return response;
+        }
+
+        component braking_state(pedest: float!, speed: float) -> (state: Braking)
+            // requires { 0. <= speed && speed < 50. } // urban limit
+            //ensures { pedest? => state != NoBrake } // safety
+        {
+            when {
+                d = pedest => {
+                    state = brakes(d, speed);
+                },
+                timeout pedest => {
+                    state = Braking::NoBrake;
+                },
+                otherwise => {
+                    state = Braking::NoBrake fby state;
+                }
+            }
+        }
+
+        import signal car::speed_km_h                   : float;
+        import event  car::detect::left::pedestrian_l   : float;
+        import event  car::detect::right::pedestrian_r  : float;
+        export signal car::urban::braking::brakes       : Braking;
+
+        let event pedestrian: timeout(float) = timeout(pedestrian_l, 500);
+        brakes = braking_state(pedestrian, speed_km_h);
+    }
+}
+
+use aeb::toto_service::{TotoService, TotoServiceInput, TotoServiceOutput};
+use interface::{
     aeb_server::{Aeb, AebServer},
     input::Message,
-    Braking, Input, Output,
+    Braking, Input, Output, Pedestrian, Speed,
 };
-use crate::interface::{Pedestrian, Speed};
-use std::env;
 use std::sync::Arc;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
-use tokio::sync::{Mutex, RwLock};
-use tokio_stream::{wrappers::ReceiverStream, Stream, StreamExt};
+use tokio::sync::Mutex;
+use tokio_stream::wrappers::ReceiverStream;
+use tokio_stream::StreamExt;
 use tonic::{transport::Server, Request, Response, Status, Streaming};
+
+// include the `interface` module, which is generated from interface.proto.
+pub mod interface {
+    tonic::include_proto!("interface");
+}
 
 pub struct AebRuntime {
     input_sender: Sender<TotoServiceInput>,
