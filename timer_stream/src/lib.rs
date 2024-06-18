cd1790b7a0738edf1a5c4c3245aba58d84e9d0a2
Emilie-Thome <emilie.thome@hotmail.fr>
2024-06-18 22:13:10 +0200

    feat: add many timer streams and queues

--- timer_stream/src/lib.rs
+++ timer_stream/src/lib.rs
@@ -1,5 +1,11 @@
+mod merge_queue;
+mod merge_stream;
+mod timer;
 mod timer_queue;
 mod timer_stream;
 
-pub use timer_queue::{Timing, Timer, TimerQueue};
+pub use merge_queue::{MergeQueue, MergeTimer};
+pub use merge_stream::{merge_stream, MergeStream};
+pub use timer::{Timer, Timing};
+pub use timer_queue::TimerQueue;
 pub use timer_stream::{timer_stream, TimerStream};
---
+++ timer_stream/src/timer.rs
@@ -0,0 +1,55 @@
+use std::time::{Duration, Instant};
+
+/// Timing trait.
+pub trait Timing {
+    fn get_duration(&self) -> Duration;
+    fn reset(&self) -> bool;
+}
+
+/// Timer.
+///
+/// A timer has a `kind`, which is its identifier (period_component_c, timeout_event_e, etc).
+/// It also has a deadline, to which it should tick.
+#[derive(Debug, PartialEq)]
+pub struct Timer<T> {
+    deadline: Instant,
+    kind: T,
+}
+impl<T> Timer<T>
+where
+    T: Timing,
+{
+    /// Initiate a new timer.
+    pub fn init(kind: T, now: Instant) -> Timer<T> {
+        Timer {
+            deadline: now + kind.get_duration(),
+            kind,
+        }
+    }
+}
+impl<T> Timer<T> {
+    /// Get timer's kind.
+    pub fn get_kind(&self) -> &T {
+        &self.kind
+    }
+    /// Get timer's deadline.
+    pub fn get_deadline(&self) -> &Instant {
+        &self.deadline
+    }
+    /// Get timer's kind and deadline.
+    pub fn get_kind_and_deadline(self) -> (T, Instant) {
+        (self.kind, self.deadline)
+    }
+    /// Create a timer from deadline.
+    pub fn from_deadline(deadline: Instant, kind: T) -> Self {
+        Timer { deadline, kind }
+    }
+    /// Create a timer from millis.
+    #[cfg(test)]
+    pub fn from_millis(millis: u64, kind: T, now: Instant) -> Self {
+        Timer {
+            deadline: now + Duration::from_millis(millis),
+            kind,
+        }
+    }
+}
