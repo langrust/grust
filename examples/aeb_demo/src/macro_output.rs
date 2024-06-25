4d2d1823bffbd6cf5264a294b74b30a826e0c23b
Emilie-Thome <emilie.thome@hotmail.fr>
2024-06-25 17:27:56 +0200

    fix: tipo in dumping path

--- examples/aeb_demo/src/macro_output.rs
+++ examples/aeb_demo/src/macro_output.rs
@@ -71,14 +71,12 @@ pub mod toto_service {
     pub enum TotoServiceTimer {
         timeout_fresh_ident,
     }
-    impl TotoServiceTimer {
-        pub fn get_duration(&self) -> std::time::Duration {
+    impl timer_stream::Timing for TotoServiceTimer {
+        fn get_duration(&self) -> std::time::Duration {
             match self {
                 T::timeout_fresh_ident => std::time::Duration::from_millis(2000u64),
             }
         }
-    }
-    impl priority_stream::Reset for TotoServiceTimer {
         fn do_reset(&self) -> bool {
             match self {
                 T::timeout_fresh_ident => true,
@@ -94,7 +92,7 @@ pub mod toto_service {
     impl priority_stream::Reset for TotoServiceInput {
         fn do_reset(&self) -> bool {
             match self {
-                TotoServiceInput::timer(timer, _) => timer.do_reset(),
+                TotoServiceInput::timer(timer, _) => timer_stream::Timing::do_reset(timer),
                 _ => false,
             }
         }
