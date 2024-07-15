2f0b80b7d0d7ea0751ad56f923b8a0af278fee34
Emilie Thome <emilie.e.thome@ampere.cars>
2024-07-15 13:44:29 +0200

    test: implementing service constrains in output.rs file

--- examples/aeb/src/lib.rs
+++ examples/aeb/src/lib.rs
@@ -7,50 +7,8 @@ pub mod output;
 grust! {
     #![dump = "examples/aeb/src/macro_output.rs", greusot = true]
 
-    // Branking type
-    enum Braking {
-        UrgentBrake,
-        SoftBrake,
-        NoBrake,
-    }
+#[cfg(test)]
+mod macro_output;
 
-    // Formula: d = 2 * s^2 / (250 * f)
-    // d = braking distance in metres (to be calculated).
-    // s = speed in km/h.
-    // 250 = fixed figure which is always used.
-    // f = coefficient of friction, approx. 0.8 on dry asphalt.
-    function compute_soft_braking_distance(speed: int) -> int
-        requires { 0 <= speed && speed < 50 } // urban limit
-    {
-        return speed * speed / 100;
-    }
-
-    // determine braking strategy
-    function brakes(distance: int, speed: int) -> Braking
-        requires { 0 <= speed && speed < 50 } // urban limit
-        ensures { result != Braking::NoBrake } // safety
-    {
-        let braking_distance: int = compute_soft_braking_distance(speed);
-        let response: Braking = if braking_distance < distance
-                                then Braking::SoftBrake
-                                else Braking::UrgentBrake;
-        return response;
-    }
-
-    component braking_state(pedest: int!, speed: int) -> (state: Braking)
-        requires { 0 <= speed && speed < 50 } // urban limit
-        ensures { when p = pedest? => state != Braking::NoBrake } // safety
-    {
-        state = when d = pedest? then brakes(d, speed)
-                timeout Braking::NoBrake otherwise previous_state;
-        let previous_state: Braking = Braking::NoBrake fby state;
-    }
-
-    // import signal car::speed_km_h                   : int;
-    // import event  car::detect::left::pedestrian_l   : int;
-    // import event  car::detect::right::pedestrian_r  : int;
-    // export signal car::urban::braking::brakes       : Braking;
-
-    // let event pedestrian: timeout(int) = timeout(pedestrian_l, 500);
-    // brakes = braking_state(pedestrian, speed_km_h);
-}
+#[cfg(test)]
+mod output;
