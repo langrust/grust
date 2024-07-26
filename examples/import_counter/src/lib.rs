3f943c9ceb25bee3ba94670d7bfa63f402faf1a0
Emilie Thome <emilie.e.thome@ampere.cars>
2024-07-26 15:05:59 +0200

    test: import a component

---
+++ examples/import_counter/src/lib.rs
@@ -0,0 +1,20 @@
+#![allow(warnings)]
+
+use grust::grust;
+mod macro_output;
+
+grust! {
+    #![dump = "examples/import_counter/src/macro_output.rs"]
+
+    import component counter: (res: bool, tick: bool) -> (o: int);
+
+    function add(x: int, y: int) -> int {
+        let res: int = x + y;
+        return res;
+    }
+
+    component test() -> (y: int) {
+        y = counter(false fby (y > 35), half);
+        let half: bool = true fby !half;
+    }
+}
