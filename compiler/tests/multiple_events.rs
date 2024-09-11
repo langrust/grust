3cadbd989cdd7c0831aeb30ccdc3bd8076815c8a
Émilie THOMÉ <emilie.e.thome@ampere.cars>
2024-09-11 11:02:00 +0200

    fix: remove 'otherwise' and add 'emit'

--- compiler/tests/multiple_events.rs
+++ compiler/tests/multiple_events.rs
@@ -9,7 +9,7 @@ fn should_compile_multiple_events() {
 
         component multiple_events(a: int?, b: int?, v: int) -> (c: int, d: float) {
             c = z;
-            d = when (let _ = a?, let _ = b?) then 0.1 otherwise 0.2;
+            d = when (let _ = a?, let _ = b?) then 0.1;
             when {
                 (let a = a?, let b = b?) if v > 50 => {
                     let z: int = 1;
@@ -19,9 +19,6 @@ fn should_compile_multiple_events() {
                 }
                 let b = b? => {
                     let z: int = if v > 50 then 3 else 4;
-                }
-                otherwise => {
-                    let z: int = 0 fby c;
                 }
             }
         }
--- compiler/tests/rising_edges.rs
+++ compiler/tests/rising_edges.rs
@@ -12,23 +12,20 @@ fn should_compile_rising_edges() {
             d: float,
             x: int?,
         ) {
-            c = when a? then a otherwise z;
-            d = when let _ = y? then 0.1 otherwise 0.2;
+            c = when a? then a;
+            d = when let _ = y? then 0.1;
             when {
                 (a?, let e = b?, v > 50) => {
                     let z: int =  if v > 80 then e else a;
-                    let y: unit? = ();
+                    let y: unit? = emit ();
                 }
                 (v < 40, a?) if a != 0 => {
                     let z: int = 2;
-                    x = 2;
+                    x = emit 2;
                 }
                 let e = b? => {
-                    let z: int = if v > 50 then 3 else 4;
-                    x = when e < 20 then 2;
-                }
-                otherwise => {
-                    let z: int = when v > 50 then v + (0 fby c) otherwise 0;
+                    let z: int = when v > 50 then v + (0 fby c);
+                    x = when e < 20 then emit 2;
                 }
             }
         }
