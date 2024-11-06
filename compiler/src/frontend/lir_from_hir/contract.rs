d79323ae83a27fc04924d16ab652084d1349d22a
Adrien Champion <adrien.champion@protonmail.com>
2024-11-06 15:10:28 +0100

    chore: minor hir contract qol

--- compiler/src/frontend/lir_from_hir/contract.rs
+++ compiler/src/frontend/lir_from_hir/contract.rs
@@ -34,7 +34,7 @@ impl LIRFromHIR for Contract {
 
 mod term {
     prelude! {
-        hir::contract::{Term, term},
+        hir::contract::{Term, Kind},
     }
 
     impl super::LIRFromHIR for Term {
@@ -42,8 +42,8 @@ mod term {
 
         fn lir_from_hir(self, symbol_table: &SymbolTable) -> Self::LIR {
             match self.kind {
-                term::Kind::Constant { constant } => lir::contract::Term::literal(constant),
-                term::Kind::Identifier { id } => {
+                Kind::Constant { constant } => lir::contract::Term::literal(constant),
+                Kind::Identifier { id } => {
                     let name = symbol_table.get_name(id);
                     match symbol_table.get_scope(id) {
                         Scope::Input => lir::contract::Term::input(name),
@@ -52,7 +52,7 @@ mod term {
                         Scope::VeryLocal => unreachable!("you should not do that with this ident"),
                     }
                 }
-                term::Kind::Enumeration {
+                Kind::Enumeration {
                     enum_id,
                     element_id,
                 } => lir::contract::Term::enumeration(
@@ -60,32 +60,30 @@ mod term {
                     symbol_table.get_name(element_id).clone(),
                     None,
                 ),
-                term::Kind::Unary { op, term } => {
+                Kind::Unary { op, term } => {
                     lir::contract::Term::unop(op, term.lir_from_hir(symbol_table))
                 }
-                term::Kind::Binary { op, left, right } => lir::contract::Term::binop(
+                Kind::Binary { op, left, right } => lir::contract::Term::binop(
                     op,
                     left.lir_from_hir(symbol_table),
                     right.lir_from_hir(symbol_table),
                 ),
-                term::Kind::ForAll { id, term } => {
+                Kind::ForAll { id, term } => {
                     let name = symbol_table.get_name(id);
                     let ty = symbol_table.get_type(id).clone();
                     let term = term.lir_from_hir(symbol_table);
                     lir::contract::Term::forall(name, ty, term)
                 }
-                term::Kind::Implication { left, right } => lir::contract::Term::implication(
+                Kind::Implication { left, right } => lir::contract::Term::implication(
                     left.lir_from_hir(symbol_table),
                     right.lir_from_hir(symbol_table),
                 ),
-                term::Kind::PresentEvent { event_id, pattern } => {
-                    match symbol_table.get_type(event_id) {
-                        Typ::SMEvent { .. } => lir::contract::Term::some(
-                            lir::contract::Term::ident(symbol_table.get_name(pattern)),
-                        ),
-                        _ => unreachable!(),
-                    }
-                }
+                Kind::PresentEvent { event_id, pattern } => match symbol_table.get_type(event_id) {
+                    Typ::SMEvent { .. } => lir::contract::Term::some(lir::contract::Term::ident(
+                        symbol_table.get_name(pattern),
+                    )),
+                    _ => unreachable!(),
+                },
             }
         }
     }
