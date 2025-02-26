6ed790aa0f250ef8d9e41ea820b800f5bd11c925
Émilie THOMÉ <emilie.thome@isae-supaero.fr>
2025-02-26 10:07:29 +0100

    fix: acc component passes proofs

--- examples/acc_fmics_greusot/src/lib.rs
+++ examples/acc_fmics_greusot/src/lib.rs
@@ -10,6 +10,7 @@ grust! {
         requires { 0 < sv_v && sv_v <= 50 }
         requires { 0 < fv_v && fv_v < sv_v && sv_v - fv_v <= 10 }
         ensures  { 0 <= result && result <= 140 }
+        ensures  { result == sv_v*1 + (sv_v^2)/(2*6) - (fv_v^2)/(2*6) }
     {
         let rho: int = 1; // SV's reaction time
         let b_max: int = 6;
@@ -19,18 +20,20 @@ grust! {
     }
 
     // Filters the ACC on driver activation and when approaching FV
-    component acc(c:bool, d:int, v:int, s:int) -> (b:int)
-        requires { c => (0 < s && s <= 50) }
-        requires { c => (-s < v && -10 <= v && v < 0) }
-        requires { c => d >= s*(1 - v/6) }
-        ensures  { 0 <= b && b <= 6 && (c => b > 0) }
-        ensures  { c => forall _t: int, 0 < _t && _t <= -v/b
-                     => d + v*_t + b*_t^2/2 >= (s-b*_t)*1 +
-                            ((s-b*_t)^2 - (s+v)^2)/(2*6) }
+    component acc(c: bool, d: int, v: int, s: int) -> (b: int)
+        requires { d < 150 } // radar detection limitation
+        requires { c => (0 < s && s <= 50) && (0 < s+v && v < 0 && -v <= 10) } // scope
+        // there is enough distance to brake at maximum rate
+        requires { c => d - (s*1 + (s^2)/(2*6) - (s+v)^2/(2*6)) > (v^2)/(2*6) }
+        // braking rate is in correct interval
+        ensures  { 0 <= b && b <= 6 }
+        // ensures  { c => forall _t: int, 0 < _t && _t <= -v/b
+        //              => d + v*_t + b*_t^2/2 >= (s-b*_t)*1 +
+        //                     ((s-b*_t)^2 - (s+v)^2)/(2*6) }
     {
         match c {
             true => {
-                b = v^2 / (2 * (d - d_safe));
+                b = compute_braking(d - d_safe, v);
                 let d_safe: int = safety_distance(s, fv_v);
                 let fv_v: int = s + v;
             },
@@ -39,5 +42,16 @@ grust! {
                 let (fv_v: int, d_safe: int) = (0, 0);
             },
         }
+    }
+
+    // Intermediate braking function to pass the proof
+    component compute_braking(d_grace: int, v: int) -> (b: int)
+        requires { (0 < d_grace &&  d_grace < 150) && (v < 0 && -v <= 10) } // scope
+        // there is enough distance to brake at maximum rate
+        requires { d_grace > (v^2)/(2*6) }
+        // braking rate is in correct interval
+        ensures  { 0 <= b && b <= 6 }
+    {
+        b = (v^2) / (2 * d_grace);
     }
 }
