2aca0e45ee6f968a88412be0e078318f2e5f7122
Émilie THOMÉ <emilie.e.thome@ampere.cars>
2025-04-04 09:28:05 +0200

    test: add constant values in acc tests

--- examples/acc_fmics_greusot/src/lib.rs
+++ examples/acc_fmics_greusot/src/lib.rs
@@ -5,17 +5,18 @@ use grust::grust;
 grust! {
     #![greusot]
 
+    const RHO: int = 1; // reaction time
+    const B_MAX: int = 6; // 0.6*9.81
+
     // Safety distance computation
     function safety_distance(sv_v: int, fv_v: int) -> int
         requires { 0 < sv_v && sv_v <= 50 }
         requires { 0 < fv_v && fv_v < sv_v && sv_v - fv_v <= 10 }
         ensures  { 0 < result && result < 150 }
-        ensures  { result == sv_v*1 + (sv_v^2)/(2*6) - (fv_v^2)/(2*6) }
+        ensures  { result == sv_v*RHO + (sv_v^2)/(2*B_MAX) - (fv_v^2)/(2*B_MAX) }
     {
-        let rho: int = 1; // SV's reaction time
-        let b_max: int = 6;
-        let sv_d_stop: int = sv_v*rho + sv_v^2/(2*b_max);
-        let fv_d_stop: int = fv_v^2/(2*b_max);
+        let sv_d_stop: int = sv_v*RHO + sv_v^2/(2*B_MAX);
+        let fv_d_stop: int = fv_v^2/(2*B_MAX);
         return sv_d_stop - fv_d_stop;
     }
 
@@ -24,12 +25,12 @@ grust! {
         requires { d < 150 } // radar detection limitation
         requires { c => (0 < s && s <= 50) && (0 < s+v && v < 0 && -v <= 10) } // scope
         // there is enough distance to brake at maximum rate
-        requires { c => d - (s*1 + (s^2)/(2*6) - (s+v)^2/(2*6)) > (v^2)/(2*6) }
+        requires { c => d - (s*RHO + (s^2)/(2*B_MAX) - (s+v)^2/(2*B_MAX)) > (v^2)/(2*B_MAX) }
         // braking rate is in correct interval
-        ensures  { 0 <= b && b <= 6 }
+        ensures  { 0 <= b && b <= B_MAX }
         // ensures  { c => forall _t: int, 0 < _t && _t <= -v/b
-        //              => d + v*_t + b*_t^2/2 >= (s-b*_t)*1 +
-        //                     ((s-b*_t)^2 - (s+v)^2)/(2*6) }
+        //              => d + v*_t + b*_t^2/2 >= (s-b*_t)*RHO +
+        //                     ((s-b*_t)^2 - (s+v)^2)/(2*B_MAX) }
     {
         match c {
             true => {
@@ -48,9 +49,9 @@ grust! {
     function compute_braking(d_grace: int, v: int) -> int
         requires { (0 < d_grace &&  d_grace < 150) && (v < 0 && -v <= 10) } // scope
         // there is enough distance to brake at maximum rate
-        requires { d_grace > (v^2)/(2*6) }
+        requires { d_grace > (v^2)/(2*B_MAX) }
         // braking rate is in correct interval
-        ensures  { 0 <= result && result <= 6 }
+        ensures  { 0 <= result && result <= B_MAX }
     {
         return (v^2) / (2 * d_grace);
     }
