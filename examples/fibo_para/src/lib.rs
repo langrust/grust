2023058dcbd33cc7f78efce9a733016c6fc44166
Adrien Champion <adrien.champion@protonmail.com>
2025-04-04 17:51:32 +0200

    chore: add basic test case for ext-fun weight hint

---
+++ examples/fibo_para/src/lib.rs
@@ -0,0 +1,38 @@
+#![allow(warnings)]
+
+use grust::grust;
+
+pub mod module {
+    pub fn add_i64(n: i64, m: i64) -> i64 {
+        n + m
+    }
+}
+
+grust! {
+    #![component_para (2, 6, 20)]
+
+    #[weight = 15]
+    fn module::add_i64(n: int, m: int) -> int;
+
+    component next(i: int) -> (next_o: int) {
+        init i = 1;
+        next_o = add_i64(i, last i);
+    }
+
+    component semi_fib(i: int) -> (o: int) {
+        let next_o: int = next(i);
+        o = last next_o;
+        init next_o = 0;
+    }
+
+    component fib_call() -> (fib: int) {
+        let next_o: int = next(fib);
+        fib = semi_fib(fib);
+    }
+
+    component fib() -> (fib: int) {
+        init (fib, next_o) = (1, 0);
+        let next_o: int = fib + last fib;
+        fib = last next_o;
+    }
+}
