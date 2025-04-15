c5d9ebca580db904694f0507a7cac9677d4ec92e
Émilie THOMÉ <emilie.thome@isae-supaero.fr>
2025-04-15 08:46:10 +0200

    test: add test of over_sampling resilience

--- grust_std/src/time.rs
+++ grust_std/src/time.rs
@@ -1,38 +1,9 @@
 pub mod integration {
-    /// Forward Euler method.
-    ///
-    /// F_n+1 = F_n + (t_n+1 - t_n) * f_n+1
-    pub struct ForwardEulerState {
-        integral: f64,
-        last_t: f64,
-    }
-    pub struct ForwardEulerInput {
-        pub x: f64,
-        pub t: f64,
-    }
-    impl grust_core::Component for ForwardEulerState {
-        type Input = ForwardEulerInput;
-        type Output = f64;
-
-        fn init() -> Self {
-            ForwardEulerState {
-                integral: 0.,
-                last_t: 0.,
-            }
-        }
-
-        fn step(&mut self, input: Self::Input) -> Self::Output {
-            let dt = input.t - self.last_t;
-            let integral = self.integral + input.x * dt;
-            self.integral = integral;
-            self.last_t = input.t;
-            integral
-        }
-    }
-
     /// Backward Euler method.
     ///
-    /// F_n+1 = F_n + (t_n+1 - t_n) * f_n
+    /// Implements `F_n+1 = F_n + (t_n+1 - t_n) * f_n`.
+    /// Called in `grust!` macro via
+    /// `use component core::time::integration::backward_euler(x: float, t: float) -> (i: float);`.
     pub struct BackwardEulerState {
         integral: f64,
         last_t: f64,
@@ -61,6 +32,99 @@ pub mod integration {
             self.last_t = input.t;
             self.last_x = input.x;
             integral
+        }
+    }
+
+    #[cfg(test)]
+    mod backward_euler {
+        use grust_core::Component;
+
+        use super::{BackwardEulerInput, BackwardEulerState};
+
+        impl Clone for BackwardEulerInput {
+            fn clone(&self) -> Self {
+                Self {
+                    x: self.x.clone(),
+                    t: self.t.clone(),
+                }
+            }
+        }
+
+        #[test]
+        fn should_be_resilient_to_oversampling() {
+            let mut euler_1 = BackwardEulerState::init();
+            let mut euler_2 = BackwardEulerState::init();
+            let i1 = BackwardEulerInput { x: 4.0, t: 0.33 };
+            let i_over_sample = BackwardEulerInput { x: 4.0, t: 1.77 };
+            let i2 = BackwardEulerInput { x: 6.6, t: 2.13 };
+
+            let _ = (euler_1.step(i1.clone()), euler_2.step(i1));
+            let _ = euler_1.step(i_over_sample); // over sample
+            let (o1, o2) = (euler_1.step(i2.clone()), euler_2.step(i2));
+
+            assert_eq!(o1, o2)
+        }
+    }
+    
+    /// Forward Euler method.
+    ///
+    /// F_n+1 = F_n + (t_n+1 - t_n) * f_n+1
+    struct ForwardEulerState {
+        integral: f64,
+        last_t: f64,
+    }
+    struct ForwardEulerInput {
+        x: f64,
+        t: f64,
+    }
+    impl grust_core::Component for ForwardEulerState {
+        type Input = ForwardEulerInput;
+        type Output = f64;
+
+        fn init() -> Self {
+            ForwardEulerState {
+                integral: 0.,
+                last_t: 0.,
+            }
+        }
+
+        fn step(&mut self, input: Self::Input) -> Self::Output {
+            let dt = input.t - self.last_t;
+            let integral = self.integral + input.x * dt;
+            self.integral = integral;
+            self.last_t = input.t;
+            integral
+        }
+    }
+
+    #[cfg(test)]
+    mod forward_euler {
+        use grust_core::Component;
+
+        use super::{ForwardEulerInput, ForwardEulerState};
+
+        impl Clone for ForwardEulerInput {
+            fn clone(&self) -> Self {
+                Self {
+                    x: self.x.clone(),
+                    t: self.t.clone(),
+                }
+            }
+        }
+
+        #[test]
+        fn not_resilient_to_oversampling() {
+            let mut euler_1 = ForwardEulerState::init();
+            let mut euler_2 = ForwardEulerState::init();
+            let i1 = ForwardEulerInput { x: 4.0, t: 0.33 };
+            let i_over_sample = ForwardEulerInput { x: 4.0, t: 1.77 };
+            let i2 = ForwardEulerInput { x: 6.0, t: 2.13 };
+
+            let _ = (euler_1.step(i1.clone()), euler_2.step(i1));
+            let _ = euler_1.step(i_over_sample); // over sample
+            let (o1, o2) = (euler_1.step(i2.clone()), euler_2.step(i2));
+
+            assert_ne!(o1, o2)
         }
     }
 }
