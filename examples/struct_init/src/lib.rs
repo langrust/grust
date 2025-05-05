ef2cef2fb4b84231ab7ecb0697f07aac7b33c21b
Émilie THOMÉ <emilie.thome@isae-supaero.fr>
2025-05-05 18:54:52 +0200

    test: init structure

---
+++ examples/struct_init/src/lib.rs
@@ -0,0 +1,22 @@
+#![allow(warnings)]
+
+use grust::grust;
+
+grust! {
+    #![dump = "examples/struct_init/out/dumped.rs"]
+
+    enum Priority {
+        Hight, Medium, Low,
+    }
+
+    struct Alarm {
+        prio: Priority,
+        data: int,
+    }
+
+    component alarm_filtering(alarm: Alarm) -> (delayed: Alarm) {
+        init temp = Alarm { prio: Priority::Low, data: 0 };
+        delayed = last temp;
+        let temp: Alarm = alarm;
+    }
+}
