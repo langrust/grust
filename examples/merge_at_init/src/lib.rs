ecc1c706edbf6e82ba14fbe671803917905e0fa8
Emilie Thome <emilie.e.thome@ampere.cars>
2025-07-21 14:49:39 +0200

    fix: merge handle absence of events

---
+++ examples/merge_at_init/src/lib.rs
@@ -0,0 +1,12 @@
+grust::grust! {
+    #![dump = "examples/merge_at_init/out/macro_output.rs"]
+
+    import signal  measure  : float;
+    import event  stabilize  : float;
+    export event  compute_ev  : float;
+
+    service kalman_task @ [10, 3000] {
+        let event measure_ev : float = on_change(measure);
+        compute_ev = merge(measure_ev, stabilize);
+    }
+}
