40d5d858cf923f4244102eaacd078a88c01c830a
MASSIF Matthieu <matthieu.massif@student.isae-supaero.fr>
2025-07-29 16:06:14 +0000

    feat: maths-modules

---
+++ grust/out/maths_fun.rs
@@ -0,0 +1,93 @@
+pub fn that_use_cos(theta: f64) -> f64 {
+    std::maths::trigo::cos(theta)
+}
+pub fn that_use_sin(theta: f64) -> f64 {
+    std::maths::trigo::sin(theta)
+}
+pub fn that_use_tan(theta: f64) -> f64 {
+    std::maths::trigo::tan(theta)
+}
+pub fn that_use_acos(theta: f64) -> f64 {
+    std::maths::trigo::acos(theta)
+}
+pub fn that_use_asin(theta: f64) -> f64 {
+    std::maths::trigo::asin(theta)
+}
+pub fn that_use_atan(theta: f64) -> f64 {
+    std::maths::trigo::atan(theta)
+}
+pub fn that_use_atan2(y: f64, x: f64) -> f64 {
+    std::maths::trigo::atan2(y, x)
+}
+pub fn that_use_cosh(theta: f64) -> f64 {
+    std::maths::trigo::cosh(theta)
+}
+pub fn that_use_sinh(theta: f64) -> f64 {
+    std::maths::trigo::sinh(theta)
+}
+pub fn that_use_tanh(theta: f64) -> f64 {
+    std::maths::trigo::tanh(theta)
+}
+pub fn that_use_acosh(theta: f64) -> f64 {
+    std::maths::trigo::acosh(theta)
+}
+pub fn that_use_asinh(theta: f64) -> f64 {
+    std::maths::trigo::asinh(theta)
+}
+pub fn that_use_atanh(theta: f64) -> f64 {
+    std::maths::trigo::atanh(theta)
+}
+pub fn that_use_round(x: f64) -> i64 {
+    std::maths::round::round(x)
+}
+pub fn that_use_floor(x: f64) -> i64 {
+    std::maths::round::floor(x)
+}
+pub fn that_use_ceil(x: f64) -> i64 {
+    std::maths::round::ceil(x)
+}
+pub fn that_use_exp(x: f64) -> f64 {
+    std::maths::exponential::exp(x)
+}
+pub fn that_use_ln(x: f64) -> f64 {
+    std::maths::exponential::ln(x)
+}
+pub fn that_use_log(x: f64, n: f64) -> f64 {
+    std::maths::exponential::log(x, n)
+}
+pub fn that_use_log10(x: f64) -> f64 {
+    std::maths::exponential::log10(x)
+}
+pub fn that_use_log2(x: f64) -> f64 {
+    std::maths::exponential::log2(x)
+}
+pub fn that_use_min(x: f64, y: f64) -> f64 {
+    std::maths::usuals::min(x, y)
+}
+pub fn that_use_min_i(x: i64, y: i64) -> i64 {
+    std::maths::usuals::min_i(x, y)
+}
+pub fn that_use_max(x: f64, y: f64) -> f64 {
+    std::maths::usuals::max(x, y)
+}
+pub fn that_use_max_i(x: i64, y: i64) -> i64 {
+    std::maths::usuals::max_i(x, y)
+}
+pub fn that_use_abs(x: f64) -> f64 {
+    std::maths::usuals::abs(x)
+}
+pub fn that_use_abs_i(x: i64) -> i64 {
+    std::maths::usuals::abs_i(x)
+}
+pub fn that_use_pow(x: f64, n: f64) -> f64 {
+    std::maths::usuals::pow(x, n)
+}
+pub fn that_use_pow_i(x: i64, n: i64) -> i64 {
+    std::maths::usuals::pow_i(x, n)
+}
+pub fn that_use_sqrt(x: f64) -> f64 {
+    std::maths::usuals::sqrt(x)
+}
+pub fn that_use_sqrt_i(x: i64) -> f64 {
+    std::maths::usuals::sqrt_i(x)
+}
--- grust_std/src/lib.rs
+++ grust_std/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(missing_docs)]
 pub extern crate rayon;
+pub mod maths;
 pub mod rising_edge;
 pub mod time;
